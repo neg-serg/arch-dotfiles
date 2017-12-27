8dee3fe18ab63e8f4aff7aebacbde3055d05a48a
Neg <serg.zorg@gmail.com>
2017-12-27 17:49:18 +0300

    [compton] no output

--- xinit/.config/xinit/compositing.zsh
+++ xinit/.config/xinit/compositing.zsh
@@ -4,8 +4,8 @@ if [[ -z "$1" ]]; then
     nexec compton || sudo pkill compton
     if [[ $(egrep -i " connected|card detect|primary dev|Setting driver" /var/log/Xorg.0.log \
         | grep -wo 'Using Kernel Mode Setting driver: i915') == "" ]]; then
-        compton -b --config "${XDG_CONFIG_HOME}/compton/compton.conf" &
+        compton -b --config "${XDG_CONFIG_HOME}/compton/compton.conf" > /dev/null &
     else
-        compton -b --config "${XDG_CONFIG_HOME}/compton/compton_intel.conf" &
+        compton -b --config "${XDG_CONFIG_HOME}/compton/compton_intel.conf" > /dev/null &
     fi
 fi
