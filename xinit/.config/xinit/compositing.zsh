a9db157ce72c3c89cefbff22f1208bbefe0382e2
Neg <serg.zorg@gmail.com>
2017-12-28 16:30:51 +0300

    [compton] merge compton configs into one

--- xinit/.config/xinit/compositing.zsh
+++ xinit/.config/xinit/compositing.zsh
@@ -2,10 +2,11 @@
 
 if [[ -z "$1" ]]; then
     nexec compton || sudo pkill compton
+    compton_cfg="${XDG_CONFIG_HOME}/compton/compton.conf"
     if [[ $(egrep -i " connected|card detect|primary dev|Setting driver" /var/log/Xorg.0.log \
         | grep -wo 'Using Kernel Mode Setting driver: i915') == "" ]]; then
-        compton -b --config "${XDG_CONFIG_HOME}/compton/compton.conf" > /dev/null &
+        compton -b --config "${compton_cfg}"  --vsync=opengl > /dev/null &
     else
-        compton -b --config "${XDG_CONFIG_HOME}/compton/compton_intel.conf" > /dev/null &
+        compton -b --config "${compton_cfg}" --vsync=none > /dev/null &
     fi
 fi
