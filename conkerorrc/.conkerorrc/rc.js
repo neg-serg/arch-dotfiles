b5e3fdfe20950c62a7814bd43933f70acc7ddf81
Neg <serg.zorg@gmail.com>
2018-01-11 22:43:19 +0300

    [conkeror] update settings

--- conkerorrc/.conkerorrc/rc.js
+++ conkerorrc/.conkerorrc/rc.js
@@ -1,11 +1,10 @@
-// the default page for new buffers.
-homepage = "localhost:31831";
+homepage = "localhost:31831"; // the default page for new buffers.
 
 require("favicon");
 require("session.js");
 require("clicks-in-new-buffer.js");
-require("block-content-focus-change.js");
 require("content-policy.js");
+// require("block-content-focus-change.js");
 
 default_minibuffer_auto_complete_delay = 100;
 window_extra_argument_max_delay = 10;
@@ -23,19 +22,22 @@ session_pref("layout.spellcheckDefault",1);
 //session_pref('network.proxy.ssl_port',8118);
 //session_pref("network.proxy.type",1);
 
-user_pref('webgl.prefer-native-gl', true);
+user_pref('webgl.prefer-native-gl',            true);
 user_pref('layers.acceleration.force-enabled', true);
-user_pref('webgl.force-enabled', true);
-user_pref('webgl.msaa-force', true);
-user_pref('gfx.filter.nearest.force-enabled', true);
-user_pref('stagefright.force-enabled', true); // H264
-user_pref('layers.acceleration.draw-fps', false);
+user_pref('webgl.force-enabled',               true);
+user_pref('webgl.msaa-force',                  true);
+user_pref('gfx.filter.nearest.force-enabled',  true);
+user_pref('stagefright.force-enabled',         true); // H264
+user_pref('layers.acceleration.draw-fps',      false);
 
-user_pref("extensions.checkCompatibility", false);
-user_pref("xpinstall.whitelist.required", false);
-user_pref("xpinstall.signatures.required", false);
-user_pref("devtools.debugger.remote-enabled",false);
+user_pref("extensions.checkCompatibility",     false);
+user_pref("xpinstall.whitelist.required",      false);
+user_pref("xpinstall.signatures.required",     false);
+user_pref("devtools.debugger.remote-enabled",  false);
 // user_pref("media.autoplay.enabled",false); // setting this breaks many videos
+// enable if full screen video stops working
+session_pref("full-screen-api.enabled", true);
+session_pref("font.minimum-size.zh-CN", 18);
 
 session_auto_save_auto_load_fn = session_auto_save_load_window_current;
 session_auto_save_auto_load = "prompt";
@@ -50,19 +52,13 @@ xkcd_add_title = true;
 read_buffer_show_icons = true;
 view_source_use_external_editor = true;  // view source in your editor.
 
-// enable if full screen video stops working
-session_pref("full-screen-api.enabled", true);
-session_pref("font.minimum-size.zh-CN", 18);
-
 function my_zoom_set (buffer) { browser_zoom_set(buffer, false, 110); }
 add_hook('create_buffer_late_hook', my_zoom_set);
 
 external_content_handlers.set("application/pdf", "zathura");
 
-// // require("block-content-focus-change");
 require("ublt-buffer");
 // require("ublt-launchers");
 require("ublt-appearance");
 // require("ublt-capture");
 // require("ublt-download");
-// require("from-other");
