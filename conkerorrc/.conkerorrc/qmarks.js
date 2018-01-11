6034656cb509bcf8a2816b22847b90abce4b198e
Neg <serg.zorg@gmail.com>
2018-01-12 01:34:06 +0300

    [conkeror] update qmarks

--- conkerorrc/.conkerorrc/qmarks.js
+++ conkerorrc/.conkerorrc/qmarks.js
@@ -1,5 +1,9 @@
 interactive("open-gmail", "Go to gmail", "follow", $browser_object = "http://gmail.com/");
-interactive("open-gmail-new", "Go to gmail", "follow-new-buffer", $browser_object = "http://gmail.com/");
+interactive("open-gmail-new", "Go to gmail in the new buffer", "follow-new-buffer", $browser_object = "http://gmail.com/");
+interactive("open-hackernews", "Go to hackernews", "follow", $browser_object = "https://news.ycombinator.com/");
+interactive("open-hackernews-new", "Go to hackernews in the new buffer", "follow", $browser_object = "https://news.ycombinator.com/");
 
 define_key(content_buffer_normal_keymap, "g o g", "open-gmail");
-define_key(content_buffer_normal_keymap, "g n g", "open-gmail");
+define_key(content_buffer_normal_keymap, "g n g", "open-gmail-new");
+define_key(content_buffer_normal_keymap, "g o h", "open-hackernews");
+define_key(content_buffer_normal_keymap, "g n h", "open-hackernews-new");
