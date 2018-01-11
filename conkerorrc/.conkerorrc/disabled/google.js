5becbf47f11fa415370ffb3e9423387d4b6b5dd0
Neg <serg.zorg@gmail.com>
2018-01-11 22:37:02 +0300

    [conkeror] disable various stuff

---
+++ conkerorrc/.conkerorrc/disabled/google.js
@@ -0,0 +1,2 @@
+// browser_prevent_automatic_form_focus_mode(true);
+google_search_bind_number_shortcuts();
---
+++ conkerorrc/.conkerorrc/disabled/page-modes.js
@@ -0,0 +1,7 @@
+if (2 + 2 == 5) {
+    require("github");
+    require("gmail");
+    require("reddit");
+    require("youtube");
+    require("page-modes/google-search-results.js");
+}
