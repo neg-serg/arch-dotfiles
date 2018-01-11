db1c23b62a702e654ca8c8d02083f36ea874e6a0
Neg <serg.zorg@gmail.com>
2018-01-11 14:06:05 +0300

    [conkeror] new keybindings

--- conkerorrc/.conkerorrc/keys.js
+++ conkerorrc/.conkerorrc/keys.js
@@ -1,11 +1,17 @@
 require ("global-overlay-keymap.js");
 define_key_alias("C-m", "return");
 
+define_key(content_buffer_normal_keymap, "o", "find-url-from-history");
 define_key(content_buffer_normal_keymap, "t", "find-url-new-buffer");
 define_key(content_buffer_normal_keymap, "d", "kill-current-buffer");
 
 undefine_key(content_buffer_normal_keymap, "g");
-define_key(content_buffer_normal_keymap, "g-t", "buffer-next");
-define_key(content_buffer_normal_keymap, "g-T", "buffer-previous");
+define_key(content_buffer_normal_keymap, "g t", "buffer-next");
+define_key(content_buffer_normal_keymap, "g T", "buffer-previous");
+
+undefine_key(content_buffer_normal_keymap, "C-n");
+undefine_key(content_buffer_normal_keymap, "C-p");
+define_key(content_buffer_normal_keymap, "j", "cmd_scrollLineDown");
+define_key(content_buffer_normal_keymap, "k", "cmd_scrollLineUp");
 
 define_key(default_global_keymap, "U", "revive-buffer");
