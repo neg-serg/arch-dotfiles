ea312ca86e5f8f6a752cd96a06a105b01f603537
Neg <serg.zorg@gmail.com>
2018-01-11 14:05:33 +0300

    [conkeror] add tinyurl command

---
+++ conkerorrc/.conkerorrc/tinyurl.js
@@ -0,0 +1,18 @@
+
+define_browser_object_class("tinyurl",
+    "Get a tinyurl for the current page",
+    function (I, prompt) {
+        check_buffer(I.buffer, content_buffer);
+        let createurl = 'http://tinyurl.com/api-create.php?url=' +
+            encodeURIComponent(
+                load_spec_uri_string(
+                    load_spec(I.buffer.top_frame)));
+        try {
+            var content = yield send_http_request(
+                load_spec({uri: createurl}));
+            yield co_return(content.responseText);
+        } catch (e) { }
+    });
+
+define_key(content_buffer_normal_keymap, "* q", "browser-object-tinyurl");
+
