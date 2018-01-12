d39b2470466cddafc552373f3c4872a109337dda
Neg <serg.zorg@gmail.com>
2018-01-12 13:01:12 +0300

    [conkeror] update css

--- conkerorrc/.conkerorrc/stylesheets.js
+++ conkerorrc/.conkerorrc/stylesheets.js
@@ -1,5 +1,4 @@
-// conkerorrc_dir = get_pref('conkeror.rcfile');
-site_css_dir = conkerorrc_dir + "/home/neg/.dotfiles/conkerorrc/.conkerorrc/css/";
+site_css_dir = "/home/neg/.dotfiles/conkerorrc/.conkerorrc/css/";
 
 function site_css(filename, url_prefixes) {
     var styles = read_text_file(make_file(site_css_dir+filename+".css"));
@@ -15,4 +14,3 @@ site_css("vk"    ,["https://vk.com"]);
 // site_css("mobile-twitter" ,["https://mobile.twitter.com"]);
 // site_css("facebook"       ,["https://www.facebook.com"]);
 // site_css("moodle-iie" ,["http://iie.fing.edu.uy/cursos"]);
-
