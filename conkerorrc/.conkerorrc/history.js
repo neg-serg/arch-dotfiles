5303bba39cfe8356953daa8dac87cfd79aacbda7
Neg <serg.zorg@gmail.com>
2018-01-12 12:52:41 +0300

    [conkeror] cleanup history.js

--- conkerorrc/.conkerorrc/history.js
+++ conkerorrc/.conkerorrc/history.js
@@ -18,13 +18,3 @@ interactive("find-url-from-history-new-buffer",
             "Find a page from history in the current buffer",
             "find-url-new-buffer",
             $browser_object = browser_object_history_url);
-
-interactive("toggle-stylesheets",
-            "Toggle whether conkeror uses style sheets (CSS) for the " +
-            "current buffer.  It is sometimes useful to turn off style " +
-            "sheets when the web site makes obnoxious choices.",
-            function(I) {
-  var s = I.buffer.document.styleSheets;
-  for (var i = 0; i < s.length; i++)
-    s[i].disabled = !s[i].disabled;
-});
