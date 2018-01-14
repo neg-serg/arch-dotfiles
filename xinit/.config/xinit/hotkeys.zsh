eed5bfbb73fd6f49356afe0d625157c229f85ed1
Neg <serg.zorg@gmail.com>
2018-01-15 01:57:29 +0300

    [xinit] fix redirection

--- xinit/.config/xinit/hotkeys.zsh
+++ xinit/.config/xinit/hotkeys.zsh
@@ -1,7 +1,8 @@
+#!/bin/zsh
 USE_SXHKD=${1:-true}
 if [[ $USE_SXHKD ]]; then
     rm -f "${SXHKD_FIFO}" && mkfifo "${SXHKD_FIFO}" && \
-        nexec sxhkd && sxhkd -s "${SXHKD_FIFO}" > /dev/null &
+        nexec sxhkd && sxhkd -s "${SXHKD_FIFO}" 2> /dev/null &
 else
     xbindkeys -p -n > /tmp/xbindkeys$$.log
 fi
