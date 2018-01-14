f96374262c1792a9a3c1cfe6794665ffab87b153
Neg <serg.zorg@gmail.com>
2018-01-15 01:56:36 +0300

    [conkeror] cleanup stylesheets.js

--- conkerorrc/.conkerorrc/stylesheets.js
+++ conkerorrc/.conkerorrc/stylesheets.js
@@ -2,7 +2,7 @@ site_css_dir = "/home/neg/.dotfiles/conkerorrc/.conkerorrc/css/";
 
 function site_css(filename, url_prefixes) {
     var styles = read_text_file(make_file(site_css_dir+filename+".css"));
-    var stylesheet = make_css_data_uri([styles], $url_prefixes = url_prefixes);
+    var stylesheet = make_css_data_uri([styles]);
     register_user_stylesheet(stylesheet);
     interactive("toggle-"+filename,"", function() {
                     unregister_user_stylesheet(stylesheet);
@@ -10,7 +10,3 @@ function site_css(filename, url_prefixes) {
 }
 
 site_css("vk"    ,["https://vk.com"]);
-// site_css("hacker-news"    ,["http://news.ycombinator.com"]);
-// site_css("mobile-twitter" ,["https://mobile.twitter.com"]);
-// site_css("facebook"       ,["https://www.facebook.com"]);
-// site_css("moodle-iie" ,["http://iie.fing.edu.uy/cursos"]);
