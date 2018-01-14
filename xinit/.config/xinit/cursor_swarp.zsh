9896da2f5ea77387357e785549e8355d0eb80569
Neg <serg.zorg@gmail.com>
2018-01-15 01:57:58 +0300

    [xinit] can be executed

--- xinit/.config/xinit/cursor_swarp.zsh
+++ xinit/.config/xinit/cursor_swarp.zsh
@@ -1,3 +1,4 @@
+#!/bin/zsh
 local width_x11=$(xrandr -q |awk '/Screen/{print $8}')
 local height_x11=$(xrandr -q | awk '/Screen/{print $10}' | awk -F"," '{print $1}')
 swarp ${width_x11} ${height_x11}
--- xinit/.config/xinit/keyboard_and_mouse.zsh
+++ xinit/.config/xinit/keyboard_and_mouse.zsh
@@ -1,3 +1,10 @@
+#!/bin/zsh
+xmodmap ${XDG_CONFIG_HOME}/keymaps/xmodmaprc
+unclutter --fork --timeout 1
+
+xset m 0 0 # disable mouse acceleration
+xset -b r rate 250 50
+
 setxkbmap \
     -option keypad:pointerkeys \
     -layout 'us,ru' \
@@ -6,10 +13,3 @@ setxkbmap \
     -option ctrl:nocaps
 
 xhost +localhost +local: +si:localuser:$(id -un) > /dev/null
-xset m 0 0 # disable mouse acceleration
-xset -b r rate 250 50
-
-[[ -f ${XDG_CONFIG_HOME}/keymaps/xmodmaprc ]] && \
-    inpath xmodmap && nexec xmodmap && xmodmap ${XDG_CONFIG_HOME}/keymaps/xmodmaprc
-
-inpath unclutter && nexec unclutter && unclutter --fork --timeout 1 &
