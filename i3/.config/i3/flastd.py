8ca7dbbcc614a9efa2d2fd56c9f7319e595ae74b
Neg <serg.zorg@gmail.com>
2018-01-20 22:03:04 +0300

    [i3] use local i3

--- i3/.config/i3/flastd.py
+++ i3/.config/i3/flastd.py
@@ -26,7 +26,8 @@ max_win_history_ = 10
 
 class flast(SingletonMixin):
     def __init__(self):
-        self.window_list = i3.get_tree().leaves()
+        self.i3 = i3ipc.Connection()
+        self.window_list = self.i3.get_tree().leaves()
         self.prev_time = 0
         self.curr_time = 0
 
@@ -48,13 +49,13 @@ class flast(SingletonMixin):
 
     def alt_tab(self, timer=0.05):
         self.curr_time = time.time()
-        windows = set(w.id for w in i3.get_tree().leaves())
+        windows = set(w.id for w in self.i3.get_tree().leaves())
         for wid in self.window_list[1:]:
             if wid not in windows:
                 self.window_list.remove(wid)
             else:
                 if self.curr_time - self.prev_time > timer:
-                    i3.command('[con_id=%s] focus' % wid)
+                    self.i3.command('[con_id=%s] focus' % wid)
                     self.prev_time = self.curr_time
                     self.curr_time = time.time()
                 break
@@ -71,7 +72,7 @@ class flast(SingletonMixin):
 
     def go_back_if_nothing(self, i3, event):
         con=event.container
-        focused_=self.i3.get_tree().find_focused()
+        focused_=i3.get_tree().find_focused()
         if not len(find_visible_windows(get_windows_on_ws())) \
         and "[pic]" in focused_.workspace().name:
             self.alt_tab(0)
