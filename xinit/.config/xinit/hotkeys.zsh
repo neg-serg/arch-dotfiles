cfd88df483d2071340af6b80faee26929cdf6fba
Neg <serg.zorg@gmail.com>
2018-01-27 17:58:45 +0300

    [sxhkd] make sxkhd run-script more independent

--- xinit/.config/xinit/hotkeys.zsh
+++ xinit/.config/xinit/hotkeys.zsh
@@ -1,8 +1,11 @@
 #!/bin/zsh
 USE_SXHKD=${1:-true}
-if [[ $USE_SXHKD ]]; then
-    rm -f "${SXHKD_FIFO}" && mkfifo "${SXHKD_FIFO}" && \
-        nexec sxhkd && sxhkd -s "${SXHKD_FIFO}" 2> /dev/null &
+SXHKD_FIFO=${2:-/tmp/sxhkd_fifo}
+
+if [[ ${USE_SXHKD} ]]; then
+    rm -f "${SXHKD_FIFO}" 
+    mkfifo "${SXHKD_FIFO}"
+    [[ ! $(pidof sxhkd) ]] && sxhkd -s "${SXHKD_FIFO}" 2> /dev/null &
 else
     xbindkeys -p -n > /tmp/xbindkeys$$.log
 fi
