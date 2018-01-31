e5876c93b4ae7cb1e59e0fc052739dabfce483a2
Neg <serg.zorg@gmail.com>
2018-01-31 21:53:19 +0300

    [i3] add config autoreload with inotify

--- i3/.config/i3/runner.py
+++ i3/.config/i3/runner.py
@@ -11,59 +11,56 @@ year :: 2018
 """
 
 import i3ipc
-import re
-import errno
 import os
-import time
-
-from sys import exit
-from docopt import docopt
-from threading import Thread
+from threading import Thread, Event
 import importlib
-
+import logging
 import inotify.adapters
-
+import gevent.event
 from i3gen import *
+import atexit
 
-import i3ipc
+class Listner():
+    def __init__(self):
+        self.ev = Event()
 
-class RestartPls(Exception):
-    pass
+    def configure_logging(self):
+        log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter(log_format)
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
 
-def configure_logging():
-    import logging
+        return logger
 
-    log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    formatter = logging.Formatter(log_format)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    def watch(self, watch_dir, file_path, logger, watched_inotify_event="IN_MODIFY"):
+        watch_dir=watch_dir.encode()
+        i=inotify.adapters.Inotify()
+        i.add_watch(watch_dir)
 
-    return logger
+        try:
+            for event in i.event_gen():
+                if event is not None:
+                    (header, type_names, watch_path, filename) = event
+                    if filename.decode('utf-8') == file_path and watched_inotify_event in type_names:
+                        logger.info("WD=(%d) MASK=(%d) COOKIE=(%d) LEN=(%d) MASK->NAMES=%s "
+                                    "WATCH-PATH=[%s] FILENAME=[%s]",
+                                    header.wd, header.mask, header.cookie, header.len, type_names,
+                                    watch_path.decode('utf-8'), filename.decode('utf-8'))
+                        self.ev.set()
+                        i.remove_watch(watch_dir)
+                        break
+        finally:
+            i.remove_watch(watch_dir)
 
-def watch(watch_dir, file_path, logger, watched_inotify_event="IN_MODIFY"):
-    watch_dir=watch_dir.encode()
-    i=inotify.adapters.Inotify()
-    i.add_watch(watch_dir)
-
-    try:
-        for event in i.event_gen():
-            if event is not None:
-                (header, type_names, watch_path, filename) = event
-                if filename.decode('utf-8') == file_path and watched_inotify_event in type_names:
-                    logger.info("WD=(%d) MASK=(%d) COOKIE=(%d) LEN=(%d) MASK->NAMES=%s "
-                                "WATCH-PATH=[%s] FILENAME=[%s]",
-                                header.wd, header.mask, header.cookie, header.len, type_names,
-                                watch_path.decode('utf-8'), filename.decode('utf-8'))
-                    raise RestartPls
-    finally:
-        i.remove_watch(watch_dir)
-
-def main():
-    try:
-        argv = docopt(__doc__, version='i3 Daemons runner')
+    def main(self):
+        def cleanup():
+            d = currm["manager"].daemons[mod]
+            if os.path.exists(d.fifos[mod]):
+                os.remove(d.fifos[mod])
+        atexit.register(cleanup)
 
         ScriptDaemonsMap={
             'circle': { "instance": None, "manager": None, },
@@ -71,11 +68,12 @@ def main():
             'flast': { "instance": None, "manager": None, }
         }
 
-        threads=[]
-
         user_name=os.environ.get("USER", "neg")
         xdg_config_path=os.environ.get("XDG_CONFIG_HOME", "/home/" + user_name + "/.config/")
-        Thread(target=watch, args=(xdg_config_path+'/i3', 'circle_conf.py', configure_logging(), )).start()
+        for mod in ScriptDaemonsMap.keys():
+            inotify_thread=Thread(target=self.watch, args=(xdg_config_path+'/i3', mod + '_conf.py', self.configure_logging(), ))
+            inotify_thread.setDaemon(False)
+            inotify_thread.start()
 
         for mod in ScriptDaemonsMap.keys():
             currm=ScriptDaemonsMap[mod]
@@ -83,26 +81,27 @@ def main():
             currm["manager"]=daemon_manager.instance()
             currm["manager"].add_daemon(mod)
 
-            def cleanup():
-                d = currm["manager"].daemons[mod]
-                if os.path.exists(d.fifos[mod]):
-                    os.remove(d.fifos[mod])
-
-            import atexit
-            atexit.register(cleanup)
-
             th=Thread(target=currm["manager"].daemons[mod].mainloop, args=(currm["instance"], mod, ))
+            th.setDaemon(True)
             th.start()
-            threads.append(th)
 
+        # you should bypass method itself, no return value
         for mod in ScriptDaemonsMap:
-            # you should bypass method itself, no return value
             th=Thread(target=ScriptDaemonsMap[mod]["instance"].i3.main)
+            th.setDaemon(True)
             th.start()
-            threads.append(th)
-    except RestartPls:
-        print("Fuck yeah!")
-        main()
+
+        if self.ev.wait():
+            raise RestartMe
+
+class RestartMe(Exception):
+    pass
 
 if __name__ == '__main__':
-    main()
+    while True:
+        listner = Listner()
+        try:
+            print("STARTED")
+            listner.main()
+        except RestartMe:
+            del listner
