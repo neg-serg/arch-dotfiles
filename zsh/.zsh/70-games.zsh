fec261bb831d006a9c2c5d604bb671b8d1405beb
Neg <serg.zorg@gmail.com>
2018-02-25 17:30:04 +0300

    [zsh] update games

--- zsh/.zsh/70-games.zsh
+++ zsh/.zsh/70-games.zsh
@@ -3,21 +3,11 @@ local wine_pref_="${HOME}/.wine"
 IFS= local wine_progs_="$(echo ${wine_pref_}'/drive_c/Program Files (x86)')"
 
 alias wine="WINEDEBUG=-all LC_ALL=ru_RU.utf8 LC_COLLATE=C LC_MESSAGES=C wine"
-alias crossover="LANG=ru_RU.utf8 setarch i386 -3 /mnt/home/crossover/bin/crossover"
+alias crossover="LANG=ru_RU.utf8 setarch i386 -3 /zero/crossover/bin/crossover"
 
 function steam_wine(){
     cd "${wine_progs_}/Steam"
     WINEDEBUG=-all LC_ALL=ru_RU.utf8 LC_COLLATE=C LC_MESSAGES=C wine Steam.exe
-}
-
-function sls(){
-    steamcmd '+apps_installed +quit' |\
-        awk '/AppID/ {
-                id = $2;
-                name = substr($0, index($0, " : ") + 3);
-                sub(" : .*", "", name);
-                print id ": " name;
-            }'
 }
 
 function bnet(){
@@ -44,7 +34,7 @@ function q3(){
         +set r_customheight "1200" \
         +setr_customwidth "1920" \
         +set r_mode "-1" \
-        +set cg_fov "120"
+        +set cg_fov "100"
     unsetopt shwordsplit
 }
 
