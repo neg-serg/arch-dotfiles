febac76118c1aaa91b40775740b1b78b792e6005
Neg <serg.zorg@gmail.com>
2018-02-26 20:20:18 +0300

    [oni] update config

--- oni/.oni/config.js
+++ oni/.oni/config.js
@@ -1,20 +1,10 @@
-// For more information on customizing Oni,
-// check out our wiki page:
 // https://github.com/onivim/oni/wiki/Configuration
 
 const activate = oni => {
     console.log("config activated")
-
-    // Input
-    //
-    // Add input bindings here:
-    //
     oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
-
-    //
-    // Or remove the default bindings here by uncommenting the below line:
-    //
     oni.input.unbind("<c-p>")
+    oni.input.bind("<f8>", "markdown.togglePreview")
     oni.commands.executeCommand("sidebar.toggle")
 }
 
@@ -25,32 +15,28 @@ const deactivate = () => {
 module.exports = {
     activate,
     deactivate,
-    //add custom config here, such as
-
+    "wildmenu.mode": false,
+    "commandline.icons": true,
+    "commandline.mode": false,
+    "editor.backgroundImageUrl": "/home/neg/pic/wl/HeuanqL.jpg",
+    "editor.backgroundOpacity": 0.9,
+    "editor.completions.enabled": true,
+    "editor.cursorLine": true,
+    "editor.fontFamily": "Iosevka Term Medium",
+    "editor.fontSize": "19pt",
+    "editor.quickOpen.execCommand": true,
+    "editor.scrollBar.visible": false,
+    "oni.hideMenu": true,
+    "oni.loadInitVim": true,
+    "oni.useDefaultConfig": false, 
+    "oni.useExternalPopupMenu": true,
+    "statusbar.enabled": false,
+    "statusbar.fontSize": "14pt",
+    "tabs.mode": "hidden",
+    "ui.animations.enabled": true,
     "ui.colorscheme": "neg",
     "ui.fontFamily": "Iosevka Term Medium",
     "ui.fontSize": "18pt",
-    "oni.useDefaultConfig": false, 
-    "oni.hideMenu": true,
-    "oni.useExternalPopupMenu": true,
-    "oni.loadInitVim": true,
-    "editor.fontSize": "19pt",
-    "editor.fontFamily": "Iosevka Term Medium",
-    "editor.completions.enabled": true,
-    "editor.backgroundImageUrl": "/home/neg/pic/wl/HeuanqL.jpg",
-    "editor.backgroundOpacity": 0.9,
-    "commandline.mode": false,
-    "commandline.icons": true,
-    "wildmenu.mode": false,
-    "statusbar.enabled": false,
-    "statusbar.fontSize": "14pt",
-    "editor.scrollBar.visible": false,
-    "editor.quickOpen.execCommand": true,
-    "editor.cursorLine": true,
-    "tabs.mode": "hidden",
-
-    // UI customizations
-    "ui.animations.enabled": true,
     "ui.fontSmoothing": "auto",
 }
 
