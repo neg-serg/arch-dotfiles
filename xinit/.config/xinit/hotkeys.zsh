8158f60dfc84b8a5234d49864b9fa8abdf8478a2
Neg <serg.zorg@gmail.com>
2018-03-07 16:58:35 +0300

    [xinit] rm wm.zsh

--- xinit/.config/xinit/hotkeys.zsh
+++ xinit/.config/xinit/hotkeys.zsh
@@ -3,7 +3,7 @@ USE_SXHKD=${1:-true}
 SXHKD_FIFO=${2:-/tmp/sxhkd_fifo}
 
 if [[ ${USE_SXHKD} ]]; then
-    rm -f "${SXHKD_FIFO}" 
+    rm -f "${SXHKD_FIFO}"
     mkfifo "${SXHKD_FIFO}"
     [[ ! $(pidof sxhkd) ]] && sxhkd -s "${SXHKD_FIFO}" 2> /dev/null &
 else
