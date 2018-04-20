de83ac8f03caffccdb7da57034f811af60415381
Neg <serg.zorg@gmail.com>
2018-04-20 17:50:55 +0300

    [oni] update config

--- oni/.config/oni/config.js
+++ oni/.config/oni/config.js
@@ -1,38 +1,47 @@
 // https://github.com/onivim/oni/wiki/Configuration
 const activate = oni => {
-    oni.input.unbind("<c-p>")
-    oni.input.bind("<f8>", "markdown.togglePreview")
+    oni.input.unbind("<c-p>");
+    oni.input.bind("<f8>", "markdown.togglePreview");
     oni.input.bind(["<enter>", "<tab>"], "contextMenu.select");
+    oni.input.bind("<c-enter>", () => oni.recorder.takeScreenshot());
 }
 
 module.exports = {
     activate,
-    "autoClosingPairs.enabled": false, // disable autoclosing pairs
-    "commandline.icons": true,
-    "sidebar.enabled": false,
-    "commandline.mode": false, // Do not override commandline UI
+    "autoClosingPairs.enabled": false, // Disable oni's autoclosing pairs.
+    "commandline.icons": true, // Commandline icons.
+    "sidebar.enabled": false, // Disable sidebar.
+    "commandline.mode": false, // Do not override commandline UI.
     "editor.backgroundImageUrl": "/home/neg/pic/wl/wallpaper-2519516.jpg",
-    "editor.backgroundOpacity": 0.88,
-    "editor.completions.mode": 'oni',
-    "editor.cursorLine": true,
-    "editor.fontFamily": "Iosevka Term Medium",
-    "editor.fontSize": "20pt",
-    "editor.quickOpen.execCommand": false,
-    "editor.scrollBar.visible": false,
-    "oni.hideMenu": true, // Hide default menu, can be opened with <alt>
-    "oni.loadInitVim": true, // Load user's init.vim
-    "oni.useDefaultConfig": false, // Do not load Oni's init.vim
-    "oni.useExternalPopupMenu": true,
-    "statusbar.enabled": false, // Disable statusbar
-    "statusbar.fontSize": "16pt", // Statusbar fontsize
-    "tabs.mode": "native", // tabs behave like vim
-    "tabs.wrap": true, // wrap tabs
-    "tabs.height" : "1.2em",
-    "ui.animations.enabled": true,
-    "ui.colorscheme": "neg",
-    "ui.fontFamily": "Iosevka Term Medium",
-    "ui.fontSize": "18pt",
-    "ui.fontSmoothing": "auto",
-    "wildmenu.mode": false, // Do not override wildmenu UI
+    "editor.backgroundOpacity": 0.88, // Dark enough.
+    "editor.completions.mode": 'oni', // VSCode-like completion.
+    "editor.cursorLine": false, // Disables cursor line highlight.
+    "editor.cursorColumn": false, // Disables cursor column highlight. 
+    "editor.cursorLineOpacity": 0.5, // Defines opacity of cursor line highlight.
+    "editor.cursorColumnOpacity": 0.5, // Defines opacity of cursor column highlight.
+    "editor.fontFamily": "Iosevka Term Medium", // Editor font.
+    "editor.textMateHighlighting.enabled": true, // TextMate highlighting.
+    "editor.fontSize": "20pt", // Font size.
+    "editor.fontLigatures": true, // Use ligatures in editor.
+    "editor.quickOpen.execCommand": 'fzf', // FZF as default src to populate fuzzy finder.
+    "editor.scrollBar.visible": false, // Hide scrollbar.
+    "editor.errors.slideOnFocus": true, // Enables showing details when cursor is over an error.
+    "oni.hideMenu": true, // Hide default menu, can be opened with <alt>.
+    "oni.loadInitVim": true, // Load user's init.vim.
+    "oni.useDefaultConfig": false, // Do not load Oni's init.vim.
+    "oni.useExternalPopupMenu": false, // Use oni GUI popup menu.
+    "statusbar.enabled": false, // Disable statusbar.
+    "statusbar.fontSize": "16pt", // Statusbar fontsize.
+    "tabs.mode": "native", // Tabs behave like vim.
+    "tabs.wrap": true, // Wrap tabs.
+    "tabs.height" : "1.2em", // Tabs height.
+    "ui.animations.enabled": true, // Use animations.
+    "ui.colorscheme": "neg", // My custom colorscheme.
+    "ui.fontFamily": "Iosevka Term Medium", // GUI font family.
+    "ui.fontSize": "14pt", // GUI font size.
+    "ui.fontSmoothing": "auto", // Auto font smooth.
+    "wildmenu.mode": false, // Do not override vim's native wildmenu UI.
+    "learning.enabled": false, // Disable learning.
+    "experimental.markdownPreview.enabled": true, // Experimental Markdown preview.
 }
 
