eaca7dfa299ef5804e8301faffa33afe02daa492
Neg <serg.zorg@gmail.com>
2018-09-05 12:50:37 +0300

    [zsh] games cleanup

--- zsh/.zsh/70-games.zsh
+++ zsh/.zsh/70-games.zsh
@@ -10,17 +10,6 @@ function steam_wine(){
     WINEDEBUG=-all LC_ALL=ru_RU.utf8 LC_COLLATE=C LC_MESSAGES=C wine Steam.exe
 }
 
-function bnet(){
-    local pref=""
-    if [[ $1 =~ ".*32" ]]; then
-        pref="setarch i386 -3 "
-        zwrap "set to 32 bit"
-    fi
-    cd "${wine_progs_}/Battle.net"
-    eval WINEDEBUG=-all LC_ALL=ru_RU.utf8 LC_COLLATE=C LC_MESSAGES=C \
-    ${pref} wine ./Battle.net.exe &
-}
-
 function doom(){
     cd "${wine_progs_}/Steam/steamapps/common/DOOM/"
     local doom_path="DOOMx64vk.exe"
@@ -36,10 +25,4 @@ function q3(){
         +set r_mode "-1" \
         +set cg_fov "100"
     unsetopt shwordsplit
-}
-
-function steam(){
-    SDL_AUDIODRIVER="alsa" \
-    LD_PRELOAD='/usr/lib/libSDL2-2.0.so.0.4.0 /usr/lib/libSDL_sound-1.0.so.1.0.2' \
-    steam-native
 }
