6f1d32e0e91bc522c2389de26e2e4b638d63848e
Neg <serg.zorg@gmail.com>
2018-11-09 18:36:08 +0300

    [zsh] remove csound

--- zsh/.zsh/05-cmds.zsh
+++ zsh/.zsh/05-cmds.zsh
@@ -526,9 +526,6 @@ alias up="rtv -s unixporn"
 alias taco='curl -L git.io/taco'
 alias starwars='telnet towel.blinkenlights.nl'
 
-#--[ Csound ]--------------
-alias ocean='play -q -n -c 2 synth 0 noise 100 noise 100 lowpass 100 gain 12 tremolo 0.125 80;'
-
 #--[ gdb ]-----------------
 alias gdb8="gdb -x ${XDG_CONFIG_HOME}/gdb/gdbinit8.gdb"
 alias gdbv="gdb -x ${XDG_CONFIG_HOME}/gdb/voltron.gdb"
