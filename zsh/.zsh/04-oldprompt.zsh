c4e2fb446bf7b8455c52f0dc15e1574ffa07324c
Neg <serg.zorg@gmail.com>
2018-12-24 22:44:22 +0300

    [zsh] fix var shadowing

--- zsh/.zsh/04-oldprompt.zsh
+++ zsh/.zsh/04-oldprompt.zsh
@@ -7,13 +7,13 @@ local snd=">"
 
 local brackets_cfg=5
 case brackets_cfg in
-    1) lhs="%F{25}⟬%F{4}<" rhs="⧔" ;;
-    2) lhs="❬" rhs="❭" ;;
-    3) lhs="❲" rhs="❳" ;;
-    4) lhs="%F{25}⟬%F{4}<" rhs="" ;;
-    5) lhs="%F{4}⟬" rhs="" ;;
-    6) lhs="❬" rhs="❭"  ;;
-    7) lhs="❲" rhs="❳" ;;
+    1) lhs_="%F{25}⟬%F{4}<" rhs_="⧔" ;;
+    2) lhs_="❬" rhs_="❭" ;;
+    3) lhs_="❲" rhs_="❳" ;;
+    4) lhs_="%F{25}⟬%F{4}<" rhs_="" ;;
+    5) lhs_="%F{4}⟬" rhs_="" ;;
+    6) lhs_="❬" rhs_="❭"  ;;
+    7) lhs_="❲" rhs_="❳" ;;
 esac
 
 SPROMPT="%F{7}Correct: %F{4}%R%f %F{7}-> %F{2}%r%F{7} [nyae]? %f"
@@ -23,7 +23,7 @@ prompt_end="∣%F{25}${fst}%B%F{26}${snd}%B%f "
 _neg_user_pretok="${DARK_BLUE}${lhs}%f"
 
 function setup_prompt { 
-    export PS1="${_neg_user_pretok}%40<..<$(lhs="${lhs}" rhs="${rhs}" prompt_end="${prompt_end}" ${ZSH}/neg-prompt)" 
+    export PS1="${_neg_user_pretok}%40<..<$(lhs="${lhs_}" rhs="${rhs_}" prompt_end="${prompt_end}" ${ZSH}/neg-prompt)" 
 }
 
 function precmd {
