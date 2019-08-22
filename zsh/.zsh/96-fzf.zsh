341cd27d79f5c45b6428dbf4ceb122c103bdb78e
Neg <serg.zorg@gmail.com>
2019-08-22 05:04:12 +0300

    [zsh] move settings to zshenv

--- zsh/.zsh/96-fzf.zsh
+++ zsh/.zsh/96-fzf.zsh
@@ -1,10 +1,2 @@
-export SKIM_DEFAULT_OPTIONS="${SKIM_DEFAULT_OPTS} --color=16 --ansi --tabstop=4 "
-export SKIM_DEFAULT_COMMAND='rg --files --hidden --follow -g'
-
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow'
-export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
-export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color=16"
-export FZF_TMUX=1
-
 source /usr/share/fzf/completion.zsh
 source /usr/share/fzf/key-bindings.zsh
