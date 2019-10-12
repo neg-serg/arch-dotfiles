f7587e3ec2cbd72e86524f0e10795045942f5f74
Neg <serg.zorg@gmail.com>
2019-10-12 08:00:12 +0300

    [xinit] cleanup hotkeys

--- xinit/.config/xinit/hotkeys.zsh
+++ xinit/.config/xinit/hotkeys.zsh
@@ -1,11 +1,6 @@
 #!/bin/zsh
-USE_SXHKD=${1:-true}
 SXHKD_FIFO=${2:-/tmp/sxhkd_fifo}
 
-if [[ ${USE_SXHKD} ]]; then
-    rm -f "${SXHKD_FIFO}"
-    mkfifo "${SXHKD_FIFO}"
-    [[ ! $(pidof sxhkd) ]] && sxhkd -s "${SXHKD_FIFO}" 2> /dev/null &
-else
-    xbindkeys -p -n > /tmp/xbindkeys$$.log
-fi
+rm -f "${SXHKD_FIFO}"
+mkfifo "${SXHKD_FIFO}"
+[[ ! $(pidof sxhkd) ]] && sxhkd -s "${SXHKD_FIFO}" 2> /dev/null &
