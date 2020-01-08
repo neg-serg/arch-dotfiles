875c44b3421823130d5f598cdb28edb1ed7b4672
Neg <serg.zorg@gmail.com>
2020-01-08 13:15:01 +0300

    [term-colorschemes] cleanup solarized

--- term-colorschemes/.config/colorschemes/solarized-dark.sh
+++ term-colorschemes/.config/colorschemes/solarized-dark.sh
@@ -1,26 +1,19 @@
-# Solarized colorscheme "dark"
-#
-# See the Solarized homepage for details:
-# http://ethanschoonover.com/solarized
-#
-                   # SOLARIZED HEX     16/8 TERMCOL   L*A*B     
-                   # --------- ------- ---- --------- ----------
- base03="#002b36"  # base03    #002b36  8/4 brblack   15 -12 -12
- base02="#073642"  # base02    #073642  0/4 black     20 -12 -12
- base01="#586e75"  # base01    #586e75 10/7 brgreen   45 -07 -07
- base00="#657b83"  # base00    #657b83 11/7 bryellow  50 -07 -07
-  base0="#839496"  # base0     #839496 12/6 brblue    60 -06 -03
-  base1="#93a1a1"  # base1     #93a1a1 14/4 brcyan    65 -05 -02
-  base2="#eee8d5"  # base2     #eee8d5  7/7 white     92 -00  10
-  base3="#fdf6e3"  # base3     #fdf6e3 15/7 brwhite   97  00  10
- yellow="#b58900"  # yellow    #b58900  3/3 yellow    60  10  65
- orange="#cb4b16"  # orange    #cb4b16  9/3 brred     50  50  55
-    red="#dc322f"  # red       #dc322f  1/1 red       50  65  45
-magenta="#d33682"  # magenta   #d33682  5/5 magenta   50  65 -05
- violet="#6c71c4"  # violet    #6c71c4 13/5 brmagenta 50  15 -45
-   blue="#268bd2"  # blue      #268bd2  4/4 blue      55 -10 -45
-   cyan="#2aa198"  # cyan      #2aa198  6/6 cyan      60 -35 -05
-  green="#859900"  # green     #859900  2/2 green     60 -20  65
+base03="#002b36" 
+base02="#073642" 
+base01="#586e75" 
+base00="#657b83" 
+base0="#839496" 
+base1="#93a1a1" 
+base2="#eee8d5" 
+base3="#fdf6e3" 
+yellow="#b58900" 
+orange="#cb4b16" 
+red="#dc322f" 
+magenta="#d33682" 
+violet="#6c71c4" 
+blue="#268bd2" 
+cyan="#2aa198" 
+green="#859900" 
 
 background="$base03"
 foreground="$base0"
@@ -30,26 +23,28 @@ mouse_foreground="$base1"
 highlight="$base02"
 border="$base03"
 
- color0="$base02"  # black
- color8="$base03"  # brblack
+color0="$base02" 
+color8="$base03" 
 
- color1="$red"     # red
- color9="$orange"  # brred
+color1="$red"    
+color9="$orange" 
 
- color2="$green"   # green
-color10="$base01"  # brgreen
+color2="$green"  
+color10="$base01" 
 
- color3="$yellow"  # yellow
-color11="$base00"  # bryellow
+color3="$yellow" 
+color11="$base00" 
 
- color4="$blue"    # blue
-color12="$base0"   # brblue
+color4="$blue"   
+color12="$base0"  
 
- color5="$magenta" # magenta
-color13="$violet"  # brmagenta
+color5="$magenta"
+color13="$violet" 
 
- color6="$cyan"    # cyan
-color14="$base1"   # brcyan
+color6="$cyan"   
+color14="$base1"  
 
- color7="$base2"   # white
-color15="$base3"   # brwhite
+color7="$base2"  
+color15="$base3"  
+
+source $XDG_CONFIG_HOME/colorschemes/new256color.sh
