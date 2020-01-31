ea33d2d5cff8b2bfddc4d00c36b7710ce61cfadc
Neg <serg.zorg@gmail.com>
2020-01-31 09:35:25 +0300

    [zsh] show vpn

--- zsh/.config/zsh/.p10k.zsh
+++ zsh/.config/zsh/.p10k.zsh
@@ -81,7 +81,7 @@
     vim_shell               # vim shell indicator (:sh)
     midnight_commander      # midnight commander shell (https://midnight-commander.org/)
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
-    # vpn_ip                # virtual private network indicator
+    vpn_ip                  # virtual private network indicator
     # load                  # CPU load
     # disk_usage            # disk usage
     # ram                   # free RAM
