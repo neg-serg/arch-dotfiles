91f24e5279f354e37df9449292a30f4433ed8434
Neg <serg.zorg@gmail.com>
2020-02-19 04:13:33 +0300

    [zsh] move most of exports to zshenv

--- zsh/.config/zsh/.zshenv
+++ zsh/.config/zsh/.zshenv
@@ -1,6 +1,8 @@
 [[ $(readlink -e ~/tmp) == "" ]] && rm -f ~/tmp
 [[ ! -L ${HOME}/tmp ]] && { rm -f ~/tmp && tmp_loc=$(mktemp -d) && ln -fs "${tmp_loc}" ${HOME}/tmp }
 
+export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
+
 export XDG_VTNR=1
 systemctl --user set-environment XDG_VTNR=1
 
@@ -16,21 +18,18 @@ export XDG_PICTURES_DIR="${HOME}/pic"
 export XDG_VIDEOS_DIR="${HOME}/vid"
 export XDG_PUBLICSHARE_DIR="${HOME}/1st_level/upload/share"
 
-export ZSHDIR="${XDG_CONFIG_HOME}/zsh"
-export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
-
 unset SSH_ASKPASS
 
-export PYTHONIOENCODING='utf-8'
 export GREP_COLOR='37;45'
 export GREP_COLORS='ms=0;32:mc=1;33:sl=:cx=:fn=1;32:ln=1;36:bn=36:se=1;30'
 
-export INPUTRC=${XDG_CONFIG_HOME}/inputrc
 export BROWSER="firefox"
 export LESSCHARSET=UTF-8
 export COLORTERM="yes"
-export LS_COLORS GREP_COLORS
 export WORDCHARS='*?_-.[]~&;!#$%^(){}<>~` '
+
+export VAGRANT_HOME="/zero/vagrant"
+export PERLBREW_ROOT=${HOME}/.perl5
 
 export MPV_HOME="${XDG_CONFIG_HOME}/mpv"
 export KEYTIMEOUT=5 # allow to use ,<key> more fast
@@ -42,8 +41,15 @@ export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswi
 _SILENT_JAVA_OPTIONS="${_JAVA_OPTIONS}"
 unset _JAVA_OPTIONS
 
+export GOPATH=/opt/go
+export STEAM_RUNTIME=1
+export AUTOPAIR_INHIBIT_INIT=1
+export QT_QPA_PLATFORMTHEME="qt5ct"
+
 export SXHKD_FIFO="/tmp/sxhkd_fifo"
 export SXHKD_SHELL="zsh"
+
+export AURDEST=$(readlink -f "${HOME}/tmp/pacaur")
 
 export FZF_DEFAULT_OPTS="--bind='ctrl-t:execute(/bin/nvim {})+abort'"
 if [[ ! -z ${DISPLAY} ]]; then
--- zsh/.config/zsh/03-exports.zsh
+++ zsh/.config/zsh/03-exports.zsh
@@ -1,14 +1,3 @@
-export XDG_CONFIG_HOME="${HOME}/.config"
-export XDG_DATA_HOME="${HOME}/.local/share"
-export XDG_CACHE_HOME="${HOME}/.cache"
-export XDG_DOWNLOAD_DIR="${HOME}/dw"
-export XDG_MUSIC_DIR="${HOME}/music"
-export XDG_DESKTOP_DIR="${HOME}/.local/desktop"
-export XDG_TEMPLATES_DIR="${HOME}/1st_level/templates"
-export XDG_DOCUMENTS_DIR="${HOME}/doc/"
-export XDG_PICTURES_DIR="${HOME}/pic"
-export XDG_VIDEOS_DIR="${HOME}/vid"
-export XDG_PUBLICSHARE_DIR="${HOME}/1st_level/upload/share"
 path_dirs=(
     /usr/bin
     ${HOME}/bin
@@ -17,21 +6,7 @@ path_dirs=(
     /opt/go/bin
     /home/neg/.cargo/bin
 )
-
-whence ruby >/dev/null && \
-    path_dirs+=$($(whence ruby) -e 'puts Gem.user_dir')/bin
-
 export PATH=${(j_:_)path_dirs}
-
-unset SSH_ASKPASS
-export VIDIR_EDITOR_ARGS='-c :set nolist | :set ft=vidir-ls'
-export AURDEST=$(readlink -f "${HOME}/tmp/pacaur")
-
-export SXHKD_FIFO="/tmp/sxhkd_fifo"
-export SXHKD_SHELL="zsh"
-
-export GREP_COLOR='37;45'
-export GREP_COLORS='ms=0;32:mc=1;33:sl=:cx=:fn=1;32:ln=1;36:bn=36:se=1;30'
 
 for q in nvim vim vis vi;
     { [[ -n ${commands}[(I)${q}] ]] \
@@ -48,12 +23,6 @@ for q in nvim vim vis vi; {
 alias less=${PAGER}
 alias zless=${PAGER}
 
-export INPUTRC="${XDG_CONFIG_HOME}/inputrc"
-export BROWSER="firefox"
-
-export VAGRANT_HOME="/zero/vagrant"
-export PERLBREW_ROOT=${HOME}/.perl5
-
 export LESSCHARSET=UTF-8
 export LESS_TERMCAP_mb="$(tput bold; tput setaf 2)" # begin blinking
 export LESS_TERMCAP_md="$(tput bold)"               # begin bold
@@ -63,8 +32,6 @@ export LESS_TERMCAP_se="$(tput sgr0)"               # end standout
 export LESS_TERMCAP_us="$(tput bold; tput setaf 2)" # begin underline
 export LESS_TERMCAP_ue="$(tput sgr0)"               # end underline
 
-export TEXINPUTS=".:${XDG_DATA_HOME}/texmf//:"
-
 export HISTFILE=${ZDOTDIR}/zsh_history
 export SAVEHIST=100000 # useful for setopt append_history
 export HISTSIZE=$(( $SAVEHIST * 1.10 ))
@@ -72,21 +39,8 @@ export HISTIGNORE="&:ls:[bf]g:exit:reset:clear:cd*:gs:gd"
 export HISTCONTROL=ignoreboth:erasedups # ignoreboth (= ignoredups + ignorespace)
 
 export TIMEFMT="[34m⟬[37m[34m⟬[37m[37m%J[34m⟭[37m[34m⟭[39m[34m⟬[37m%U[34m⟭[39m[34m⟬[37muser %S[34m⟭[39m[34m⟬[37msystem %P[34m⟭[39m[34m⟬[37mcpu %*E total[34m⟭[39m[34m[39m[34m⟬[37mMem: %M kb max[34m⟭[39m"
-export COLORTERM="yes"
-export LS_COLORS
 export WORDCHARS='*?_-.[]~&;!#$%^(){}<>~` '
 
-export MPV_HOME="${HOME}/.config/mpv"
-export GOPATH=/opt/go
-export KEYTIMEOUT=5 # allow to use ,<key> more fast
-export ESCDELAY=1
-
-export JAVA_FONTS=/usr/share/fonts/TTF
-export _JAVA_AWT_WM_NONREPARENTING=1
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-_SILENT_JAVA_OPTIONS="${_JAVA_OPTIONS}"
-
-export STEAM_RUNTIME=1
-export AUTOPAIR_INHIBIT_INIT=1
-
-export QT_QPA_PLATFORMTHEME="qt5ct"
+export LS_COLORS
+export GREP_COLOR='37;45'
+export GREP_COLORS='ms=0;32:mc=1;33:sl=:cx=:fn=1;32:ln=1;36:bn=36:se=1;30'
