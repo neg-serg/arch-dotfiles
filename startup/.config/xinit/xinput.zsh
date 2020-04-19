7fd45f8c8e71cc3f28d6b76fae537ce33200a4b8
Neg <serg.zorg@gmail.com>
2020-04-19 04:40:49 +0300

    [startup] add comments

--- startup/.config/xinit/xinput.zsh
+++ startup/.config/xinit/xinput.zsh
@@ -1,14 +1,11 @@
 #!/bin/dash
-
 xmodmap ${XDG_CONFIG_HOME}/keymaps/xmodmaprc &
 unclutter --fork --timeout 1 &
-
-xset m 0 0 # disable mouse acceleration
+# disable mouse acceleration
+xset m 0 0
+# fast keyboard rate
 xset -b r rate 250 50
 
-setxkbmap \
-    -option keypad:pointerkeys \
-    -layout 'us,ru' \
+setxkbmap -option keypad:pointerkeys -layout 'us,ru' \
     -option 'grp:alt_shift_toggle' \
-    -variant altgr-intl \
-    -option ctrl:nocaps
+    -variant altgr-intl -option ctrl:nocaps
