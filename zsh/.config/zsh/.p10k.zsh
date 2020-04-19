01361b56850ac8176c7380f3aca8b3aa947902dd
Neg <serg.zorg@gmail.com>
2020-04-19 03:25:41 +0300

    [zsh] p10k remove taskwarrior

--- zsh/.config/zsh/.p10k.zsh
+++ zsh/.config/zsh/.p10k.zsh
@@ -89,7 +89,7 @@
     # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
-    taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
+    # taskwarrior           # taskwarrior task count (https://taskwarrior.org/)
     # time                  # current time
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
