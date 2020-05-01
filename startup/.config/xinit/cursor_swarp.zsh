1ec3b8cb0b6b8582af1e472ceba17a66d28e469f
Neg <serg.zorg@gmail.com>
2020-05-01 15:55:52 +0300

    [xinit] cleanup from useless scripts

--- startup/.config/xinit/cursor_swarp.zsh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-local width_x11=$(xrandr -q |awk '/Screen/{print $8}')
-local height_x11=$(xrandr -q | awk '/Screen/{print $10}' | awk -F"," '{print $1}')
-swarp ${width_x11} ${height_x11}
--- startup/.config/xinit/hotkeys.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-SXHKD_FIFO=${2:-/tmp/sxhkd_fifo}
-
-rm -f "${SXHKD_FIFO}"
-mkfifo "${SXHKD_FIFO}"
-[[ ! $(pidof sxhkd) ]] && sxhkd -s "${SXHKD_FIFO}" 2> /dev/null &
