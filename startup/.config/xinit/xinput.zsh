f6f5f214947c5ab4daacc904a269c1d3e97a62d0
Neg <serg.zorg@gmail.com>
2020-05-09 14:58:15 +0300

    [xinput] setxkbmap in background

--- startup/.config/xinit/xinput.zsh
+++ startup/.config/xinit/xinput.zsh
@@ -7,4 +7,4 @@ xset -b r rate 250 50
 
 setxkbmap -option keypad:pointerkeys -layout 'us,ru' \
     -option 'grp:alt_shift_toggle' \
-    -variant altgr-intl -option ctrl:nocaps
+    -variant altgr-intl -option ctrl:nocaps &
