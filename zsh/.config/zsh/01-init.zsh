64753873490b9411a3cfd0060971eac0825a743a
Neg <serg.zorg@gmail.com>
2020-05-18 13:26:46 +0300

    [zsh] fix wordchars

--- zsh/.config/zsh/01-init.zsh
+++ zsh/.config/zsh/01-init.zsh
@@ -136,7 +136,7 @@ for q in nvim vis vi; {
 }
 
 export TIMEFMT="[37m[34m⟬[37m[37m%J[34m⟭[39m[34m⟬[37m%U[34m⟭[39m[34m⟬[37muser %S[34m⟭[39m[34m⟬[37msystem %P[34m⟭[39m[34m⟬[37mcpu %*E total[34m⟭[39m[34m[39m[34m⟬[37mMem: %M kb max[34m⟭[39m"
-export WORDCHARS='*?_-.[]~&;!#$%^(){}<>~` '
+export WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
 export LS_COLORS
 
 export HISTFILE=${ZDOTDIR}/zsh_history
