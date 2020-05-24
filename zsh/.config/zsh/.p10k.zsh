2503c0c0a38a4557e911716fabe20d0617bdb142
Neg <serg.zorg@gmail.com>
2020-05-24 04:38:04 +0300

    [zsh] cleanup p10k config

--- zsh/.config/zsh/.p10k.zsh
+++ zsh/.config/zsh/.p10k.zsh
@@ -1417,15 +1417,6 @@
   # Custom prefix.
   # typeset -g POWERLEVEL9K_TIME_PREFIX='%fat '
 
-  # Example of a user-defined prompt segment. Function prompt_example will be called on every
-  # prompt if `example` prompt segment is added to POWERLEVEL9K_LEFT_PROMPT_ELEMENTS or
-  # POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS. It displays an icon and orange text greeting the user.
-  #
-  # Type `p10k help segment` for documentation and a more sophisticated example.
-  function prompt_example() {
-    p10k segment -f 208 -i '⭐' -t 'hello, %n'
-  }
-
   function prompt_neg() {
     setopt sh_word_split
     _neg_full_path="$PWD"
@@ -1474,7 +1465,7 @@
     fi
 
     output+="${neg_user_pretok}%f%40<..<${neg_prompt}"
-    p10k segment -f 25 -i '' -t "$output"
+    p10k segment -f 25 -t "$output"
   }
 
   # User-defined prompt segments may optionally provide an instant_prompt_* function. Its job
@@ -1489,16 +1480,6 @@
   #
   # Usually, you should either not define instant_prompt_* or simply call prompt_* from it. If
   # instant_prompt_* is not defined for a segment, the segment won't be shown in instant prompt.
-  function instant_prompt_example() {
-    # Since prompt_example always makes the same `p10k segment` calls, we can call it from
-    # instant_prompt_example. This will give us the same `example` prompt segment in the instant
-    # and regular prompts.
-    prompt_example
-  }
-
-  # User-defined prompt segments can be customized the same way as built-in segments.
-  # typeset -g POWERLEVEL9K_EXAMPLE_FOREGROUND=208
-  # typeset -g POWERLEVEL9K_EXAMPLE_VISUAL_IDENTIFIER_EXPANSION='⭐'
 
   # Transient prompt works similarly to the builtin transient_rprompt option. It trims down prompt
   # when accepting a command line. Supported values:
