10ebbba9ac046aa8cc078dcbd81ff4d625bef5a9
Neg <serg.zorg@gmail.com>
2020-05-27 17:30:45 +0300

    [xinit] cleanup xinit

--- startup/.config/xinit/xinput.zsh
+++ startup/.config/xinit/xinput.zsh
@@ -1,10 +1,11 @@
 #!/bin/dash
 unclutter --fork --timeout 1 &
-# disable mouse acceleration
-xset m 0 0
-# fast keyboard rate
-xset -b r rate 250 50
+xset m 0 0 # disable mouse acceleration
+xset -b r rate 250 50 # fast keyboard rate
 
-setxkbmap -option keypad:pointerkeys -layout 'us,ru' \
+setxkbmap \
+    -layout 'us,ru' \
     -option 'grp:alt_shift_toggle' \
-    -variant altgr-intl -option ctrl:nocaps &
+    -option keypad:pointerkeys \
+    -option ctrl:nocaps \
+    -variant altgr-intl &
