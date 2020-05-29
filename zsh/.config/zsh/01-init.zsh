ff4bc50888b95006874f864a48d79daf3b64ba91
Neg <serg.zorg@gmail.com>
2020-05-29 09:35:31 +0300

    [zsh] update settings

--- zsh/.config/zsh/01-init.zsh
+++ zsh/.config/zsh/01-init.zsh
@@ -4,60 +4,45 @@ limit stack 8192
 limit core 0
 limit -s
 
-setopt append_history # this is default, but set for share_history
-setopt share_history # import new commands from the history file also in other zsh-session
-setopt extended_history # save each command's beginning timestamp and the duration to the history file
-setopt histignorealldups # remove command lines from the history list when the first character on the line is a space
-
-setopt hist_expire_dups_first # when trimming history, lose oldest duplicates first
-setopt hist_ignore_dups # ignore duplication command history list
-setopt hist_verify # don't execute, just expand history
-setopt hist_ignore_space # reduce whitespace in history
-setopt inc_append_history # add comamnds as they are typed, don't wait until shell exit
-
-# remove command lines from the history list when the first character on the
-# line is a space
-setopt histignorespace
-# if a command is issued that can't be executed as a normal command, and the
-# command is the name of a directory, perform the cd command to that directory.
-setopt auto_cd
-
-# in order to use #, ~ and ^ for filename generation grep word
-# *~(*.gz|*.bz|*.bz2|*.zip|*.Z) -> searches for word not in compressed files
-# don't forget to quote '^', '~' and '#'!
-setopt extended_glob # ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^^ ^ ^ ^ ^
-setopt longlistjobs # display PID when suspending processes as well
-setopt nonomatch # try to avoid the 'zsh: no matches found...'
-setopt notify  # report the status of backgrounds jobs immediately
-setopt hash_list_all  # whenever a command completion is attempted, make sure the entire command path is hashed first.
-setopt completeinword # not just at the end
-setopt nohup  # don't send SIGHUP to background processes when the shell exits.
+setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word
+setopt auto_cd # if a command is issued that can't be executed as a normal command, and the command is the name of a directory, perform the cd command to that directory.
 setopt auto_pushd # make cd push the old directory onto the directory stack.
+setopt c_bases  # print $(( [#16] 0xff ))
+setopt completeinword # not just at the end
+setopt extendedglob # enable extended globbing
+setopt glob_star_short # */** -> **
+setopt hash_list_all  # whenever a command completion is attempted, make sure the entire command path is hashed first.
+setopt interactivecomments # allow interactive comments
+setopt interactivecomments # allow interactive comments after '#' in command line
+setopt longlistjobs # display PID when suspending processes as well
+setopt magicequalsubst # ~ substitution and tab completion after a = (for --x=filename args)
+setopt nobeep # get rid of beeps
+setopt no_bg_nice # don't nice background jobs
+setopt noflowcontrol # no c-s/c-q output freezing
+setopt noglobdots  # * shouldn't match dotfiles. ever.
+setopt nohup  # don't send SIGHUP to background processes when the shell exits.
+setopt no_monitor # do not notify about bg task ending
+setopt nonomatch # try to avoid the 'zsh: no matches found...'
+setopt no_notify # do not notify about bg task ending
+setopt noshwordsplit  # use zsh style word splitting
+setopt notify  # report the status of backgrounds jobs immediately
+setopt notransient_rprompt # only show the rprompt on the current prompt
+setopt prompt_subst # set the prompt
+setopt pushd_ignore_dups # don't push the same dir twice.
 setopt pushdminus # pushd -N goes to Nth dir in stack
 setopt pushdsilent # do not print dirstack after each cd/pushd
 setopt pushdtohome #pushd with no args pushes to home
-setopt pushd_ignore_dups # don't push the same dir twice.
-setopt nobeep # get rid of beeps
-setopt noglobdots  # * shouldn't match dotfiles. ever.
-setopt noshwordsplit  # use zsh style word splitting
-setopt noflowcontrol # no c-s/c-q output freezing
-setopt no_bg_nice # don't nice background jobs
-setopt no_notify # do not notify about bg task ending
-setopt no_monitor # do not notify about bg task ending
 
-setopt c_bases  # print $(( [#16] 0xff ))
-setopt prompt_subst # set the prompt
-# make sure to use right prompt only when not running a command
-setopt transient_rprompt # only show the rprompt on the current prompt
-setopt interactivecomments # allow interactive comments
-setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word
-
-setopt extendedglob # enable extended globbing
-setopt interactivecomments # allow interactive comments after '#' in command line
-
-# ~ substitution and tab completion after a = (for --x=filename args)
-setopt magicequalsubst
-# setopt glob_star_short # */** -> **
+setopt append_history # this is default, but set for share_history
+setopt extended_history # save each command's beginning timestamp and the duration to the history file
+setopt hist_expire_dups_first # when trimming history, lose oldest duplicates first
+setopt histignorealldups # remove command lines from the history list when the first character on the line is a space
+setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_ignore_space # reduce whitespace in history
+setopt histignorespace # remove command lines from the history list when the first character on the line is a space
+setopt hist_verify # don't execute, just expand history
+setopt inc_append_history # add comamnds as they are typed, don't wait until shell exit
+setopt share_history # import new commands from the history file also in other zsh-session
 
 export PATH=/usr/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/opt/go/bin:/home/neg/.cargo/bin
 export EDITOR="nvim"
@@ -134,7 +119,7 @@ stty_setup() {
     stty min 0 2> /dev/null
     stty line 6 2> /dev/null
     stty speed 38400 &> /dev/null
-    stty eof  2> /dev/null  # stty eof ''
+    stty eof  2> /dev/null
 }
 [[ $- =~ i ]] && stty_setup &!
 
