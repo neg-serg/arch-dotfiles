2c36cbe033ef63ae3d611bc8549b967989d05950
Neg <serg.zorg@gmail.com>
2020-06-01 12:42:53 +0300

    [bootstrap] cleanup package list

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,106 +1,113 @@
 #!/bin/sh
-yay -S bash rlwrap zsh zsh-completions dash mksh
+yay -S stow
+yay -S patchelf patchutils patool pciutils perf
+yay -S bash rlwrap zsh zsh-completions dash mksh babashka-bin
 yay -S ansible ansible-lint mitogen
-yay -S acpi acpid albumdetails-git alsa-firmware alsa-utils ananicy-git alacritty android-file-transfer angband ansilove-git
-yay -S antiword asciinema
-yay -S adobe-icc arandr argyllcms autorandr
-yay -S asp aurutils-git aurvote pacman-contrib pacvis-git
-
-yay -S atop htop vmtouch vnstat pv blktrace glances itop
-yay -S ipcalc iperf iproute2 iptckwed-git iptraf-ng iputils
-
+yay -S acpi acpid albumdetails-git alsa-firmware alsa-utils ananicy-git alacritty ansilove-git
+yay -S irqbalance
+yay -S tizen-sdb android-file-transfer
+yay -S antiword asciinema catdoc unrtf 
+yay -S adobe-icc arandr argyllcms autorandr sampleicc xcalib
+yay -S asp pacman-contrib pacvis-git downgrade
+yay -S atop htop vmtouch vnstat pv blktrace glances itop pycoinmon smartmontools traceroute tiptop mtr-git fio dmidecode httpflow-git httpie ioping iotop nethogs gptfdisk lm_sensors lshw lsof lsscsi
+yay -S tcpdump tcpflow tcptrack wireshark-cli
+yay -S iperf iproute2 iputils
 yay -S aspell-en aspell-ru
-yay -S aubio
 yay -S audit autoconf2.13 autogen
-yay -S babashka-bin
-yay -S bdf-bitocra bdf-ctrld-git bdf-spleen bdf-tewi-git bdf-unifont
-yay -S beets picard
+yay -S beets picard split2flac-git flac2all taffy id3 id3v2
 yay -S betterdiscordctl-git
-yay -S bind-tools bitfetch
-yay -S bleachbit
 yay -S bitwig-studio carla-bridges-win airwave-git
-yay -S bzip2 cairo-ubuntu catdoc ccat cclive ccls cflow cgroup_event_listener chafa check
-yay -S clang cloc
-yay -S clojure
-yay -S cmake cmatrix collectl convmv cope-git coreutils cpio cppo cpupower cquery cronie cryptsetup cscope ctags cuetools curlftpfs cxxmatrix-git davfs2 dconf-editor ddcutil ddgr ddrescue deadd-notification-center deheader dep depot-tools-git device-mapper devil direnv discord discover displaycal dkms dmg2img dmidecode dnscrypt-proxy docbook-xsl
+yay -S bzip2 cclive ccls
+yay -S clang deheader strace
+yay -S cxxmatrix-git
+yay -S perl cope-git
+yay -S cmatrix convmv cryptsetup cuetools davfs2 ddrescue device-mapper
 yay -S diff-so-fancy diffstat diffutils
 yay -S docker docker-compose
 yay -S dos2unix dosbox-staging dosemu dosfstools
-yay -S downgrade
-yay -S ffmpeg
-yay -S drkonqi drumkv1 dstat dumpzilla-git dunst dxvk-cache-server-git e2fsprogs easy-rsa ecl ecm-tools ed editorconfig-core-c edk2-ovmf efibootmgr eglexternalplatform-git eigen elvish emonoda encodef-git eog epiphany eslint etckeeper ettercap evhz-git evtest exfat-utils exiv2 expect extrace-git extra-cmake-modules fann fasd fatrace fio
+yay -S ffmpeg x264 x265
+yay -S easy-rsa efibootmgr
+yay -S exiv2 
+yay -S etckeeper
+yay -S dunst
 yay -S earlyoom
 yay -S firefox firefox-spell-ru browsh-bin
 yay -S flawfinder foremost
-yay -S flac2all freehdl freeimage freetype2-ultimate5 fselect fzf gcalcli gcolor3 gdb gedit geekbench gegl geis gflags gifsicle gimp
-yay -S gita-git gitbatch-bin git-crypt git-extras github-cli git-lfs gitsome-git
-yay -S gllock-git
-yay -S fontconfig-ubuntu fontforge
-yay -S gnome-backgrounds gnome-books gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-contacts gnome-control-center gnome-dictionary gnome-disk-utility gnome-documents gnome-font-viewer gnome-getting-started-docs gnome-hud gnome-logs gnome-maps gnome-menus gnome-music gnome-photos gnome-remote-desktop gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-software gnome-system-monitor gnome-terminal gnome-themes-extra gnome-todo gnome-user-docs gnome-user-share gnome-video-effects gnome-weather cantarell-fonts
+yay -S gcolor3 gdb gimp
+yay -S gita-git gitbatch-bin git-crypt git-extras github-cli git-lfs tig
+yay -S gnome-shell gnome-session
 yay -S go go-tools
 yay -S gpart gparted
-yay -S gpaste gperftools gphoto2 gpick gptfdisk grabc grex grilo-plugins grub gst-libav gst-plugins-good gst-plugins-ugly gstreamer-vaapi
+yay -S gpaste
 yay -S gtk-engine-murrine gtk-engines gtk-engine-unico gtkperf
-yay -S gucharmap gulp guvcview gxplugins.lv2 gzdoom haveged hddtemp hdparm hellband hexedit hidapi highlight hsetroot httpflow-git httpie hunspell hunspell-ru hyperfine i2c-tools
-yay -S i3-gaps i3-gnome-git i3ipc-python-git caffeine
-yay -S i7z icdiff id3 id3v2 ideviceinstaller-git idump iftop ifuse imgurqt imgur-screenshot-git indent inetutils iniparser inkscape inotify-tools intel-ucode intel-ucode-clear inxi ioping iotop
-yay -S jdk8-openjdk jdk-openjdk java-rhino drip-git icedtea-web
-yay -S ipython irqbalance isl iw ix jad jami-daemon jbigkit jfsutils jhead jnettop joker-bin jpegoptim jq jshon jwildfire kati-git kdeplasma-addons kexec-tools keynav-improved-grid-git kgamma5 kinfocenter kmon kotatogram-desktop-dynamic-bin kscreen ksshaskpass kvantum-qt5 kwallet-pam kwrited lcov leiningen
-yay -S lib32-alsa-plugins lib32-curl lib32-freetype2 lib32-gcc-libs lib32-glew lib32-glew1.10 lib32-gperftools lib32-gst-plugins-good lib32-gtk2 lib32-gtk3 lib32-gtk-engine-murrine lib32-libcaca lib32-libcurl-compat lib32-libcurl-gnutls lib32-libexif lib32-libgcrypt lib32-libgcrypt15 lib32-libpng12 lib32-libpulse lib32-librtmp0 lib32-libsamplerate lib32-libstdc++5 lib32-libtheora lib32-libtiff4 lib32-libudev0-shim lib32-libvpx lib32-libwebp lib32-mpg123 lib32-ncurses lib32-nss lib32-nvidia-utils lib32-rtmpdump lib32-sdl lib32-sdl2 lib32-sdl2_image lib32-sdl2_mixer lib32-sdl2_net lib32-sdl2_ttf lib32-sdl_image lib32-sdl_mixer lib32-sdl_sound lib32-sdl_ttf lib32-speex lib32-vkd3d
-yay -S libavc1394 libcaca libcurl-gnutls libdbusmenu-gtk2 libdbusmenu-gtk3 libev libevdev libfbclient libglvnd libimobiledevice libjamiclient libjson libmfx libopenraw libpng12 libpqxx libraqm librtmp0 libsixel libspnav libssh libstdc++5 libstroke libtraceevent libudev0-shim libusb libutil-linux libva-intel-driver libva-vdpau-driver libvdpau-va-gl
+yay -S hunspell hunspell-ru
+yay -S hddtemp hdparm
+yay -S i3-gaps i3-gnome-git i3ipc-python-git caffeine unclutter-xfixes-git wmctrl wmname slop hsetroot gllock-git
+yay -S i7z
+yay -S imgur-screenshot-git
+yay -S jdk8-openjdk jdk-openjdk drip-git icedtea-web
+yay -S jq keynav-improved-grid-git kotatogram-desktop-dynamic-bin kvantum-qt5
 yay -S licenses
-yay -S linux-clear linux-clear-bin linux-clear-headers
+yay -S linux-clear linux-clear-bin linux-clear-headers kexec-tools intel-ucode intel-ucode-clear dkms
 yay -S flameshot maim
-yay -S llvm lmdb lm_sensors lnav lockfile-progs logtop lostfiles lrs lrzip lsb-release lshw lsof lsscsi ltrace lttng-ust lutris lvm2 lxappearance lz4 lzo lzop mac man-db man-pages marked-man massren mattext mawk mblaze-git mcomix mdadm mdf2iso mediainfo mencoder mesa mesa-demos meson microsoft-python-language-server miller mimeo minetest mingetty minizip mksh mktorrent mlocate mlterm modprobed-db mopidy moreutils mosh mousetweaks mp3val mpdecimal
+yay -S lostfiles
+yay -S mesa mesa-demos
+yay -S modprobed-db
+yay -S llvm lsb-release lvm2 lz4 lzo lzop mawk mcomix mdadm
 yay -S mpd-notification-git mpdris2 mpdviz mpvc-git
-yay -S msmtp-mta mtr-git muparser namcap nanovg-git nap nasm nbsdgames-git neofetch
+yay -S neofetch inxi
 yay -S ncmpcpp mpc mpd mpdas-git
-yay -S neovim-git neovim-remote vi vis
-yay -S netctl nethack nethack4 nethogs netstat-nat nettle nftables ngrep ngrok nmap nmon
+yay -S neovim-git neovim-remote vi vis ruby-neovim
+yay -S netctl netstat-nat nftables nmap
 yay -S network-manager-applet networkmanager-openvpn
 yay -S notmuch-addrlookup-c notmuchfs-git notmuch-mutt neomutt
-yay -S npm nq-git nrg2iso ntop ntp
-yay -S nvidia nvidia-dkms nvidia-settings
-yay -S nvimpager-git nvm-git nyx obs-studio ocp-git ohsnap oilshell onefetch openbox openbsd-netcat openresolv
-yay -S openssh openssh-askpass openssl openssl-1.0 openvpn
-yay -S optipng os-prober p7zip pacgraph
-yay -S pango-ubuntu
-yay -S paprefs parallel
+yay -S ntp
+yay -S nvidia nvidia-dkms nvidia-settings lib32-nvidia-utils
+yay -S nvimpager-git obs-studio 
+yay -S openbsd-netcat
+yay -S openresolv
+yay -S openssh openssh-askpass openssl openvpn
+yay -S optipng jpegoptim
+yay -S p7zip pacgraph
+yay -S cairo-infinality fontforge freetype2-infinality fontconfig-infinality-ultimate pango
+yay -S parallel
 yay -S pass pass-import pass-otp
-yay -S patchelf patchutils patool pavucontrol pciutils perf
-yay -S perl perl-anyevent perl-anyevent-i3 perlbrew perl-class-accessor perl-class-method-modifiers perl-cpan-meta perl-curses perl-data-dump perl-extutils-config perl-extutils-depends perl-extutils-helpers perl-extutils-installpaths perl-extutils-pkgconfig perl-extutils-xsbuilder perl-file-find-rule perl-file-mmagic perl-file-slurp perl-filesys-df perl-image-exiftool perl-import-into perl-io-tty perl-ipc-run perl-json-xs perl-list-moreutils perl-lwp-useragent-cached perl-math-round perl-mime-base32 perl-module-implementation perl-module-runtime perl-moo perl-package-stash-xs perl-params-validate perl-regexp-ipv6 perl-rename perl-role-tiny perl-sub-identify perl-test-deep perl-test-nowarnings perl-text-charwidth perl-text-iconv perl-unicode-linebreak
-yay -S picom-tryone-git pigz pinentry piper pkgfile planck plantri
-yay -S polybar-neg-git potrace powerdevil powertop ppi3-git conky-cairo c-lolcat figlet 
+yay -S picom-tryone-git
+yay -S polybar-neg-git potrace powerdevil powertop ppi3-git conky-cairo c-lolcat figlet
 yay -S prettyping fping-git
-yay -S procdump procps-ng progress protontricks-git proxychains-ng psmisc psutils
-yay -S pulseaudio pulseaudio-alsa pulseaudio-jack pulsemixer-git cadence ncpamixer pamixer pamix-git
-yay -S pwsafe pycoinmon
+yay -S procdump procps-ng progress proxychains-ng
+yay -S pulseaudio pulseaudio-alsa pulseaudio-jack pulsemixer-git cadence ncpamixer pamixer pamix-git pavucontrol
 yay -S pypy3 pypy3-pip pyright pyenv pyprof2calltree
-yay -S python2 python2-ansigenome python2-cairo python2-codegen python2-coverage python2-distutils-extra python2-flowui python2-funcsigs python2-leveldb python2-markdown2-git python2-miniupnpc python2-mock python2-mpd2-git python2-ninja-syntax python2-nose python2-pbr python2-pyinotify python2-trollius
-yay -S python-aiohttp python-ansi2html python-argparse python-aubio python-bcc python-cachecontrol python-celery python-codegen python-colored python-colorlog python-colorthief python-coverage python-cram python-discid python-discogs-client python-distlib python-distro python-ewmh python-feedparser python-flup python-fonttools python-geoip python-gevent python-glog python-haishoku python-hexdump python-hiredis python-html2text python-html5lib python-httplib2 python-i3-py python-idna_ssl python-imagehash-git python-librtmp python-lmdb python-lockfile python-mpd2 python-musicpd python-natsort python-ndg-httpsclient python-netaddr python-networkx python-nose python-notify2 python-numpy python-pep517 python-pillow-simd python-pip python-plumbum python-prettytable python-proboscis python-progress python-psutil python-pudb python-pyacoustid python-pyaml python-pycallgraph python-pygame python-pygeoip python-pygit2 python-pygments python-pyinotify python-pylast python-pylint python-pymongo python-pynvim python-pyqt5 python-pyqtgraph python-pyroute2 python-pytoml python-pywal python-qscintilla-qt5 python-qt.py python-rapidjson python-redis python-requests python-retrying python-robobrowser python-rope python-service-identity python-tempita python-toml python-ueberzug python-unittest-mixins python-urwid_readline python-urwidtrees python-uvloop python-vdf python-virtualenv python-wand python-xlib python-xpybutil
-yay -S qemu qemu-block-iscsi qemu-guest-agent
-yay -S qt5-base qt5-charts qt5-connectivity qt5ct qt5-datavis3d qt5-enginio qt5-gamepad qt5-imageformats qt5-networkauth qt5-remoteobjects qt5-styleplugins qt5-translations qt5-websockets qt5-x11extras qt5-xcb-private-headers
+yay -S python-pip python-pylint python-pynvim
+yay -S qemu qemu-block-iscsi qemu-guest-agent spice-vdagent
+yay -S qt5-base qt5ct
 yay -S radare2 radare2-bindings-git
-yay -S quickwall ranger range-v3 rbenv rclone re2 read-edid readymedia-transcode-git rebound recoll redasm reflector reiserfsprogs reptyr rofi rofi-pass-git rpmextract rp-pppoe rsync rubber ruby-neovim rygel s3fs-fuse sampleicc samplv1 sane schedtool schedtoold sc-im scons scrot sdl2 seahorse sfnt2woff sgi-fonts shards shntool simh slop smartmontools s-nail snarf soundcloud-dl-git spice-vdagent spirv-tools splint split2flac-git sprunge ssdeep sshfs
-yay -S cdu ripgrep rmlint fd
+yay -S ranger
+yay -S recoll
+yay -S rofi rofi-pass-git
+yay -S rsync sdl2 sshfs
+yay -S cdu ripgrep rmlint fd fasd fzf parallel massren mimeo ipython
 yay -S steam steamcmd steam-fonts steam-native-runtime
-yay -S stig
-yay -S stone-soup
-yay -S stow strace straw-viewer-git streamlink stress-ng s-tui sushi swig sxhkd-git synergy-git sysfsutils sysstat systemd-boot-pacman-hook systemd-sysvcompat tabbed taffy tar task tasksh tcpdump tcpflow tcptrack thermald the_silver_searcher tig tiptop tizen-sdb tk tl tldr tmatrix-git tmon
+yay -S stig torrentinfo transmission-cli
+yay -S stone-soup nethack nethack4 angband gzdoom
+yay -S synergy-git
+yay -S tar rpmextract unrar
+yay -S streamlink stress-ng s-tui sysfsutils sysstat thermald tmatrix-git
+yay -S task tasksh
+yay -S systemd-boot-pacman-hook
 yay -S tmux dtach-git
-yay -S toilet tor-browser torrentinfo torsocks totem t-prot traceroute tracker tracker-miners translate-shell transline-git transmission-cli tre tree tsocks cozette-otb cozette-ttf ttf-anka-coder ttf-bitstream-vera ttf-code2000 ttf-dejavu ttf-droid ttf-font-icons ttf-ibm-plex ttf-iosevka ttf-iosevka-term ttf-ms-win10 ttf-ms-win10-japanese ttf-ms-win10-korean ttf-ms-win10-other ttf-ms-win10-sea ttf-ms-win10-thai ttf-ms-win10-zh_cn ttf-ms-win10-zh_tw ttf-opensans ttf-spacemono ttf-ume 
-yay -S udiskie unclutter-xfixes-git 
-yay -S unrar unrtf unzip-natspec upx urlscan-git urlview util-linux valgrind vkd3d
+yay -S toilet
+yay -S tor-browser torsocks
+yay -S t-prot tracker tracker-miners transline-git tree tsocks
+yay -S cozette-otb cozette-ttf ttf-code2000 ttf-iosevka ttf-iosevka-term ttf-ms-win10 ttf-ms-win10-japanese ttf-ms-win10-korean ttf-ms-win10-other ttf-ms-win10-sea ttf-ms-win10-thai ttf-ms-win10-zh_cn ttf-ms-win10-zh_tw
+yay -S udiskie
+yay -S util-linux valgrind
 yay -S vulkan-extra-layers vulkan-headers vulkan-html-docs vulkan-mesa-layer vulkan-tools vulkan-trace vulkan-validation-layers
-yay -S wireshark-cli wmctrl wmname x86_energy_perf_policy xcalib xclip xiccd xinit-xsession
-yay -S x264 x265
-yay -S xtrace
-yay -S xurls
-yay -S xxhash
-yay -S yarn
-yay -S yasm
-yay -S youtube-dl youtube-viewer you-get
+yay -S xclip
+yay -S xurls urlscan-git urlview
+yay -S youtube-dl you-get straw-viewer-git sprunge 
+yay -S wine wine-mono winetricks dxvk-bin dxvk-cache-server-git vkd3d protontricks-git lib32-vkd3d lutris
 yay -S zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps fbless
-yay -S zoom 
+yay -S zoom discord
 
-find -maxdepth 1 -type d -not -path '*/\.*' -printf '%P\n' | xargs stow -v 
+find -maxdepth 1 -type d -not -path '*/\.*' -printf '%P\n' | xargs stow -v
