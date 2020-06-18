edfdf5eb6a31d515a1bd2cb591d44e1ff41c6fda
Neg <serg.zorg@gmail.com>
2020-06-18 07:36:32 +0300

    [zsh] bindings CS

--- zsh/.config/zsh/04-bindings.zsh
+++ zsh/.config/zsh/04-bindings.zsh
@@ -21,17 +21,17 @@ bindkey "^Z" fg-widget
 bindkey '^j' fasd-complete
 bindkey "^I" expand-or-complete-with-dots
 bindkey '^M' special-accept-line
-bindkey " "             magic-space
-bindkey ",."            zleiab
+bindkey " "  magic-space
+bindkey ",." zleiab
 bindkey . rationalise-dot
 bindkey -M isearch . self-insert # without this, typing a . aborts incremental history search
 zle -N inplace_mk_dirs && bindkey '^xM' inplace_mk_dirs # load the lookup subsystem if it's available on the system
 
-bindkey -M menuselect '^o'    accept-and-infer-next-history
-bindkey -M menuselect 'h'     vi-backward-char
-bindkey -M menuselect 'j'     vi-down-line-or-history
-bindkey -M menuselect 'k'     vi-up-line-or-history
-bindkey -M menuselect 'l'     vi-forward-char
-bindkey -M menuselect 'i'     accept-and-menu-complete
-bindkey -M menuselect 'o'     accept-and-infer-next-history
-bindkey -M menuselect '\e^M'  accept-and-menu-complete
+bindkey -M menuselect '^o' accept-and-infer-next-history
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'i' accept-and-menu-complete
+bindkey -M menuselect 'o' accept-and-infer-next-history
+bindkey -M menuselect '\e^M' accept-and-menu-complete
