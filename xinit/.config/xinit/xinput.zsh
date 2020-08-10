c6cae22c1067e05ab5fe28d7d4efc31fee6a49fc
Neg <serg.zorg@gmail.com>
2020-08-11 00:24:02 +0300

    [xinit] xset do not work anymore

--- xinit/.config/xinit/xinput.zsh
+++ xinit/.config/xinit/xinput.zsh
@@ -1,8 +1,6 @@
 #!/bin/dash
 unclutter --fork --timeout 1 &
-xset m 0 0 # disable mouse acceleration
 xset -b r rate 250 50 # fast keyboard rate
-
 setxkbmap \
     -layout 'us,ru' \
     -option 'grp:alt_shift_toggle' \
