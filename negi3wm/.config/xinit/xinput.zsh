8deb2cb553c706a417728bb13ce47decefa0e054
Neg <serg.zorg@gmail.com>
2020-11-04 16:50:57 +0300

    [xinput] better keyboard settings order

--- negi3wm/.config/xinit/xinput.zsh
+++ negi3wm/.config/xinit/xinput.zsh
@@ -1,5 +1,4 @@
 #!/bin/dash
-unclutter --fork --timeout 1 &
 xset -b r rate 250 50 # fast keyboard rate
 setxkbmap \
     -layout 'us,ru' \
@@ -7,3 +6,4 @@ setxkbmap \
     -option keypad:pointerkeys \
     -option ctrl:nocaps \
     -variant altgr-intl &
+unclutter --fork --timeout 1
