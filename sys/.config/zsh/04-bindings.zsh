7588b3b63c161c5a557f51fae368131852caaa5c
Neg <serg.zorg@gmail.com>
2021-01-27 04:47:42 +0300

    [zsh] cleanup bindings

--- sys/.config/zsh/04-bindings.zsh
+++ sys/.config/zsh/04-bindings.zsh
@@ -1,18 +1,14 @@
 bindkey -e
-
 bindkey "^[-" cd-forward
 bindkey "^[=" cd-back
-
 autoload up-line-or-beginning-search
 zle -N up-line-or-beginning-search
 autoload down-line-or-beginning-search
 zle -N down-line-or-beginning-search
-
 bindkey "^[[A" up-line-or-beginning-search
 bindkey "^[[B" down-line-or-beginning-search
 bindkey "^P" up-line-or-beginning-search
 bindkey "^N" down-line-or-beginning-search
-
 bindkey " " magic-abbrev-expand
 bindkey "^x " no-magic-abbrev-expand
 bindkey . rationalise-dot
@@ -26,12 +22,7 @@ bindkey ",." zleiab
 bindkey . rationalise-dot
 bindkey -M isearch . self-insert # without this, typing a . aborts incremental history search
 zle -N inplace_mk_dirs && bindkey '^xM' inplace_mk_dirs # load the lookup subsystem if it's available on the system
-
 bindkey -M menuselect '^o' accept-and-infer-next-history
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'i' accept-and-menu-complete
 bindkey -M menuselect 'o' accept-and-infer-next-history
 bindkey -M menuselect '\e^M' accept-and-menu-complete
