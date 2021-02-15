55e01498bd0772fc2d673dcf610ccd95a68d1ea0
Neg <serg.zorg@gmail.com>
2021-02-15 23:34:46 +0300

    [nvim] better linting

--- dev/.config/nvim/lua/plugcfg/ale.lua
+++ dev/.config/nvim/lua/plugcfg/ale.lua
@@ -1,16 +1,19 @@
 -- ┌───────────────────────────────────────────────────────────────────────────────────┐
 -- │ █▓▒░ w0rp/ale                                                                     │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
-vim.g["ale_fixers"] = {
-    ['javascript'] = {'eslint'},
-    ['json'] = {'jq'},
-    ['html'] = {'prettier'},
-    ['scss'] = {'stylelint'},
-    ['less'] = {'stylelint'},
-    ['css'] = {'stylelint'},
-    ['python'] = {'black', 'yapf'},
-    ['rust'] = {'rustfmt'}
+vim.g.ale_fixers = {
+    ['javascript'] = 'eslint',
+    ['json'] = 'jq',
+    ['html'] = 'prettier',
+    ['scss'] = 'stylelint',
+    ['less'] = 'stylelint',
+    ['css'] = 'stylelint',
+    ['python'] = 'yapf',
+    ['rust'] = 'rustfmt'
 }
+vim.g.ale_linters = {['python'] = {'bandit', 'jedils', 'mypy',
+'prospector', 'pycodestyle', 'pydocstyle', 'pyflakes', 'pylama', 'pylint',
+'pyls', 'pyre', 'pyright', 'vulture'}}
 vim.g["ale_echo_msg_format"] = '%linter% %s %severity%'
 vim.g["ale_sign_highlight_linenrs"] = 1
 vim.g["ale_sign_error"] = ''
