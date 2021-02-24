df0c3309ccdad5fdae1241eeb426e1840eff4c45
Neg <serg.zorg@gmail.com>
2021-02-24 18:28:55 +0300

    [nvim] stash hotkeys for ansible

--- dev/.config/nvim/lua/plugcfg/coc.lua
+++ dev/.config/nvim/lua/plugcfg/coc.lua
@@ -34,3 +34,15 @@ vim.api.nvim_exec([[
 command! -nargs=0 Format :call CocAction('format')
 let g:coc_global_extensions = ['coc-actions', 'coc-browser', 'coc-clangd', 'coc-diagnostic', 'coc-git', 'coc-json', 'coc-lists', 'coc-prettier', 'coc-python', 'coc-rust-analyzer', 'coc-snippets', 'coc-solargraph', 'coc-stylelint', 'coc-translator', 'coc-yaml']
 ]], true)
+-- -- Use <C-l> for trigger snippet expand.
+-- imap <C-l> <Plug>(coc-snippets-expand)
+-- -- Use <C-j> for select text for visual placeholder of snippet.
+-- vmap <C-j> <Plug>(coc-snippets-select)
+-- -- Use <C-j> for jump to next placeholder, it's default of coc.nvim
+-- let g:coc_snippet_next = '<c-j>'
+-- -- Use <C-k> for jump to previous placeholder, it's default of coc.nvim
+-- let g:coc_snippet_prev = '<c-k>'
+-- -- Use <C-j> for both expand and jump (make expand higher priority.)
+-- imap <C-j> <Plug>(coc-snippets-expand-jump)
+-- -- Use <leader>x for convert visual selected code to snippet
+-- xmap <leader>x  <Plug>(coc-convert-snippet)
