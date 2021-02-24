e90aec0b173aeab6b9f4b99f5c84a92efd00d89a
Neg <serg.zorg@gmail.com>
2021-02-24 20:10:17 +0300

    [nvim] cleanup init.lua

--- dev/.config/nvim/lua/init.lua
+++ dev/.config/nvim/lua/init.lua
@@ -25,39 +25,7 @@ require 'plugcfg/easyalign'
 require 'plugcfg/vista'
 require 'plugcfg/coc'
 require 'plugcfg/dap'
-
--- --SET LEADER GLOBALLY
--- g.mapleader = ' '
-
--- --TELESCOPE MAPPINGS
--- utils.nnoremap(leader..'s', ":lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.vnoremap(leader..'s', ":lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.nnoremap(leader..'S', ":lua require'telescope.builtin'.oldfiles(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.vnoremap(leader..'S', ":lua require'telescope.builtin'.oldfiles(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.nnoremap(leader..'G', ":lua require'telescope.builtin'.git_files(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.vnoremap(leader..'G', ":lua require'telescope.builtin'.git_files(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.nnoremap(leader..'b', ":lua require'telescope.builtin'.buffers(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.vnoremap(leader..'b', ":lua require'telescope.builtin'.buffers(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.nnoremap(leader..'c', ":lua require'telescope.builtin'.commands(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.vnoremap(leader..'c', ":lua require'telescope.builtin'.commands(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.nnoremap(leader..'f', ":lua require'telescope.builtin'.live_grep(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.vnoremap(leader..'f', ":lua require'telescope.builtin'.live_grep(require('telescope.themes').get_dropdown({}))<cr>")
---
--- utils.nnoremap(leader..'5', ":Startify<cr>")
--- utils.vnoremap(leader..'5', ":Startify<cr>")
--- utils.nnoremap(leader..'4', ":lua require'telescope.builtin'.man_pages(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.vnoremap(leader..'4', ":lua require'telescope.builtin'.man_pages(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.nnoremap(leader..'3', ":lua require'telescope.builtin'.symbols(require('telescope.themes').get_dropdown({sources = {'emoji'}}))<cr>")
--- utils.vnoremap(leader..'3', ":lua require'telescope.builtin'.symbols(require('telescope.themes').get_dropdown({sources = {'emoji'}}))<cr>")
--- utils.nnoremap(leader..'2', ":lua require'telescope.builtin'.help_tags(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.vnoremap(leader..'2', ":lua require'telescope.builtin'.help_tags(require('telescope.themes').get_dropdown({}))<cr>")
--- utils.noremap('',leader..'1', ":lua require'telescope.builtin'.builtin()<cr>")
---
--- --KOMMENTARY MAPPINGS
 -- vim.api.nvim_set_keymap('n', '++', '<Plug>kommentary_line_default', { silent = true })
 -- vim.api.nvim_set_keymap('v', '++', '<Plug>kommentary_visual_default', { silent = true })
---
--- --TERMINAL MAPPINGS
 -- utils.nnoremap('<leader>t', '<CMD>lua require"FTerm".toggle()<CR>')
 -- utils.tnoremap('<leader>t', '<C-\\><C-n><CMD>lua require"FTerm".toggle()<CR>')
---
