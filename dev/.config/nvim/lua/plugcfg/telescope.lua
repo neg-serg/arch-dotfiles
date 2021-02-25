2e646a03f1b837ca9d45421e48052ade09a91122
Neg <serg.zorg@gmail.com>
2021-02-25 12:10:09 +0300

    [nvim] cleanup telescope stuff

--- dev/.config/nvim/lua/plugcfg/telescope.lua
+++ dev/.config/nvim/lua/plugcfg/telescope.lua
@@ -57,3 +57,44 @@ vim.cmd[[highlight TelescopeSelectionCaret guifg=#749484 gui=bold]]
 
 -- require('telescope').load_extension('fzy_native')
 pcall(require('telescope').load_extension, 'fzy_native')
+
+-- require('telescope').setup{
+--   defaults = {
+--     vimgrep_arguments = {
+--       'rg',
+--       '--color=never',
+--       '--no-heading',
+--       '--with-filename',
+--       '--line-number',
+--       '--column',
+--       '--smart-case'
+--     },
+--     prompt_position = "bottom",
+--     prompt_prefix = ">",
+--     initial_mode = "insert",
+--     selection_strategy = "reset",
+--     sorting_strategy = "descending",
+--     layout_strategy = "horizontal",
+--     layout_defaults = {},
+--     file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+--     file_ignore_patterns = {},
+--     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+--     shorten_path = true,
+--     winblend = 0,
+--     width = 0.75,
+--     preview_cutoff = 120,
+--     results_height = 1,
+--     results_width = 0.8,
+--     border = {},
+--     borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+--     color_devicons = true,
+--     use_less = true,
+--     set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+--     file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+--     grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+--     qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+--
+--     -- Developer configurations: Not meant for general override
+--     buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+--   }
+-- }
