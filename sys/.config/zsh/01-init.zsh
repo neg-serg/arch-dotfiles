3a6550039ddb9072b78ce0052e5f77f5e1ec46c9
Neg <serg.zorg@gmail.com>
2021-03-01 12:23:46 +0300

    [zsh] use nvimpager everywhere

--- sys/.config/zsh/01-init.zsh
+++ sys/.config/zsh/01-init.zsh
@@ -54,8 +54,8 @@ typeset -U path cdpath fpath manpath # automatically remove duplicates from thes
 export PATH=/usr/bin:$HOME/bin:/usr/local/bin:/sbin:/bin:/usr/bin/core_perl:/opt/go/bin:/opt/cuda/bin
 export EDITOR="nvim"
 export VISUAL="${EDITOR}"
-export PAGER="slit"
-export READNULLCMD="highlight"
+export PAGER="nvimpager"
+export READNULLCMD="${HOME}/bin/nccat"
 export MANPAGER="nvim +Man!"
 
 export TIMEFMT="[37m[34m⟬[37m[37m%J[34m⟭[39m[34m⟬[37m%U[34m⟭[39m[34m⟬[37muser %S[34m⟭[39m[34m⟬[37msystem %P[34m⟭[39m[34m⟬[37mcpu %*E total[34m⟭[39m[34m[39m[34m⟬[37mMem: %M kb max[34m⟭[39m"
