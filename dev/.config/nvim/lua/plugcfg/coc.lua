1389e1c56e78f1f7159b1a9b3ca418007da04aa1
Neg <serg.zorg@gmail.com>
2021-03-01 21:38:32 +0300

    [nvim] stash bindings for coc

--- dev/.config/nvim/lua/plugcfg/coc.lua
+++ dev/.config/nvim/lua/plugcfg/coc.lua
@@ -48,3 +48,25 @@ vim.g.coc_global_extensions = {
 vim.api.nvim_exec([[
 command! -nargs=0 Format :call CocAction('format')
 ]], true)
+
+--[[ " Remap keys for applying codeAction to the current buffer.
+nmap <leader>ac  <Plug>(coc-codeaction)
+" Apply AutoFix to problem on the current line.
+nmap <leader>qf  <Plug>(coc-fix-current)
+
+" Map function and class text objects
+" NOTE: Requires 'textDocument.documentSymbol' support from the language server.
+xmap if <Plug>(coc-funcobj-i)
+omap if <Plug>(coc-funcobj-i)
+xmap af <Plug>(coc-funcobj-a)
+omap af <Plug>(coc-funcobj-a)
+xmap ic <Plug>(coc-classobj-i)
+omap ic <Plug>(coc-classobj-i)
+xmap ac <Plug>(coc-classobj-a)
+omap ac <Plug>(coc-classobj-a)
+
+" Use CTRL-S for selections ranges.
+" Requires 'textDocument/selectionRange' support of language server.
+nmap <silent> <C-s> <Plug>(coc-range-select)
+xmap <silent> <C-s> <Plug>(coc-range-select)
+ ]]
