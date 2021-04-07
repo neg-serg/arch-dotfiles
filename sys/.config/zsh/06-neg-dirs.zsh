c11e984cdd52012880b8d3ed4f1dde4579d65475
Neg <serg.zorg@gmail.com>
2021-04-07 16:24:21 +0300

    [zsh] neg-dirs refactoring

--- sys/.config/zsh/06-neg-dirs.zsh
+++ sys/.config/zsh/06-neg-dirs.zsh
@@ -1,16 +1,9 @@
-local -U neg_dirs=(
-    "${HOME}/1st_level"
-    "${HOME}/dw"
-    "${HOME}/src/1st_level"
-    "${HOME}/src/wrk/infrastructure"
-)
-for t in {1..5}; export "NEGCD[${t}]=$neg_dirs[${t}]"
-for t in {1..5}; bindkey "^[${t}" negcd-${t}
-negcd-1() { cd "${NEGCD[1]}" && redraw-prompt }
-negcd-2() { cd "${NEGCD[2]}" && redraw-prompt }
-negcd-3() { cd "${NEGCD[3]}" && redraw-prompt }
-negcd-4() { cd "${NEGCD[4]}" && redraw-prompt }
-zle -N negcd-1
-zle -N negcd-2
-zle -N negcd-3
-zle -N negcd-4
+if [[ ! -z "${HOME}" ]]; then
+    local -U neg_dirs=("${HOME}"/{1st_level,dw,src/1st_level})
+    for t in {1..$#neg_dirs}; do
+        export "NEGCD[${t}]=$neg_dirs[${t}]"
+        bindkey "^[${t}" negcd-${t}
+        eval "negcd-$t() { cd \"${NEGCD[$t]}\" && redraw-prompt }"
+        eval "zle -N negcd-$t"
+    done
+fi
