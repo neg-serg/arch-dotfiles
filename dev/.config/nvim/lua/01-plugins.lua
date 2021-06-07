0faeec20f115ad9fe30bd3c5ef6c87383369c4ce
Neg <serg.zorg@gmail.com>
2021-06-08 01:29:16 +0300

    [nvim] custom colors for git markers

--- dev/.config/nvim/lua/01-plugins.lua
+++ dev/.config/nvim/lua/01-plugins.lua
@@ -144,7 +144,15 @@ return require('packer').startup({function()
 -- │ █▓▒░ DCVS                                                                         │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
     use {'iberianpig/tig-explorer.vim'} -- tig integration
-    use {'rhysd/conflict-marker.vim'} -- good conflict marker
+    use {'rhysd/conflict-marker.vim',
+        config = function()
+            vim.cmd[[highlight ConflictMarkerBegin guibg=#2f7366]]
+            vim.cmd[[highlight ConflictMarkerOurs guibg=#2e5049]]
+            vim.cmd[[highlight ConflictMarkerTheirs guibg=#344f69]]
+            vim.cmd[[highlight ConflictMarkerEnd guibg=#2f628e]]
+            vim.cmd[[highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81]]
+        end
+    } -- good conflict marker
     use {'sindrets/diffview.nvim',
         cmd = {'DiffviewLoad'},
         config = function() require'plugcfg/diffview' end
