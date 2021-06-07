f52eed103a20e2ba94ff56b874919c8e4db675ad
Neg <serg.zorg@gmail.com>
2021-06-07 22:31:13 +0300

    [nvim] stash hotkeys stuff

--- dev/.config/nvim/lua/70-rsi.lua
+++ dev/.config/nvim/lua/70-rsi.lua
@@ -17,3 +17,19 @@ api.nvim_command('nnoremap <C-x>b <C-o>:Buffers<CR>')
 api.nvim_command('omap <M-b> <Left>')
 api.nvim_command('omap <M-e> <End>')
 api.nvim_command('onoremap <M-f> <C-o>e<Right>')
+api.nvim_command('inoremap <C-a> <C-o>^')
+api.nvim_command('inoremap <C-e> <C-o>$')
+
+-- -- Move in command line mode using hjkl
+-- for b in [["<C-h>", "<left>"],
+-- 	\ ["<C-j>", "<down>"],
+-- 	\ ["<C-k>", "<up>"],
+-- 	\ ["<C-l>", "<right>"],
+-- 	\ ["<C-a>", "<Home>"],
+-- 	\ ["<C-e>", "<End>"]]
+--     execute('lnoremap ' . b[0] . ' ' . b[1])
+--     execute('cnoremap ' . b[0] . ' ' . b[1])
+--     " Warning: Unexpected behavior might ensue when it comes to closing
+--     " completion menu because of <C-e>
+--     execute('inoremap <expr> ' . b[0] . ' (pumvisible() ? "\<C-e>" : "") . "\' . b[1] . '"')
+-- endfor
