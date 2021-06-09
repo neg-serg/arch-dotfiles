87dd741ea1ec86b5dd46ccd29b386bfdd37a3c72
Neg <serg.zorg@gmail.com>
2021-06-09 13:34:34 +0300

    [nvim] matchup disable motion

--- dev/.config/nvim/lua/plugcfg/vim-matchup.lua
+++ dev/.config/nvim/lua/plugcfg/vim-matchup.lua
@@ -2,5 +2,5 @@
 -- │ █▓▒░ andymass/vim-matchup                                                         │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 vim.g.matchup_matchparen_enabled = 0
-vim.g.matchup_motion_enabled = 1
+vim.g.matchup_motion_enabled = 0
 vim.g.matchup_text_obj_enabled = 1
