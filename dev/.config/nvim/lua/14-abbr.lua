72620c16318b9e0f0bb538523400574bee21ebb3
Neg <serg.zorg@gmail.com>
2021-06-10 22:48:41 +0300

    [nvim] better cabbrev

--- dev/.config/nvim/lua/14-abbr.lua
+++ dev/.config/nvim/lua/14-abbr.lua
@@ -1,13 +1,32 @@
 vim.cmd "cabbrev W w"
-vim.cmd "cnoreabbrev gd Gvdiff"
-vim.cmd "cnoreabbrev gp Gpush"
-vim.cmd "cnoreabbrev gc Gcommit -v -m"
+vim.cmd "cabbrev W! w!"
+vim.cmd "cnoreabbrev bd Bd"
+vim.cmd "cnoreabbrev cp Cp"
+vim.cmd "cnoreabbrev e E"
 vim.cmd "cnoreabbrev gca Gcommit --amend -v"
+vim.cmd "cnoreabbrev gc Gcommit -v -m"
 vim.cmd "cnoreabbrev gcheckout Gcheckout"
-vim.cmd "cnoreabbrev grm Gremove"
+vim.cmd "cnoreabbrev gd Gvdiff"
 vim.cmd "cnoreabbrev gmv Gmove"
+vim.cmd "cnoreabbrev gp Gpush"
+vim.cmd "cnoreabbrev grm Gremove"
 vim.cmd "cnoreabbrev L CocList"
+vim.cmd "cnoreabbrev mkdir Mkdir"
+vim.cmd "cnoreabbrev mv Mv"
+vim.cmd "cnoreabbrev QA qa"
+vim.cmd "cnoreabbrev QA! qa!"
+vim.cmd "cnoreabbrev Q q"
+vim.cmd "cnoreabbrev Q! q!"
+vim.cmd "cnoreabbrev rm Rm"
+vim.cmd "cnoreabbrev sp Sp"
+vim.cmd "cnoreabbrev tabe TabE"
 vim.cmd "cnoreabbrev U UltiSnipsEdit"
+vim.cmd "cnoreabbrev vs VS"
+vim.cmd "cnoreabbrev wQ wq"
+vim.cmd "cnoreabbrev Wq wq"
+vim.cmd "cnoreabbrev WQ wq"
+vim.cmd "cnoreabbrev W w"
+
 vim.api.nvim_exec([[
 function! CCR()
     let cmdline = getcmdline()
@@ -19,22 +38,3 @@ function! CCR()
 endfunction
 cnoremap <expr> <CR> CCR()
 ]], true)
-
--- cnoreabbrev W! w!
--- cnoreabbrev Q! q!
--- cnoreabbrev QA! qa!
--- cnoreabbrev QA qa
--- cnoreabbrev Wq wq
--- cnoreabbrev wQ wq
--- cnoreabbrev WQ wq
--- cnoreabbrev W w
--- cnoreabbrev Q q
--- cnoreabbrev e E
--- cnoreabbrev sp Sp
--- cnoreabbrev vs VS
--- cnoreabbrev tabe TabE
--- cnoreabbrev bd Bd
--- cnoreabbrev rm Rm
--- cnoreabbrev mkdir Mkdir
--- cnoreabbrev cp Cp
--- cnoreabbrev mv Mv
