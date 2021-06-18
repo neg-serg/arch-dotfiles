22a66c6aa2769787388ee23a5bec993925f3316b
Neg <serg.zorg@gmail.com>
2021-06-19 01:53:12 +0300

    [nvim] better sort

--- dev/.config/nvim/lua/init.lua
+++ dev/.config/nvim/lua/init.lua
@@ -1,7 +1,7 @@
-require '01-plugins'
-_, nvim_utils = pcall(require, "nvim_utils")
 require '00-helpers'
 require '00-settings'
+require '01-plugins'
+_, nvim_utils = pcall(require, "nvim_utils")
 require '02-bindings'
 require '04-aucmds'
 require '08-cmds'
