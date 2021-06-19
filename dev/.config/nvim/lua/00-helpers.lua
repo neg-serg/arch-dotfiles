54f76ecba26d02c5013ecfebe0190943b1dbe7ef
Neg <serg.zorg@gmail.com>
2021-06-19 22:02:58 +0300

    [nvim] update helpers

--- dev/.config/nvim/lua/00-helpers.lua
+++ dev/.config/nvim/lua/00-helpers.lua
@@ -15,3 +15,17 @@ function _G.dump(...)
     local objects = vim.tbl_map(vim.inspect, {...})
     print(unpack(objects))
 end
+
+-- For autocommands, extracted from https://github.com/norcalli/nvim_utils
+function _G.create_augroups(definitions)
+	for group_name, definition in pairs(definitions) do
+		Execute('augroup ' .. group_name)
+		Execute('autocmd!')
+		for _, def in ipairs(definition) do
+			local command =
+				table.concat(vim.tbl_flatten({ 'autocmd', def }), ' ')
+			Execute(command)
+		end
+		Execute('augroup END')
+	end
+end
