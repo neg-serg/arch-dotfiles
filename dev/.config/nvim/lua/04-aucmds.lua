1685e959539f0c632d0114cd16fead7ae1323289
Neg <serg.zorg@gmail.com>
2021-06-19 23:53:02 +0300

    [nvim] rewrite autocmds in lua

--- dev/.config/nvim/lua/04-aucmds.lua
+++ dev/.config/nvim/lua/04-aucmds.lua
@@ -1,54 +1,43 @@
-vim.api.nvim_exec([[
-augroup ModeChangeSettings
-    autocmd!
-    " Clear search context when entering insert mode, which implicitly stops the
-    " highlighting of whatever was searched for with hlsearch on. It should also
-    " not be persisted between sessions.
-    autocmd InsertEnter * let @/ = ''
-    autocmd BufReadPre,FileReadPre * let @/ = ''
-    autocmd InsertLeave * setlocal nopaste
-augroup END
-augroup HighlightYank
-    autocmd!
-    autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout=60, higroup="Search"})
-augroup END
-augroup CursorLine
-    autocmd!
-    autocmd VimEnter,WinEnter,BufWinEnter,BufEnter * setlocal cursorline
-    autocmd BufLeave,WinLeave * setlocal nocursorline
-augroup END
-augroup cocgroup
-    autocmd!
-    " Update signature help on jump placeholder
-    autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
-augroup end
-function! RestoreCursorPosition()
-    if &filetype !~ 'svn\|commit\c'
+nvim_create_augroups({
+    core = {
+        {'BufReadPost', '*', [[
         if line("'\"") > 1 && line("'\"") <= line("$") |
-            execute 'normal! g`"zvzz' |
-        endif
-    end
-endfunction
-autocmd BufReadPost * call RestoreCursorPosition()
-autocmd FocusGained,BufEnter,FileChangedShell,WinEnter * checktime
-
-" Disables automatic commenting on newline:
-autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
-
-" Enable Goyo by default for mutt writting
-" Goyo's width will be the line limit in mutt.
-autocmd BufRead,BufNewFile /tmp/neomutt* let g:goyo_width=80
-autocmd BufRead,BufNewFile /tmp/neomutt* :Goyo \| set bg=light
-
-" Update binds when sxhkdrc is updated.
-autocmd BufWritePost *sxhkdrc !pkill -USR1 sxhkd
-
-augroup custom_updates
-	autocmd!
-	au BufWritePost ~/.config/xorg/Xdefaults !xrdb -merge ~/.config/xorg/Xdefaults
-	au BufWritePost fonts.conf !fc-cache
-augroup END
-
-
-]], true)
-vim.cmd "autocmd BufWritePost plugins.lua PackerCompile"
+            exe "normal! g'\"" |
+        endif ]], },
+        {'FocusGained,BufEnter,FileChangedShell,WinEnter', '*', 'checktime'},
+        -- Disables automatic commenting on newline:
+        {'FileType', '*', 'setlocal formatoptions-=c formatoptions-=r formatoptions-=o'},
+        -- Update binds when sxhkdrc is updated.
+        {'BufWritePost', '*sxhkdrc', '!pkill -USR1 sxhkd'},
+        {'BufWritePost', 'plugins.lua', 'PackerCompile'},
+        -- This is equivalent to :set autochdir but lets buffer-local
+        -- autocommands change the dir. Autochdir doesn't.
+        {'BufEnter', '*', [[
+        if exists('b:shelley') && exists('b:shelley["path"]')
+            | execute('lcd ' . b:shelley['path'])
+        | endif]]}
+    },
+    custom_updates = {
+        {'BufWritePost', '~/.config/xorg/Xdefaults', '!xrdb -merge ~/.config/xorg/Xdefaults'},
+        {'BufWritePost', 'fonts.conf', '!fc-cache'},
+    },
+    ModeChangeSettings = {
+        -- Clear search context when entering insert mode, which implicitly stops the
+        -- highlighting of whatever was searched for with hlsearch on. It should also
+        -- not be persisted between sessions.
+        {'InsertEnter', '*', [[let @/ = '']]},
+        {'BufReadPre,FileReadPre', '*', [[let @/ = '']]},
+        {'InsertLeave', '*', 'setlocal nopaste'},
+    },
+    HighlightYank = {
+        {'TextYankPost', '*', [[silent! lua require'vim.highlight'.on_yank({timeout=60, higroup="Search"})]]}
+    },
+    CursorLine = {
+        {'VimEnter,WinEnter,BufWinEnter,BufEnter',  '*', 'setlocal cursorline'},
+        {'BufLeave,WinLeave',  '*', 'setlocal nocursorline'}
+    },
+    cocgroup = {
+        -- Update signature help on jump placeholder
+        {'User', 'CocJumpPlaceholder', [[call CocActionAsync('showSignatureHelp')]]}
+    }
+})
