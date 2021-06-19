33725e7ab477de0c69ea321169a5d866b95f1e8f
Neg <serg.zorg@gmail.com>
2021-06-20 00:02:36 +0300

    [nvim] add shelley for shell autocd

--- dev/.config/nvim/lua/01-plugins.lua
+++ dev/.config/nvim/lua/01-plugins.lua
@@ -20,6 +20,7 @@ return require('packer').startup({function(use)
     use {'jamessan/vim-gnupg', ft={'gpg'}, opt=true} -- transparent work with gpg-encrypted files
     use {'nacro90/numb.nvim', config=[[require('numb').setup()]] }
     use {'kopischke/vim-fetch'} -- vim path/to/file.ext:12:3
+    use {'glacambre/shelley'} -- shell autocd
     -- use {'nvim-telescope/telescope.nvim'} -- try it later
     use {'neg-serg/lusty',
           config=function()
