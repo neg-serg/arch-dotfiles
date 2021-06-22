30d61c86af859b7021cc52119ccb65f52c645c93
Neg <serg.zorg@gmail.com>
2021-06-22 12:20:23 +0300

    [nvim] stash yet another statusline stuff

--- dev/.config/nvim/lua/80-statusline.lua
+++ dev/.config/nvim/lua/80-statusline.lua
@@ -19,3 +19,41 @@ function scrollbar_instance(scroll_bar_chars)
     end
     return chars[index]
 end
+
+--[[
+scriptencoding utf-8
+if exists('g:loaded_spaceline') || v:version < 700
+  finish
+endif
+
+let g:loaded_spaceline = 1
+
+let g:symbol = get(g:, 'spaceline_line_symbol', 0)
+let g:seperate_style = get(g:, 'spaceline_seperate_style', 'arrow')
+let g:spaceline_colorscheme = get(g:, 'spaceline_colorscheme', 'space')
+let g:spaceline_shortline_filetype = ['defx','coc-explorer','dbui','vista','vista_markdown','Mundo','MundoDiff']
+let g:spaceline_scroll_bar_chars = get(g:,'spaceline_scroll_bar_chars', [
+  \  '▁', '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█'
+  \  ])
+let g:spaceline_lsp = get(g:,'spaceline_lsp_executive','coc')
+let g:spaceline_git = get(g:,'spaceline_git_tool','coc')
+
+let g:spaceline_errorsign = get(g:,'spaceline_diagnostic_errorsign', '●')
+let g:spaceline_warnsign = get(g:,'spaceline_diagnostic_warnsign', '●')
+let g:spaceline_oksign = get(g:,'spaceline_diagnostic_oksign', '')
+
+let g:sep= {}
+let g:sep = spaceline#seperator#spacelineStyle(g:seperate_style)
+
+
+augroup spaceline
+  autocmd!
+  autocmd FileType,WinEnter,BufWinEnter,BufReadPost,BufWritePost * call spaceline#spacelinetoggle()
+  autocmd Colorscheme * call spaceline#colorscheme_init()
+  autocmd VimResized * call spaceline#spacelinetoggle()
+  autocmd WinLeave * call spaceline#setInActiveStatusLine()
+  autocmd User CocStatusChange,CocGitStatusChange,ClapOnExit call spaceline#spacelinetoggle()
+  autocmd User CocDiagnosticChange call spaceline#spacelinetoggle()
+augroup END "}}}
+
+--]]
