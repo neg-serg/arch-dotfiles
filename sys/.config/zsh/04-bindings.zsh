548eb91d9c721f7d4e6bb3405a4cd6ca6760ed66
Neg <serg.zorg@gmail.com>
2021-06-26 16:07:26 +0300

    [zsh] cleanup z4h functions

--- sys/.config/zsh/04-bindings.zsh
+++ sys/.config/zsh/04-bindings.zsh
@@ -1,43 +1,30 @@
 bindings_init(){
     bindkey -e
-
     typeset -gx KEYTIMEOUT=10
 
     source /usr/share/fzf/completion.zsh
     source /usr/share/fzf/key-bindings.zsh
 
-    autoload -Uz zleiab
-    zle -N zleiab
-    autoload -Uz inplace_mk_dirs
+    autoload -Uz zleiab; zle -N zleiab
+    autoload -Uz inplace_mk_dirs; zle -N inplace_mk_dirs
     autoload -Uz imv
-    autoload -Uz rationalise-dot
-    zle -N rationalise-dot
-    autoload -Uz fg-widget
-    zle -N fg-widget
+    autoload -Uz rationalise-dot; zle -N rationalise-dot
+    autoload -Uz fg-widget; zle -N fg-widget
     autoload -Uz redraw-prompt
+    autoload -Uz magic-abbrev-expand; zle -N magic-abbrev-expand
+    autoload -Uz special-accept-line; zle -N special-accept-line
+    _nothing() {}; zle -N _nothing
+
     autoload -Uz cd-rotate
     cd-back() { cd-rotate +1 }
     cd-forward() { cd-rotate -0 }
-    zle -N cd-back
-    zle -N cd-forward
-    autoload -Uz magic-abbrev-expand
-    zle -N magic-abbrev-expand
-    no-magic-abbrev-expand() { LBUFFER+=' ' }
-    zle -N no-magic-abbrev-expand
-    autoload -Uz special-accept-line
-    zle -N special-accept-line
-    autoload -Uz expand-or-complete-with-dots
-    zle -N expand-or-complete-with-dots
-    _nothing() {}
-    zle -N _nothing
-
-    bindkey "^[" zvm_readkeys_handler
+    zle -N cd-back; zle -N cd-forward
     bindkey "^[-" cd-forward
     bindkey "^[=" cd-back
-    autoload up-line-or-beginning-search
-    zle -N up-line-or-beginning-search
-    autoload down-line-or-beginning-search
-    zle -N down-line-or-beginning-search
+
+    bindkey "^[" zvm_readkeys_handler
+    autoload up-line-or-beginning-search; zle -N up-line-or-beginning-search
+    autoload down-line-or-beginning-search; zle -N down-line-or-beginning-search
     bindkey "^[[A" up-line-or-beginning-search
     bindkey "^[[B" down-line-or-beginning-search
     bindkey "^p" up-line-or-beginning-search
@@ -48,50 +35,29 @@ bindings_init(){
     bindkey "^z" fg-widget
     bindkey '^J' fasd-complete
     bindkey '^@' fasd-complete
-    bindkey "^i" expand-or-complete-with-dots
     bindkey '^m' special-accept-line
     bindkey " "  magic-space
     bindkey ",." zleiab
     bindkey . rationalise-dot
     bindkey -M isearch . self-insert # without this, typing a . aborts incremental history search
-    zle -N inplace_mk_dirs && bindkey '^xM' inplace_mk_dirs # load the lookup subsystem if it's available on the system
-    bindkey -M menuselect '^o' accept-and-infer-next-history
-    bindkey -M menuselect 'i' accept-and-menu-complete
-    bindkey -M menuselect 'o' accept-and-infer-next-history
-    bindkey -M menuselect '\e^M' accept-and-menu-complete
-    bindkey '^[[1;3D' _nothing
-    bindkey '^[[1;3C' _nothing
-    bindkey '^[[1;5A' _nothing
-    bindkey '^[[1;5B' _nothing
+    bindkey '^xM' inplace_mk_dirs # load the lookup subsystem if it's available on the system
 
+    for b in '3D' '3C' '5A' '5B'; bindkey "^[[1;$b" _nothing
     source "${ZDOTDIR}/06-neg-dirs.zsh"
 
-    -z4h-cursor-show() { [[ -t 1 ]] && echoti cnorm || echoti cnorm >"$TTY" }
-
-    autoload -Uz -- '-z4h-move-and-kill'
+    autoload -Uz -- 'z4h-move-and-kill'
     autoload -Uz -- 'z4h-forward-word'
     autoload -Uz -- 'z4h-forward-zword'
     autoload -Uz -- 'z4h-backward-word'
     autoload -Uz -- 'z4h-backward-zword'
-    autoload -Uz -- 'z4h-fzf-complete'
+    autoload -Uz -- 'z4h-main-complete'
+    autoload -Uz -- 'z4h-set-list-colors'
 
-    autoload -Uz -- '-z4h-comp-files'
-    autoload -Uz -- '-z4h-comp-words'
-    function -z4h-cursor-hide() { [[ -t 1 ]] && echoti civis || echoti civis >"$TTY" }
-    autoload -Uz -- '-z4h-cursor-show'
-    autoload -Uz -- '-z4h-get-cursor-pos'
-    autoload -Uz -- '-z4h-main-complete'
-    autoload -Uz -- '-z4h-sanitize-word-prefix'
-    autoload -Uz -- '-z4h-set-list-colors'
-    autoload -Uz -- '-z4h-show-dots'
-    autoload -Uz -- '-z4h-insert-all'
+    z4h-kill-word() { z4h-move-and-kill z4h-forward-word }
+    z4h-backward-kill-word() { z4h-move-and-kill z4h-backward-word  }
+    z4h-kill-zword() { z4h-move-and-kill z4h-forward-zword  }
+    z4h-backward-kill-zword() { z4h-move-and-kill z4h-backward-zword }
 
-    z4h-kill-word() { -z4h-move-and-kill z4h-forward-word }
-    z4h-backward-kill-word() { -z4h-move-and-kill z4h-backward-word  }
-    z4h-kill-zword() { -z4h-move-and-kill z4h-forward-zword  }
-    z4h-backward-kill-zword() { -z4h-move-and-kill z4h-backward-zword }
-
-    zle -N z4h-expand
     zle -N z4h-forward-word
     zle -N z4h-kill-word
     zle -N z4h-backward-word
@@ -101,29 +67,24 @@ bindings_init(){
     zle -N z4h-backward-zword
     zle -N z4h-backward-kill-zword
 
-    zle -N z4h-fzf-complete
-    zle -N z4h-fzf-history
-    zle -N z4h-fzf-dir-history
+    bindkey   '^[d'     z4h-kill-word # alt+d
+    bindkey   '^[D'     z4h-kill-word # alt+D
+    bindkey   '^W'      z4h-backward-kill-word # ctrl+w
 
-    bindkey   '^[d'     z4h-kill-word                  # alt+d
-    bindkey   '^[D'     z4h-kill-word                  # alt+D
-    bindkey   '^[[3;5~' z4h-kill-word                  # ctrl+del
-    bindkey   '^[[3;3~' z4h-kill-word                  # alt+del
-    # Delete previous word.
-    bindkey   '^W'      z4h-backward-kill-word         # ctrl+w
-    bindkey   '^[^?'    z4h-backward-kill-word         # alt+bs
-    bindkey   '^[^H'    z4h-backward-kill-word         # ctrl+alt+bs
+    zstyle ':fzf-tab:*' continuous-trigger 'ctrl-space'
 
-    fzf-tab-partial-and-complete() {
-        if [[ $LASTWIDGET = 'fzf-tab-partial-and-complete' ]]; then
+    tab_then_fzf() {
+        if [[ $LASTWIDGET = 'tab_then_fzf' ]]; then
             fzf-tab-complete
         else
-            zle complete-word
+            echo -n "\e[36m··\e[0m"
+            zle expand-or-complete
+            zle redisplay
         fi
     }
 
-    zle -N fzf-tab-partial-and-complete
-    bindkey '^@' fzf-tab-partial-and-complete
+    zle -N tab_then_fzf
+    bindkey '^@' tab_then_fzf
 }
 ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLOCK
 ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_UNDERLINE
