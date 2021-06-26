1804bf8ea32614a3158942fd97963f8f7a56391c
Neg <serg.zorg@gmail.com>
2021-06-26 16:51:54 +0300

    [nvim] initial telescope config

---
+++ dev/.config/nvim/lua/plugcfg/telescope.lua
@@ -0,0 +1,63 @@
+require('telescope').setup{
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case'
+    },
+    prompt_position = "top",
+    prompt_prefix = "❯> ",
+    selection_caret = "> ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
+    layout_strategy = "vertical",
+    layout_defaults = {
+      horizontal = {mirror = false},
+      vertical = {mirror = false},
+    },
+    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+    file_ignore_patterns = {},
+    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+    shorten_path = true,
+    winblend = 0,
+    width = 0.75,
+    results_height = 1,
+    results_width = 0.8,
+    border = {},
+    borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+    color_devicons = true,
+    use_less = false,
+    set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
+    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+    -- Developer configurations: Not meant for general override
+    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+  },
+  pickers = {
+    find_files = {
+      theme = "ivy",
+      border = false,
+      previewer = false,
+      layout_config = {height = 8},
+      sorting_strategy = "descending",
+      prompt_position = "bottom",
+      prompt_title = false,
+    },
+    oldfiles = {
+      theme = "ivy",
+      border = false,
+      previewer = false,
+      layout_config = {height = 8},
+      sorting_strategy = "descending",
+      prompt_position = "bottom",
+      prompt_title = false,
+    },
+  }
+}
