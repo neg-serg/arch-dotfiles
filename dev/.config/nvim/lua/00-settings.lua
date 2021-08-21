362a1e1eba795e1b1fbebc42fa0a1d4fc0fa0974
Neg <serg.zorg@gmail.com>
2021-08-21 14:45:51 +0300

    [nvim] disable builtins

--- dev/.config/nvim/lua/00-settings.lua
+++ dev/.config/nvim/lua/00-settings.lua
@@ -1,3 +1,27 @@
+-- thx to https://www.reddit.com/r/neovim/comments/opipij/guide_tips_and_tricks_to_reduce_startup_and/
+local disabled_built_ins = {
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "gzip",
+    "zip",
+    "zipPlugin",
+    "tar",
+    "tarPlugin",
+    "getscript",
+    "getscriptPlugin",
+    "vimball",
+    "vimballPlugin",
+    "2html_plugin",
+    "logipat",
+    "rrhelper",
+    "spellfile_plugin",
+    "matchit"
+}
+for _, plugin in pairs(disabled_built_ins) do
+    vim.g["loaded_" .. plugin] = 1
+end
 vim.opt.exrc = false
 vim.opt.secure = true
 vim.opt.termguicolors = true
