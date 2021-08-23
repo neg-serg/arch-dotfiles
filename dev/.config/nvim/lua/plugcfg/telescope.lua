76489df085ea831fa9b8c60069f4b92493d65696
Neg <serg.zorg@gmail.com>
2021-08-24 01:45:38 +0300

    [nvim] update telescope settings

--- dev/.config/nvim/lua/plugcfg/telescope.lua
+++ dev/.config/nvim/lua/plugcfg/telescope.lua
@@ -13,24 +13,33 @@ require('telescope').setup{
     sorting_strategy = "descending",
     layout_strategy = "vertical",
     layout_config = {
+      prompt_position = "top",
+      -- prompt_position = "bottom",
       horizontal = {mirror = false},
       vertical = {mirror = false},
       height = 8
     },
-    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+    file_sorter = require("telescope.sorters").get_fzy_sorter,
     file_ignore_patterns = {},
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+    path_display = { shorten = 5 },
     winblend = 0,
     border = {},
     borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
     color_devicons = true,
     use_less = false,
-    set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
+    set_env = {['COLORTERM'] = 'truecolor'},
     file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
     grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
     qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
     -- Developer configurations: Not meant for general override
     buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+  },
+  extensions = {
+    fzy_native = {
+      override_generic_sorter = false,
+      override_file_sorter = true,
+    },
   },
   pickers = {
     find_files = {
