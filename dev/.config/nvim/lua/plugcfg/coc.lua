c13de073b35a8bda56656d1f35e5a1523ced0c4b
Neg <serg.zorg@gmail.com>
2021-08-30 16:41:13 +0300

    [nvim] coc: CS

--- dev/.config/nvim/lua/plugcfg/coc.lua
+++ dev/.config/nvim/lua/plugcfg/coc.lua
@@ -6,13 +6,13 @@ vim.g.coc_status_error_sign = ''
 vim.g.coc_snippet_next = '<tab>'
 api.nvim_command('inoremap <expr> <C-Space> pumvisible() ? coc#_select_confirm() : coc#expandableOrJumpable() ? "" : "<Space>"')
 api.nvim_command('inoremap <silent><expr> <Tab> pumvisible() ? coc#_select_confirm() : coc#expandableOrJumpable() ? "" : <SID>check_back_space() ? "<TAB>" : coc#refresh()')
+api.nvim_command('inoremap <expr> <S-Tab> pumvisible() ? "<C-p>" : "<S-Tab>"')
 api.nvim_command([[
 function! s:check_back_space() abort
   let col = col('.') - 1
   return !col || getline('.')[col - 1]  =~# '\s'
 endfunction
 ]])
-api.nvim_command('inoremap <expr> <S-Tab> pumvisible() ? "<C-p>" : "<S-Tab>"')
 api.nvim_command('nnoremap <leader>d <Plug>(coc-diagnostic-prev)')
 api.nvim_command('nnoremap <leader>s <Plug>(coc-diagnostic-next)')
 -- Remap keys for gotos
@@ -46,6 +46,6 @@ api.nvim_command('omap ic <Plug>(coc-classobj-i)')
 api.nvim_command('xmap ac <Plug>(coc-classobj-a)')
 api.nvim_command('omap ac <Plug>(coc-classobj-a)')
 -- Remap keys for applying codeAction to the current buffer.
-api.nvim_command('nmap <leader>ac  <Plug>(coc-codeaction)')
+api.nvim_command('nmap <leader>ac <Plug>(coc-codeaction)')
 -- Apply AutoFix to problem on the current line.
-api.nvim_command('nmap <leader>qf  <Plug>(coc-fix-current)')
+api.nvim_command('nmap <leader>qf <Plug>(coc-fix-current)')
