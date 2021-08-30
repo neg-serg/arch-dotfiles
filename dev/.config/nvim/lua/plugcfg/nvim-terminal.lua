f7c5491ce2f622a6042ca957a4231c315879c78d
Neg <serg.zorg@gmail.com>
2021-08-30 22:07:19 +0300

    [nvim] nvim-terminal sane defaults

---
+++ dev/.config/nvim/lua/plugcfg/nvim-terminal.lua
@@ -0,0 +1,5 @@
+require('nvim-terminal').setup({
+    window = { position = 'botright', split = 'sp', width = 50, height = 10},
+    disable_default_keymaps = false,
+    toggle_keymap = '<leader>l',
+})
