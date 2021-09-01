54a30fee705e9009816bc13a343900f4c2618b21
Neg <serg.zorg@gmail.com>
2021-09-01 18:43:00 +0300

    [nvim] remove useless height

--- dev/.config/nvim/lua/plugcfg/telescope.lua
+++ dev/.config/nvim/lua/plugcfg/telescope.lua
@@ -16,7 +16,6 @@ require('telescope').setup{
       prompt_position = "bottom",
       horizontal = {mirror = false},
       vertical = {mirror = false},
-      height = 8
     },
     file_sorter = require("telescope.sorters").get_fzy_sorter,
     file_ignore_patterns = { "node_modules", ".git" },
