f78635d354d9148b389487db3dfc39f8ccd6faad
Neg <serg.zorg@gmail.com>
2021-11-11 16:21:33 +0300

    [nvim] fix autopairs

---
+++ dev/.config/nvim/lua/plugcfg/nvim-autopairs.lua
@@ -0,0 +1,14 @@
+require('nvim-autopairs').setup({
+    disable_filetype = { "TelescopePrompt" },
+    disable_in_macro = false,  -- disable when recording or executing a macro
+    ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]],"%s+", ""),
+    enable_moveright = true,
+    enable_afterquote = true,  -- add bracket pairs after quote
+    enable_check_bracket_line = true,  --- check bracket in same line
+    check_ts = false,
+    map_bs = true,  -- map the <BS> key
+    map_c_w = true, -- map <C-w> to delete an pair if possible
+})
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
