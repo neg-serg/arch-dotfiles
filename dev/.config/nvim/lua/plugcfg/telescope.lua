bcce1907f29069a44aa782d94d02cab9c29a166b
Neg <serg.zorg@gmail.com>
2021-12-20 02:43:47 +0300

    [nvim] use fzf-telescope

--- dev/.config/nvim/lua/plugcfg/telescope.lua
+++ dev/.config/nvim/lua/plugcfg/telescope.lua
@@ -35,14 +35,17 @@ require('telescope').setup{
     buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
   },
   extensions = {
-    fzy_native = {
-      override_generic_sorter = false,
-      override_file_sorter = true,
-    },
     frecency = {
       show_unindexed = true,
       db_safe_mode = false,
       auto_validate = true,
+    },
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
     },
   },
   pickers = {
@@ -66,3 +69,5 @@ require('telescope').setup{
     },
   },
 }
+
+require('telescope').load_extension('fzf')
