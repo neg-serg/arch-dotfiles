3cf233b27bd2465ec908f07ae5d9c2a3b47b6f6d
Neg <serg.zorg@gmail.com>
2022-01-21 23:49:52 +0300

    [nvim] treesitter use only maintained

--- dev/.config/nvim/lua/plugcfg/treesitter.lua
+++ dev/.config/nvim/lua/plugcfg/treesitter.lua
@@ -14,7 +14,7 @@ parser_configs.norg = {
 }
 
 tsconf.setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = {},  -- list of language that will be disabled
