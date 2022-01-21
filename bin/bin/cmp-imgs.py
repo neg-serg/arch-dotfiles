d59ebf1115801ebaf39186c883b4319f46830d6a
Neg <serg.zorg@gmail.com>
2022-01-22 01:09:28 +0300

    [bin] cleanup cmp-imgs

--- bin/bin/cmp-imgs.py
+++ bin/bin/cmp-imgs.py
@@ -4,10 +4,7 @@ import sys
 from PIL import Image
 from imagehash import dhash
 
-try:
-    if dhash(Image.open(sys.argv[1])) == dhash(Image.open(sys.argv[2])):
-        sys.exit(0)
-    else:
-        sys.exit(2)
-except Exception:
-    sys.exit(1)
+if dhash(Image.open(sys.argv[1])) == dhash(Image.open(sys.argv[2])):
+    sys.exit(0)
+else:
+    sys.exit(2)
