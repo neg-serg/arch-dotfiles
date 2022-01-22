b3ae895a076d566da7eb5b2a1a51fb1d740e2a07
Neg <serg.zorg@gmail.com>
2022-01-22 18:21:24 +0300

    [nvim] treesitter match integration

--- dev/.config/nvim/lua/plugcfg/treesitter.lua
+++ dev/.config/nvim/lua/plugcfg/treesitter.lua
@@ -18,18 +18,18 @@ tsconf.setup {
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = {},  -- list of language that will be disabled
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
+  },
+  matchup = {
+    enable = true, -- mandatory, false will disable the whole extension
+    disable = {},  -- optional, list of language that will be disabled
   },
   rainbow = {
     enable = true,
-    extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+    extended_mode = false, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
     max_file_lines = 4000, -- Do not enable for files with more than 1000 lines, int
     colors = {'#365e96', '#315587', '#2e5080', '#294873', '#26436b', '#21395c', '#1d3352'},
   },
   indent = {enable = {"javascriptreact"}},
-  autotag = {enable = true},
+  autotag = {enable = false},
 }
