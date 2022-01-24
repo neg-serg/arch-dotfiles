351ce2b607710d467382792d230d47921f87be94
Neg <serg.zorg@gmail.com>
2022-01-25 00:40:39 +0300

    [nvim] install everything except phpdoc

--- dev/.config/nvim/lua/plugcfg/treesitter.lua
+++ dev/.config/nvim/lua/plugcfg/treesitter.lua
@@ -14,7 +14,8 @@ parser_configs.norg = {
 }
 
 tsconf.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ignore_install = { "phpdoc" },
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = {},  -- list of language that will be disabled
