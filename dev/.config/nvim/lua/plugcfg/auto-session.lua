25ee69566ba3c55b58a9a2a8fa86669029677995
Neg <serg.zorg@gmail.com>
2022-01-29 01:10:12 +0300

    [nvim] cleanup auto-session

--- dev/.config/nvim/lua/plugcfg/auto-session.lua
+++ dev/.config/nvim/lua/plugcfg/auto-session.lua
@@ -1,12 +1,9 @@
 require('auto-session').setup({
   log_level = 'error',
-  pre_save_cmds = { 'NvimTreeClose', 'cclose', 'lua vim.notify.dismiss()' },
+  pre_save_cmds = {},
   auto_session_enable_last_session = true,
   auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
   auto_session_enabled = true,
-  auto_save_enabled = nil,
-  auto_restore_enabled = nil,
-  auto_session_suppress_dirs = nil
 })
 
 map('n', '<leader>sl', ':silent RestoreSession<cr>')
