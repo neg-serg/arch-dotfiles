0feaf3878ef78402bab090a9588b37921673e3d8
Neg <serg.zorg@gmail.com>
2022-02-01 12:44:45 +0300

    [nvim] cleanup and update gitsigns config

--- dev/.config/nvim/lua/plugcfg/gitsigns.lua
+++ dev/.config/nvim/lua/plugcfg/gitsigns.lua
@@ -14,19 +14,10 @@ require('gitsigns').setup {
     buffer = true,
     ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
     ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
-    ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-    ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-    -- Text objects
-    ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
-    ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
   },
-  watch_index = {interval = 500, follow_files = true},
+  watch_gitdir = {interval = 500, follow_files = true},
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
-  use_internal_diff = true,  -- If luajit is present
+  diff_opts = {internal = true},  -- If luajit is present
 }
