278debef109eb7570d6652a9a9c6c9ee43fc7db1
Neg <serg.zorg@gmail.com>
2022-02-02 16:59:18 +0300

    [nvim] proper conflict-marker config

--- dev/.config/nvim/lua/01-plugins.lua
+++ dev/.config/nvim/lua/01-plugins.lua
@@ -104,15 +104,7 @@ return require('packer').startup({function(use)
 -- ┌───────────────────────────────────────────────────────────────────────────────────┐
 -- │ █▓▒░ DCVS                                                                         │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
-    use {'rhysd/conflict-marker.vim',
-        config=[[
-            vim.cmd('highlight ConflictMarkerBegin guibg=#2f7366')
-            vim.cmd('highlight ConflictMarkerOurs guibg=#2e5049')
-            vim.cmd('highlight ConflictMarkerTheirs guibg=#344f69')
-            vim.cmd('highlight ConflictMarkerEnd guibg=#2f628e')
-            vim.cmd('highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81')
-        ]]
-    } -- good conflict marker
+    use {'rhysd/conflict-marker.vim', config=[[require'plugcfg/conflict-marker']]} -- good conflict marker
     use {'sindrets/diffview.nvim', cmd={'DiffviewLoad'}, config=[[require'plugcfg/diffview']]} -- diff view for multiple files
     use 'tpope/vim-fugitive' -- git stuff old
 end, config={
