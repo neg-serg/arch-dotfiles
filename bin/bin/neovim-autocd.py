c8821ffed7ceaec537507ca6b4e40995854ac953
Neg <serg.zorg@gmail.com>
2022-02-03 00:40:39 +0300

    [bin] fix neovim-autocd

--- bin/bin/neovim-autocd.py
+++ bin/bin/neovim-autocd.py
@@ -2,7 +2,7 @@
 import os
 import neovim
 
-nvim = neovim.attach('socket', path='/tmp/nvimsocket')
+nvim = neovim.attach('tcp', address='localhost', port=7777)
 nvim.vars['__autocd_cwd'] = os.getcwd()
 nvim.command('execute "lcd" fnameescape(g:__autocd_cwd)')
 del nvim.vars['__autocd_cwd']
