376a21e8ae0071bb5cdc1aeaa7eb583f19edd57e
Neg <serg.zorg@gmail.com>
2022-02-05 22:49:28 +0300

    [nvim] add packer commands

--- dev/.config/nvim/init.lua
+++ dev/.config/nvim/init.lua
@@ -24,7 +24,18 @@ function loadrequire(module)
     res = pcall(requiref,module)
 end
 
-loadrequire 'impatient'
+local present, impatient = pcall(require, "impatient")
+if present then
+   impatient.enable_profile()
+end
+
+vim.cmd "silent! command PackerClean lua require 'plugins' require('packer').clean()"
+vim.cmd "silent! command PackerCompile lua require 'plugins' require('packer').compile()"
+vim.cmd "silent! command PackerInstall lua require 'plugins' require('packer').install()"
+vim.cmd "silent! command PackerStatus lua require 'plugins' require('packer').status()"
+vim.cmd "silent! command PackerSync lua require 'plugins' require('packer').sync()"
+vim.cmd "silent! command PackerUpdate lua require 'plugins' require('packer').update()"
+
 loadrequire 'nvim_utils'
 loadrequire '00-helpers'
 loadrequire '00-settings'
