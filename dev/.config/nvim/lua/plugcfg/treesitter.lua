594568b77211f41286ef22f56b0c1e2b3f0d257e
Neg <serg.zorg@gmail.com>
2022-02-08 16:31:33 +0300

    [nvim] treesitter better norg

--- dev/.config/nvim/lua/plugcfg/treesitter.lua
+++ dev/.config/nvim/lua/plugcfg/treesitter.lua
@@ -5,11 +5,28 @@ if not tsconf then
 end
 
 local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+
 parser_configs.norg = {
     install_info = {
         url = "https://github.com/nvim-neorg/tree-sitter-norg",
         files = { "src/parser.c", "src/scanner.cc" },
-        branch = "main"
+        branch = "main",
+    },
+}
+
+parser_configs.norg_table = {
+    install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
+        files = { "src/parser.c" },
+        branch = "main",
+    },
+}
+
+parser_configs.norg_meta = {
+    install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
+        branch = "main",
+        files = { "src/parser.c" },
     },
 }
 
