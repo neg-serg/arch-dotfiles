3e47d10bff52c99f9205231277fb4748c2e62022
Neg <serg.zorg@gmail.com>
2022-02-13 14:16:22 +0300

    [nvim] pure lua colorscheme

--- dev/.config/nvim/lua/01-plugins.lua
+++ dev/.config/nvim/lua/01-plugins.lua
@@ -117,7 +117,7 @@ return require('packer').startup({function(use)
     use 'folke/todo-comments.nvim' -- better highlight TODO, HACK, etc
     use {'kyazdani42/nvim-web-devicons', config=[[require'plugcfg/nvim-web-devicons']]} -- fancy webicons
     use {'yamatsum/nvim-nonicons', requires={'kyazdani42/nvim-web-devicons'}}
-    use {'neg-serg/neg', config=[[vim.cmd('colorscheme neg')]]} -- my pure-dark neovim colorscheme
+    use {'neg-serg/neg', config=[[require'neg']]} -- my pure-dark neovim colorscheme
     use {'wuelnerdotexe/vim-enfocado'} -- nice colorscheme approach
     use {'RRethy/vim-hexokinase', run='make hexokinase'} -- best way to display colors in the file
     use 'tjdevries/colorbuddy.vim' -- for future experiments with new colorschemes
