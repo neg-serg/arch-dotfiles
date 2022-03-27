21c80d9eb9a150b4e0e65e83ac768c7e88c5a9a2
Neg <serg.zorg@gmail.com>
2022-03-27 23:21:18 +0300

    [zsh] code style

--- sys/.config/zsh/02-cmds.zsh
+++ sys/.config/zsh/02-cmds.zsh
@@ -24,17 +24,17 @@ alias mk='mkdir -p'
 alias rd='rmdir'
 if _exists ugrep; then
     # ------------------------------------------------------------------
-    alias grep='ugrep -G'  # search with basic regular expressions (BRE)
+    alias grep='ugrep -G' # search with basic regular expressions (BRE)
     alias egrep='ugrep -E' # search with extended regular expressions (ERE)
     alias fgrep='ugrep -F' # find string(s)
     alias pgrep='ugrep -P' # search with Perl regular expressions
     alias xgrep='ugrep -W' # search (ERE) and output text or hex for binary
     # ---------------------------------------------------------------------
-    alias zgrep='ugrep -zG'   # search compressed files and archives with BRE
-    alias zegrep='ugrep -zE'   # search compressed files and archives with ERE
-    alias zfgrep='ugrep -zF'   # find string(s) in compressed files and/or archives
-    alias zpgrep='ugrep -zP'   # search compressed files and archives with Perl regular expressions
-    alias zxgrep='ugrep -zW'   # search (ERE) compressed files/archives and output text or hex for binary
+    alias zgrep='ugrep -zG' # search compressed files and archives with BRE
+    alias zegrep='ugrep -zE' # search compressed files and archives with ERE
+    alias zfgrep='ugrep -zF' # find string(s) in compressed files and/or archives
+    alias zpgrep='ugrep -zP' # search compressed files and archives with Perl regular expressions
+    alias zxgrep='ugrep -zW' # search (ERE) compressed files/archives and output text or hex for binary
     # ---------------------------------------------------------------------------------------------------
     alias xdump='ugrep -X ""' # hexdump files without searching
 else
