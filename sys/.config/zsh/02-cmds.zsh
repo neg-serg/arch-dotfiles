ef5c798a20f2fbbc3bf003f8c3b7fc65b42af57d
Neg <serg.zorg@gmail.com>
2022-04-09 13:27:45 +0300

    [zsh] remove yt functions

--- sys/.config/zsh/02-cmds.zsh
+++ sys/.config/zsh/02-cmds.zsh
@@ -95,8 +95,8 @@ _exists mpc && {
     alias unlove='mpc sendmessage mpdas unlove'
 }
 _exists yt-dlp && {
-    yt() { noglob yt-dlp -q --downloader aria2c -f "(bestvideo+bestaudio/best)" "$@" }
-    ytt() { ytfzf --preview-side=left -t "$@" }
+    alias yt='noglob yt-dlp -q --downloader aria2c -f "(bestvideo+bestaudio/best)"'
+    alias ytt='noglob ytfzf --preview-side=left -t'
 }
 _exists wget2 && alias wget='wget2'
 _exists imgur_screenshot && alias img='imgur-screenshot'
