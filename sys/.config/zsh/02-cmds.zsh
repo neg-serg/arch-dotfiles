8ab40a128315b2cc2002466af3de5a17ea16c0f0
Neg <serg.zorg@gmail.com>
2022-04-24 14:35:05 +0300

    [zsh] add rmorphans alias

--- sys/.config/zsh/02-cmds.zsh
+++ sys/.config/zsh/02-cmds.zsh
@@ -60,7 +60,7 @@ _exists rg && {
 alias sort='sort --parallel 8 -S 16M'
 alias :q="exit"
 alias emptydir='ls -ld **/*(/^F)'
-_exists paru && alias yay='paru'
+_exists paru && {alias yay='paru'; alias rmorphans='paru -Rs $(paru -Qqdt)'}
 _exists sudo && {alias sudo='sudo '; alias s='sudo '}
 _exists plocate && alias locate='plocate'
 _exists dd && alias dd='dd status=progress'
