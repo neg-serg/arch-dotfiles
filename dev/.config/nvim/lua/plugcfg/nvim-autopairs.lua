51b535d11e245ea1bff4e42f16e75459db308fce
Neg <serg.zorg@gmail.com>
2022-05-06 00:59:35 +0300

    [nvim] update autopairs

--- dev/.config/nvim/lua/plugcfg/nvim-autopairs.lua
+++ dev/.config/nvim/lua/plugcfg/nvim-autopairs.lua
@@ -1,11 +1,13 @@
 require('nvim-autopairs').setup({
     disable_filetype = {"TelescopePrompt"},
     disable_in_macro = false,  -- disable when recording or executing a macro
+    disable_in_visualblock = false, -- disable when insert after visual block mode
     ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]],"%s+", ""),
     enable_moveright = true,
     enable_afterquote = true,  -- add bracket pairs after quote
-    enable_check_bracket_line = true,  --- check bracket in same line
+    enable_check_bracket_line = true,  -- check bracket in same line
     check_ts = true,
+    map_cr = true,
     map_bs = true,  -- map the <BS> key
     map_c_w = true, -- map <C-w> to delete an pair if possible
 })
