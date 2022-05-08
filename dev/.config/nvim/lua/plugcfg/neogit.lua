eb8a70c12f6ac9cd882239cc2f0756de685032ef
Neg <serg.zorg@gmail.com>
2022-05-08 18:56:11 +0300

    [nvim] additional neogit config

--- dev/.config/nvim/lua/plugcfg/neogit.lua
+++ dev/.config/nvim/lua/plugcfg/neogit.lua
@@ -4,7 +4,7 @@ neogit.setup {
   disable_signs = false,
   disable_hint = false,
   disable_context_highlighting = false,
-  disable_commit_confirmation = false,
+  disable_commit_confirmation = true,
   -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
   -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
   auto_refresh = true,
@@ -18,7 +18,7 @@ neogit.setup {
   integrations = {
     -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
     -- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
-    diffview = false
+    diffview = true
   },
   -- Setting any section to `false` will make the section not render at all
   sections = {
@@ -34,10 +34,7 @@ neogit.setup {
   mappings = {
     -- modify status buffer mappings
     status = {
-      -- Adds a mapping with "B" as key that does the "BranchPopup" command
-      ["B"] = "BranchPopup",
-      -- Removes the default mapping of "s"
-      ["s"] = "",
+      ["B"] = "BranchPopup", -- Adds a mapping with "B" as key that does the "BranchPopup" command
     }
   }
 }
