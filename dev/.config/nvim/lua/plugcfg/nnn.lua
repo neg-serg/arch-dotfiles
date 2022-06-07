d487a4ce0a131db8970a6094adb9625d9c1f2b2c
Neg <serg.zorg@gmail.com>
2022-06-07 14:15:10 +0300

    [nvim] nnn: cleanup

--- dev/.config/nvim/lua/plugcfg/nnn.lua
+++ dev/.config/nvim/lua/plugcfg/nnn.lua
@@ -1,16 +1,22 @@
 require("nnn").setup({
 	picker = {
-		cmd = "nnn",       -- command override (-p flag is implied)
 		style = {
 			width = 1.0,      -- percentage relative to terminal size when < 1, absolute otherwise
 			height = 1.0,     -- ^
 			xoffset = 0.0,    -- ^
 			yoffset = 0.0,    -- ^
-			border = "single" -- border decoration for example "rounded"(:h nvim_open_win)
+			border = "none"   -- border decoration for example "rounded"(:h nvim_open_win)
 		},
-		session = "",      -- or "global" / "local" / "shared"
+		session = "shared",      -- or "global" / "local" / "shared"
 	},
+    auto_open = {
+        setup = nil,
+        tabpage = nil,
+        empty = false,
+        ft_ignore = { "gitcommit" }
+    },
 	replace_netrw = "picker",
 	auto_close = false,  -- close tabpage/nvim when nnn is last window
+	buflisted = false,   -- wether or not nnn buffers show up in the bufferlist
 	window_nav = "<C-l>"
 })
