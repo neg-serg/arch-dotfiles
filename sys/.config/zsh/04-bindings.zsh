1b4cc513e6396f86f939b4b888efe5ca33cd646e
Neg <serg.zorg@gmail.com>
2022-06-19 04:05:03 +0300

    [zsh] add check for fzf bindings existence

--- sys/.config/zsh/04-bindings.zsh
+++ sys/.config/zsh/04-bindings.zsh
@@ -13,8 +13,8 @@ neg_dirs(){
 bindkey -e
 typeset -gx KEYTIMEOUT=10
 
-source /usr/share/fzf/completion.zsh
-source /usr/share/fzf/key-bindings.zsh
+[[ -f /usr/share/fzf/completion.zsh ]] && source /usr/share/fzf/completion.zsh
+[[ -f /usr/share/fzf/key-bindings.zsh ]] && source /usr/share/fzf/key-bindings.zsh
 
 autoload -Uz zleiab && zle -N zleiab
 autoload -Uz inplace_mk_dirs && zle -N inplace_mk_dirs
