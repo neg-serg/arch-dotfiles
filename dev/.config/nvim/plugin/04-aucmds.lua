e716074503d1d050e1b9ece1e2b3e60c3fa743c9
Neg <serg.zorg@gmail.com>
2022-06-28 19:32:21 +0300

    [nvim] cleanup aucmds

--- dev/.config/nvim/plugin/04-aucmds.lua
+++ dev/.config/nvim/plugin/04-aucmds.lua
@@ -8,6 +8,9 @@ local hi_yank = gr("hi_yank", {clear=true})
 local cursor_line = gr("cursor_line", {clear=true})
 local statusline = gr("statusline", {clear=true})
 
+au({'BufWinEnter','BufEnter','VimEnter'}, {callback=function() vim.wo.statusline=NegStatusline('active') end, group=statusline})
+au({'BufWinLeave','BufLeave','WinLeave'}, {callback=function() vim.wo.statusline=NegStatusline('inactive') end, group=statusline})
+
 au({'Filetype'}, {command='setlocal formatoptions-=c formatoptions-=r formatoptions-=o', group=main})
 au({'FocusGained','BufEnter','FileChangedShell','WinEnter'}, {command='checktime', group=main})
 -- Disables automatic commenting on newline:
@@ -45,18 +48,4 @@ au({'BufWritePost'}, {pattern='fonts.conf', command='!fc-cache', group=custom_up
 au({'TextYankPost'}, {
     callback=function() require'vim.highlight'.on_yank({timeout=60, higroup="Search"}) end,
     group=hi_yank
-})
-
-au({'VimEnter','WinEnter','BufWinEnter','BufEnter'}, {command='setlocal cursorline', group=cursor_line})
-au({'BufLeave','WinLeave'}, {command='setlocal nocursorline', group=cursor_line})
-
-au({'WinEnter','BufEnter','BufWinEnter'}, {callback=function() vim.wo.statusline=NegStatusline('active') end, group=statusline})
-au({'WinLeave','BufLeave','BufWinLeave'}, {callback=function() vim.wo.statusline=NegStatusline('inactive') end, group=statusline})
-
--- Fix alacritty resize: github.com/neovim/neovim/issues/11330
-au({"VimEnter"}, {
-  callback = function()
-    local pid, WINCH = vim.fn.getpid(), vim.loop.constants.SIGWINCH
-    vim.defer_fn(function() vim.loop.kill(pid, WINCH) end, 20)
-  end
 })
