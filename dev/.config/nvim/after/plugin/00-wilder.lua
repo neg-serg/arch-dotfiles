2a75efcfbba7717d0ad280e7897d821a793a5d25
Neg <serg.zorg@gmail.com>
2022-07-08 00:02:03 +0300

    [nvim] remove wilder fuzzy completion

--- dev/.config/nvim/after/plugin/00-wilder.lua
+++ dev/.config/nvim/after/plugin/00-wilder.lua
@@ -13,14 +13,7 @@ if _G.packer_plugins["wilder.nvim"] and _G.packer_plugins["wilder.nvim"].loaded 
     wilder.set_option('noselect', 0)
     wilder.set_option('pipeline', {
       wilder.branch(
-        wilder.cmdline_pipeline({
-          fuzzy = 2,
-        }),
-        wilder.python_file_finder_pipeline({
-            file_command = {'fd', '-tf'},
-            dir_command = {'fd', '-td'},
-            filters = {'fuzzy_filter', 'difflib_sorter'},
-        }),
+        wilder.cmdline_pipeline({ fuzzy = 2, }),
         wilder.vim_search_pipeline()
       )
     })
