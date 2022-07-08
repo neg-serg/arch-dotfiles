c4c0210a133ae0cd424e34cde80eca09c1bc1544
Neg <serg.zorg@gmail.com>
2022-07-08 15:23:13 +0300

    [nvim] cleanup sort operator

--- dev/.config/nvim/plugin/62-sort-operator.lua
+++ dev/.config/nvim/plugin/62-sort-operator.lua
@@ -4,12 +4,4 @@ function! Sort(type, ...)
     '[,']sort
 endfunction
 nmap <silent> <leader>s :set opfunc=Sort<CR>g@
-
-" Use a bunch of standard UNIX commands for quick an dirty
-" whitespace-based alignment
-function! Align()
-	'<,'>!column -t|sed 's/  \(\S\)/ \1/g'
-	normal gv=
-endfunction
-xnoremap <silent> <leader><S-a> :<C-u>silent call Align()<CR>
 ]], true)
