e794a93a1912f40e8518b12b806a984cccd93e78
Neg <serg.zorg@gmail.com>
2022-07-10 15:35:04 +0300

    [nvim] do not load generic filetypes

--- dev/.config/nvim/plugin/00-settings.lua
+++ dev/.config/nvim/plugin/00-settings.lua
@@ -31,6 +31,7 @@ end
 -- use opt-in filetype.lua instead of vimscript default
 -- EXPERIMENTAL: https://github.com/neovim/neovim/pull/16600
 vim.g.do_filetype_lua = 1
+vim.g.did_load_filetypes = 0
 vim.opt.exrc = false
 vim.opt.secure = true
 vim.opt.termguicolors = true
