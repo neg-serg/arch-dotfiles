a52e481c5aa73ad7e9f61d852b4ad547cd86d078
Neg <serg.zorg@gmail.com>
2022-07-18 11:56:52 +0300

    [nvim] initial headlines config

---
+++ dev/.config/nvim/lua/cfg/headlines.lua
@@ -0,0 +1,52 @@
+require("headlines").setup {
+    markdown = {
+        source_pattern_start = "^```",
+        source_pattern_end = "^```$",
+        dash_pattern = "^---+$",
+        headline_pattern = "^#+",
+        headline_highlights = { "Headline1", "Headline2" },
+        codeblock_highlight = "CodeBlock",
+        dash_highlight = "Dash",
+        fat_headlines = true,
+    },
+    rmd = {
+        source_pattern_start = "^```",
+        source_pattern_end = "^```$",
+        dash_pattern = "^---+$",
+        headline_pattern = "^#+",
+        headline_highlights = { "Headline1", "Headline2" },
+        codeblock_sign = "CodeBlock",
+        dash_highlight = "Dash",
+        fat_headlines = true,
+    },
+    vimwiki = {
+        source_pattern_start = "^{{{%a+",
+        source_pattern_end = "^}}}$",
+        dash_pattern = "^---+$",
+        headline_pattern = "^=+",
+        headline_highlights = { "Headline1", "Headline2" },
+        codeblock_highlight = "CodeBlock",
+        dash_highlight = "Dash",
+        fat_headlines = true,
+    },
+    org = {
+        source_pattern_start = "#%+[bB][eE][gG][iI][nN]_[sS][rR][cC]",
+        source_pattern_end = "#%+[eE][nN][dD]_[sS][rR][cC]",
+        dash_pattern = "^-----+$",
+        headline_pattern = "^%*+",
+        headline_highlights = { "Headline1", "Headline2" },
+        codeblock_highlight = "CodeBlock",
+        dash_highlight = "Dash",
+        fat_headlines = true,
+    },
+}
+
+vim.cmd [[highlight Headline1 guibg=#1e2718]]
+vim.cmd [[highlight Headline2 guibg=#21262d]]
+vim.cmd [[highlight CodeBlock guibg=#1c1c1c]]
+vim.cmd [[highlight Dash guibg=#D19A66 gui=bold]]
+vim.cmd("hi MdHeadlineLevel1 guifg=#7388de")
+vim.cmd("hi MdHeadlineLevel2 guifg=#7388de")
+vim.cmd("hi MdHeadlineLevel3 guifg=#7388de")
+vim.cmd("hi MdHeadlineLevel4 guifg=#7388de")
+vim.cmd("hi MdHeadlineLevel5 guifg=#a373FF")
