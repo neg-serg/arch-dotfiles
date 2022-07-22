3465178a74d2bbb548bd7c3461d71cc4e37b5e8d
Neg <serg.zorg@gmail.com>
2022-07-22 22:57:32 +0300

    [nvim] cleanup neoroot

--- dev/.config/nvim/lua/cfg/neoroot.lua
+++ dev/.config/nvim/lua/cfg/neoroot.lua
@@ -1,9 +1,7 @@
 -- ┌───────────────────────────────────────────────────────────────────────────────────┐
 -- │ █▓▒░ nyngwang/NeoRoot.lua                                                         │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
-require('neo-root').setup {
-  CUR_MODE = 1 -- 1 for file/buffer mode, 2 for proj-mode
-}
+require('neo-root').setup { CUR_MODE = 1 } -- 1 for file/buffer mode, 2 for proj-mode
 vim.cmd'au BufWinEnter * NeoRoot'
-map('n', '<leader>r', function() vim.cmd('NeoRootSwitchMode') end, {})
-map('n', '<leader>R', function() vim.cmd('NeoRootChange') end, {})
+vim.keymap.set('n', '<leader>r', function() vim.cmd('NeoRootSwitchMode') end, {})
+vim.keymap.set('n', '<leader>R', function() vim.cmd('NeoRootChange') end, {})
