2839543950e27fa0d19920c2a346156c46d8bc57
Neg <serg.zorg@gmail.com>
2022-07-22 18:11:33 +0300

    [nvim] do no use space here

--- dev/.config/nvim/lua/cfg/neoroot.lua
+++ dev/.config/nvim/lua/cfg/neoroot.lua
@@ -5,5 +5,5 @@ require('neo-root').setup {
   CUR_MODE = 1 -- 1 for file/buffer mode, 2 for proj-mode
 }
 vim.cmd'au BufWinEnter * NeoRoot'
-vim.keymap.set('n', '<Space>r', function() vim.cmd('NeoRootSwitchMode') end, NOREF_NOERR_TRUNC)
-vim.keymap.set('n', '<Space>R', function() vim.cmd('NeoRootChange') end, NOREF_NOERR_TRUNC)
+map('n', '<leader>r', function() vim.cmd('NeoRootSwitchMode') end, {})
+map('n', '<leader>R', function() vim.cmd('NeoRootChange') end, {})
