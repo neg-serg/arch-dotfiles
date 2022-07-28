3d2945e32810e7d380c213eea37bc25b28606e4a
Neg <serg.zorg@gmail.com>
2022-07-28 18:30:41 +0300

    [nvim] remove neoroot

--- dev/.config/nvim/lua/cfg/neoroot.lua
+++
@@ -1,7 +0,0 @@
--- ┌───────────────────────────────────────────────────────────────────────────────────┐
--- │ █▓▒░ nyngwang/NeoRoot.lua                                                         │
--- └───────────────────────────────────────────────────────────────────────────────────┘
-require('neo-root').setup { CUR_MODE = 1 } -- 1 for file/buffer mode, 2 for proj-mode
-vim.cmd'au BufWinEnter * NeoRoot'
-vim.keymap.set('n', '<leader>r', function() vim.cmd('NeoRootSwitchMode') end, {})
-vim.keymap.set('n', '<leader>R', function() vim.cmd('NeoRootChange') end, {})
