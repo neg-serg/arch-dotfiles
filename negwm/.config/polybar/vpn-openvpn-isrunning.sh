02d457d9eca167c0f7934a8cdb05c218e82df89d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-08-17 00:18:53 +0300

    [polybar] move polybar stuff to polybar

--- negwm/.config/polybar/vpn-openvpn-isrunning.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-connection=$(\pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' |xargs basename| cut -f 1 -d '.')
-if [ -n "$connection" ]; then
-    echo $connection
-else
-    echo ""
-fi
