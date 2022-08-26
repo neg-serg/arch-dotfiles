6431d3298788a2c0f7646a40ae1e485956482727
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-08-26 23:01:51 +0300

    [nvim] telekasten cleanup / update config

--- dev/.config/nvim/lua/cfg/telekasten.lua
+++ dev/.config/nvim/lua/cfg/telekasten.lua
@@ -1,83 +1,74 @@
 local home=vim.fn.expand("~/1st_level/")
 require('telekasten').setup({
     home=home,
-    -- if true, telekasten will be enabled when opening a note within the configured home
-    take_over_my_home=true,
-    -- auto-set telekasten filetype: if false, the telekasten filetype will not be used
-    --                               and thus the telekasten syntax will not be loaded either
+    take_over_my_home=true, -- if true, telekasten will be enabled when opening a note within the configured home
     auto_set_filetype=true,
     -- dir names for special notes (absolute path or subdir name)
     dailies=home .. '/' .. 'daily',
     weeklies=home .. '/' .. 'weekly',
     templates=home .. '/' .. 'templates',
-    -- image (sub)dir for pasting
-    -- dir name (absolute path or subdir name)
-    -- or nil if pasted images shouldn't go into a special subdir
     image_subdir="img",
-    -- markdown file extension
     extension   =".md",
-    -- Generate note filenames. One of:
-    -- "title" (default) - Use title if supplied, uuid otherwise
-    -- "uuid" - Use uuid
-    -- "uuid-title" - Prefix title by uuid
-    -- "title-uuid" - Suffix title with uuid
     new_note_filename="title",
-    -- file uuid type ("rand" or input for os.date()")
     uuid_type="%Y%m%d%H%M",
-    -- UUID separator
     uuid_sep="-",
-    -- following a link to a non-existing note will create it
     follow_creates_nonexisting=true,
     dailies_create_nonexisting=true,
     weeklies_create_nonexisting=true,
-    -- skip telescope prompt for goto_today and goto_thisweek
     journal_auto_open=false,
-    -- template for new notes (new_note, follow_link)
-    -- set to `nil` or do not specify if you do not want a template
     template_new_note=home .. '/' .. 'templates/new_note.md',
-    -- template for newly created daily notes (goto_today)
-    -- set to `nil` or do not specify if you do not want a template
     template_new_daily=home .. '/' .. 'templates/daily.md',
-    -- template for newly created weekly notes (goto_thisweek)
-    -- set to `nil` or do not specify if you do not want a template
     template_new_weekly= home .. '/' .. 'templates/weekly.md',
-    -- image link style
-    -- wiki:     ![[image name]]
-    -- markdown: ![](image_subdir/xxxxx.png)
     image_link_style="markdown",
-    -- default sort option: 'filename', 'modified'
     sort="filename",
-    -- integrate with calendar-vim
     plug_into_calendar=true,
     calendar_opts={
-        -- calendar week display mode: 1 .. 'WK01', 2 .. 'WK 1', 3 .. 'KW01', 4 .. 'KW 1', 5 .. '1'
         weeknm=4,
-        -- use monday as first day of week: 1 .. true, 0 .. false
         calendar_monday=1,
-        -- calendar mark: where to put mark for marked days: 'left', 'right', 'left-fit'
         calendar_mark='left-fit',
     },
-    -- telescope actions behavior
     close_after_yanking=false,
     insert_after_inserting=true,
-    -- tag notation: '#tag', ':tag:', 'yaml-bare'
     tag_notation="#tag",
     -- command palette theme: dropdown (window) or ivy (bottom panel)
     command_palette_theme="ivy",
-    -- tag list theme:
-    -- get_cursor: small tag list at cursor; ivy and dropdown like above
     show_tags_theme="ivy",
-    -- when linking to a note in subdir/, create a [[subdir/title]] link
-    -- instead of a [[title only]] link
-    subdirs_in_links=true,
-    -- template_handling
-    -- What to do when creating a new note via `new_note()` or `follow_link()`
-    -- to a non-existing note
-    -- - prefer_new_note: use `new_note` template
-    -- - smart: if day or week is detected in title, use daily / weekly templates (default)
-    -- - always_ask: always ask before creating a note
+    subdirs_in_links=true, -- when linking to a note in subdir/, create a [[subdir/title]] link instead of a [[title only]] link
     template_handling="smart",
     new_note_location="smart",
-    -- should all links be updated when a file is renamed
     rename_update_links=true,
+    media_previewer = "telescope-media-files",
 })
+
+local opts={silent=true, noremap=true}
+map('n', '<M-b>', '<Cmd>lua require"telescope.builtin".buffers()<CR>', opts)
+map('n', '<leader>zf', '<Cmd>lua require("telekasten").find_notes()<CR>'
+map('n', '<leader>zd', '<Cmd>lua require("telekasten").find_daily_notes()<CR>'
+map('n', '<leader>zg', '<Cmd>lua require("telekasten").search_notes()<CR>', opts)
+map('n', '<leader>zz', '<Cmd>lua require("telekasten").follow_link()<CR>', opts)
+map('n', '<leader>zT', '<Cmd>lua require("telekasten").goto_today()<CR>', opts)
+map('n', '<leader>zW', '<Cmd>lua require("telekasten").goto_thisweek()<CR>', opts)
+map('n', '<leader>zw', '<Cmd>lua require("telekasten").find_weekly_notes()<CR>', opts)
+map('n', '<leader>zn', '<Cmd>lua require("telekasten").new_note()<CR>', opts)
+map('n', '<leader>zN', '<Cmd>lua require("telekasten").new_templated_note()<CR>', opts)
+map('n', '<leader>zy', '<Cmd>lua require("telekasten").yank_notelink()<CR>', opts)
+map('n', '<leader>zc', '<Cmd>lua require("telekasten").show_calendar()<CR>', opts)
+map('n', '<leader>zC', '<Cmd>CalendarT<CR>', opts)
+map('n', '<leader>zi', '<Cmd>lua require("telekasten").paste_img_and_link()<CR>', opts)
+map('n', '<leader>zt', '<Cmd>lua require("telekasten").toggle_todo()<CR>', opts)
+map('n', '<leader>zb', '<Cmd>lua require("telekasten").show_backlinks()<CR>', opts)
+map('n', '<leader>zF', '<Cmd>lua require("telekasten").find_friends()<CR>', opts)
+map('n', '<leader>zI', '<Cmd>lua require("telekasten").insert_img_link({ i=true })<CR>', opts)
+map('n', '<leader>zp', '<Cmd>lua require("telekasten").preview_img()<CR>', opts)
+map('n', '<leader>zm', '<Cmd>lua require("telekasten").browse_media()<CR>', opts)
+map('n', '<leader>za', '<Cmd>lua require("telekasten").show_tags()<CR>', opts)
+map('n', '<leader>#',  '<Cmd>lua require("telekasten").show_tags()<CR>', opts)
+map('n', '<leader>zr', '<Cmd>lua require("telekasten").rename_note()<CR>', opts)
+-- on hesitation, bring up the panel
+map('n', '<leader>z', '<Cmd>lua require("telekasten").panel()<CR>', opts)
+-- we could define [[ in **insert mode** to call insert link
+-- inoremap [[ <cmd>:lua require('telekasten').insert_link()<CR>
+-- alternatively: leader [
+map('i', '<leader>[',  '<Cmd>lua require("telekasten").insert_link({i=true})<CR>', opts)
+map('i', '<leader>zt', '<Cmd>lua require("telekasten").toggle_todo({i=true})<CR>', opts)
+map('i', '<leader>#',  '<Cmd>lua require("telekasten").show_tags({i=true})<CR>', opts)
