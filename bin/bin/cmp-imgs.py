b0e0bd84e929428d1fca39b4e8ef127d2ac34c8b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-09-07 18:24:50 +0300

    [bin] cmp-imgs refactoring

--- bin/bin/cmp-imgs.py
+++ bin/bin/cmp-imgs.py
@@ -4,7 +4,5 @@ import sys
 from PIL import Image
 from imagehash import dhash
 
-if dhash(Image.open(sys.argv[1])) == dhash(Image.open(sys.argv[2])):
-    sys.exit(0)
-else:
-    sys.exit(2)
+if len(sys.argv) == 3:
+    sys.exit((dhash(Image.open(sys.argv[1])) == dhash(Image.open(sys.argv[2])))*2)
