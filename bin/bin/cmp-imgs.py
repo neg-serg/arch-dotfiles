8dc27ed3f0b51f2f807003673e9ab820ed634672
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-10-24 19:30:54 +0300

    [bin] cmp-imgs fix

--- bin/bin/cmp-imgs.py
+++ bin/bin/cmp-imgs.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
-
+import os
 import sys
 from PIL import Image
 from imagehash import dhash
 
+for t in sys.argv[1], sys.argv[2]:
+    if not os.path.exists(t):
+        print('')
+        sys.exit(0)
+
 if len(sys.argv) == 3:
-    sys.exit((dhash(Image.open(sys.argv[1])) == dhash(Image.open(sys.argv[2])))*2)
+    img1=Image.open(sys.argv[1])
+    img2=Image.open(sys.argv[2])
+    ret=(dhash(img1) == dhash(img2))
+    if ret:
+        print('')
+    else:
+        print('ok')
