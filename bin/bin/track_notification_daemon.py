447f221f4fd6ebc7b1fc1f11be48bcbc85a70c46
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-10-29 00:37:48 +0300

    [bin] better track-notify

--- bin/bin/track_notification_daemon.py
+++ bin/bin/track_notification_daemon.py
@@ -1,32 +1,35 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
+# noqa: E402
 import gi
 gi.require_version('Playerctl', '2.0')
 from gi.repository import Playerctl, GLib
 import subprocess
 
-player = Playerctl.Player()
-md5sums = []
+player=Playerctl.Player()
+md5sums=[]
 
 def remove_prefix(text, prefix):
     return text[text.startswith(prefix) and len(prefix):]
 
-def on_track_change(t, data):
+def on_track_change(player, data):
+    if data is None or not data:
+        return 0
     cover=data['mpris:artUrl']
     if cover:
         try:
+            cover=remove_prefix(cover, 'file://')
             cover_sum=subprocess.check_output(
-                ["md5sum", remove_prefix(cover, 'file://')]
+                ["md5sum", cover]
             ).decode("utf-8").strip()
             md5sums.append(cover_sum.split()[0])
         except subprocess.CalledProcessError:
             pass
-        if t:
-            print(md5sums)
+        if player:
             if len(md5sums) == 1:
-                subprocess.Popen(['track-notify'])
+                subprocess.Popen(['track-notify', cover])
             elif md5sums[-1] != md5sums[-2]:
-                subprocess.Popen(['track-notify'])
+                subprocess.Popen(['track-notify', cover])
             del md5sums[:-2]
 
 player.connect('metadata', on_track_change)
@@ -40,16 +43,8 @@ GLib.MainLoop().run()
 # from PIL import Image
 # from imagehash import dhash
 #
-# for t in sys.argv[1], sys.argv[2]:
-#     if not os.path.exists(t):
-#         print('')
-#         sys.exit(0)
-#
-# if len(sys.argv) == 3:
-#     img1=Image.open(sys.argv[1])
-#     img2=Image.open(sys.argv[2])
-#     ret=(dhash(img1) == dhash(img2))
-#     if ret:
-#         print('fail')
-#     else:
-#         print('ok')
+# img1,img2=Image.open(sys.argv[1]),Image.open(sys.argv[2])
+# if dhash(img1) == dhash(img2):
+#     print('fail')
+# else:
+#     print('ok')
