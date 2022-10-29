e024dcb65b8df18096754b46056b27adff8f8fd0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-10-29 03:19:03 +0300

    [nvim] better noice config

--- dev/.config/nvim/lua/cfg/noice.lua
+++ dev/.config/nvim/lua/cfg/noice.lua
@@ -11,4 +11,61 @@ require("noice").setup({
         {filter={event="msg_show", kind="", find="more line",}, opts={skip=true},},
         {filter={event="msg_show", kind="", find="line yanked",}, opts={skip=true},},
     },
+    lsp = {
+        progress = {
+            enabled = true,
+            -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+            -- See the section on formatting for more details on how to customize.
+            --- @type NoiceFormat|string
+            format = "lsp_progress",
+            --- @type NoiceFormat|string
+            format_done = "lsp_progress_done",
+            throttle = 1000 / 30, -- frequency to update lsp progress message
+            view = "mini",
+        },
+        override = {
+            -- override the default lsp markdown formatter with Noice
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+            -- override the lsp markdown formatter with Noice
+            ["vim.lsp.util.stylize_markdown"] = false,
+            -- override cmp documentation with Noice (needs the other options to work)
+            ["cmp.entry.get_documentation"] = false,
+        },
+        hover = {
+            enabled = true,
+            view = nil, -- when nil, use defaults from documentation
+            ---@type NoiceViewOptions
+            opts = {}, -- merged with defaults from documentation
+        },
+        signature = {
+            enabled = true,
+            auto_open = {
+                enabled = true,
+                trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+                luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+                throttle = 50, -- Debounce lsp signature help request by 50ms
+            },
+            view = nil, -- when nil, use defaults from documentation
+            ---@type NoiceViewOptions
+            opts = {}, -- merged with defaults from documentation
+        },
+        message = {
+            -- Messages shown by lsp servers
+            enabled = true,
+            view = "notify",
+            opts = {},
+        },
+        -- defaults for hover and signature help
+        documentation = {
+            view = "hover",
+            ---@type NoiceViewOptions
+            opts = {
+                lang = "markdown",
+                replace = true,
+                render = "plain",
+                format = { "{message}" },
+                win_options = { concealcursor = "n", conceallevel = 3 },
+            },
+        },
+    },
 })
