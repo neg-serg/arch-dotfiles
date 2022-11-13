2ece95d9c9f42231b7574e66ad83a9bc04182c06
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-11-13 15:07:14 +0300

    [nvim] noice define popup menu

--- dev/.config/nvim/lua/cfg/noice.lua
+++ dev/.config/nvim/lua/cfg/noice.lua
@@ -55,6 +55,14 @@ require("noice").setup({
             view = "notify",
             opts = {},
         },
+        popupmenu = {
+            enabled = true, -- enables the Noice popupmenu UI
+            ---@type 'nui'|'cmp'
+            backend = "nui", -- backend to use to show regular cmdline completions
+            ---@type NoicePopupmenuItemKind|false
+            -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
+            kind_icons = {}, -- set to `false` to disable icons
+        },
         -- defaults for hover and signature help
         documentation = {
             view = "hover",
