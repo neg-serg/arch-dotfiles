fdc1bf2d18de3dd46f35217e04fa3243e7a66b47
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-11-16 23:16:52 +0300

    [nvim] noice: fix

--- dev/.config/nvim/lua/cfg/noice.lua
+++ dev/.config/nvim/lua/cfg/noice.lua
@@ -24,8 +24,6 @@ require("noice").setup({
             ["vim.lsp.util.convert_input_to_markdown_lines"]=true,
             -- override the lsp markdown formatter with Noice
             ["vim.lsp.util.stylize_markdown"]=true,
-            -- override cmp documentation with Noice (needs the other options to work)
-            ["cmp.entry.get_documentation"]=true,
         },
         hover={
             enabled=true,
