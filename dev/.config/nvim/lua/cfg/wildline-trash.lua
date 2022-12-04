9b42a5b2a8fa98826b81c4516d514430e76c6ead
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-12-04 17:30:04 +0300

    [nvim] cfg: cleanup

--- dev/.config/nvim/lua/cfg/wildline-trash.lua
+++
@@ -1,9 +0,0 @@
-basic.square_mode_left={
-    hl_colors=colors_mode,
-    text=function() return {{'', 'Normal'}} end,
-}
-
-basic.square_mode_right={
-    hl_colors=colors_mode,
-    text=function() return {{'', 'Normal'}} end,
-}
