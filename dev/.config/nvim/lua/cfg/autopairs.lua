e205a882f2defc896c4fe33072f8c9f83ad1f0b9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-12-04 17:29:58 +0300

    [nvim] autopairs: CS

--- dev/.config/nvim/lua/cfg/autopairs.lua
+++ dev/.config/nvim/lua/cfg/autopairs.lua
@@ -2,30 +2,30 @@
 -- │ █▓▒░ windwp/nvim-autopairs                                                        │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 require('nvim-autopairs').setup({
-    disable_filetype = {"TelescopePrompt"},
-    disable_in_macro = false,  -- disable when recording or executing a macro
-    disable_in_visualblock = false, -- disable when insert after visual block mode
-    ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]],"%s+", ""),
-    enable_moveright = true,
-    enable_afterquote = true,  -- add bracket pairs after quote
-    enable_check_bracket_line = true,  -- check bracket in same line
-    check_ts = true,
-    map_cr = true,
-    map_bs = true,  -- map the <BS> key
-    map_c_w = true, -- map <C-w> to delete an pair if possible
+    disable_filetype={"TelescopePrompt"},
+    disable_in_macro=false,  -- disable when recording or executing a macro
+    disable_in_visualblock=false, -- disable when insert after visual block mode
+    ignored_next_char=string.gsub([[ [%w%%%'%[%"%.] ]],"%s+", ""),
+    enable_moveright=true,
+    enable_afterquote=true,  -- add bracket pairs after quote
+    enable_check_bracket_line=true,  -- check bracket in same line
+    check_ts=true,
+    map_cr=true,
+    map_bs=true,  -- map the <BS> key
+    map_c_w=true, -- map <C-w> to delete an pair if possible
 })
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
+local cmp_autopairs=require('nvim-autopairs.completion.cmp')
+local cmp=require('cmp')
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char={ tex=''}}))
 
-local Rule = require('nvim-autopairs.rule')
-local npairs = require('nvim-autopairs')
+local Rule=require('nvim-autopairs.rule')
+local npairs=require('nvim-autopairs')
 npairs.add_rules {
     -- before   insert  after
     --  (|)     ( |)	( | )
     Rule(' ', ' ')
         :with_pair(function (opts)
-            local pair = opts.line:sub(opts.col - 1, opts.col)
+            local pair=opts.line:sub(opts.col - 1, opts.col)
             return vim.tbl_contains({ '()', '[]', '{}' }, pair)
         end),
     Rule('( ', ' )')
