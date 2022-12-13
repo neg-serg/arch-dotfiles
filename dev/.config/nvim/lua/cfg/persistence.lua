56745f0f39793b3b21314456b5cc0aae5bcedb93
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-12-13 20:53:41 +0300

    [nvim] persistence config

---
+++ dev/.config/nvim/lua/cfg/persistence.lua
@@ -0,0 +1,10 @@
+require("persistence").setup {
+    dir=vim.fn.expand(vim.fn.stdpath('config') .. '/sessions/'), -- directory where session files are saved
+    options={'buffers','curdir','tabpages','winsize'}, -- sessionoptions used for saving
+}
+-- restore the session for the current directory
+map('n', '<leader>qs', [[<cmd>lua require('persistence').load()<cr>]], {})
+-- restore the last session
+map('n', '<leader>ql', [[<cmd>lua require('persistence').load({ last = true })<cr>]], {})
+-- stop Persistence => session won't be saved on exit
+map('n', '<leader>qd', [[<cmd>lua require('persistence').stop()<cr>]], {})
