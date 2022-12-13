946144e4ea8ea4b6648cadb5e03a795499844981
Sergey Miroshnichenko <serg.zorg@gmail.com>
2022-12-13 21:13:42 +0300

    [nvim] toggleterm bindings

--- dev/.config/nvim/lua/cfg/toggleterm.lua
+++ dev/.config/nvim/lua/cfg/toggleterm.lua
@@ -1,4 +1,25 @@
-require("toggleterm").setup()
+require('toggleterm').setup({
+    size=10,
+    open_mapping='<F4>',
+    hide_numbers=true,
+    shade_filetypes={},
+    shade_terminals=true,
+    start_in_insert=true,
+    persist_size=true,
+    direction='horizontal',
+    close_on_exit=true,
+    float_opts={
+        border='curved',
+        width=70,
+        height=20,
+        winblend=0,
+        highlights={border='Special', background='Normal'},
+    },
+})
+
 local Terminal=require('toggleterm.terminal').Terminal
-navigator=Terminal:new({cmd="zsh", env={NEOVIM_TERMINAL=1}})
-vim.api.nvim_set_keymap("n", "<leader>l", "<Cmd>lua navigator:toggle()<CR>", {noremap=true, silent=true})
+navigator=Terminal:new({
+    cmd='zsh',
+    env={NEOVIM_TERMINAL=1}
+})
+map('n', '<leader>l', '<Cmd>lua navigator:toggle()<CR>', {noremap=true, silent=true})
