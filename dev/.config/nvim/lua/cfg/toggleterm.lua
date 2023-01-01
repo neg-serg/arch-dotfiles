6b64c0131691c57fa472cb87fa67943ccfce29a6
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-01-01 16:33:28 +0300

    [nvim] cleanup toggleterm

--- dev/.config/nvim/lua/cfg/toggleterm.lua
+++ dev/.config/nvim/lua/cfg/toggleterm.lua
@@ -2,7 +2,6 @@ local status, toggleterm = pcall(require, 'toggleterm')
 if (not status) then return end
 toggleterm.setup({
     size=10,
-    open_mapping='<F4>',
     hide_numbers=true,
     shade_filetypes={},
     shade_terminals=true,
@@ -10,13 +9,6 @@ toggleterm.setup({
     persist_size=true,
     direction='horizontal',
     close_on_exit=true,
-    float_opts={
-        border='none',
-        width=140,
-        height=8,
-        winblend=0,
-        highlights={border='Special', background='Normal'},
-    },
 })
 
 local Terminal=require('toggleterm.terminal').Terminal
