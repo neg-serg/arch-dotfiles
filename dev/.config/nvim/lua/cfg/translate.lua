2331afe9078c6983341c11196765bdda355c495a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-03-20 18:04:54 +0300

    [nvim] better pantran

--- dev/.config/nvim/lua/cfg/translate.lua
+++ dev/.config/nvim/lua/cfg/translate.lua
@@ -2,6 +2,11 @@ local status, pantran = pcall(require, 'pantran')
 if (not status) then return end
 pantran.setup{
     default_engine="yandex",
-    engines={yandex={default_source="auto", default_target="ru"}},
-    controls={mappings={edit={n={["j"]="gj", ["k"]="gk"}}}}
+    engines={
+        yandex={default_target="ru"}
+    },
+    controls={mappings={
+        edit={n={["j"]="gj", ["k"]="gk"}}
+    }},
+    ui={width_percentage=0.8, height_percentage=0.8},
 }
