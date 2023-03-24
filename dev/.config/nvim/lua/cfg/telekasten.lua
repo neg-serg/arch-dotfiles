418205c20879ec902c0a87c1d90207cf345a12f1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-03-24 21:04:20 +0300

    [nvim] telekasten new keymaps

--- dev/.config/nvim/lua/cfg/telekasten.lua
+++ dev/.config/nvim/lua/cfg/telekasten.lua
@@ -7,11 +7,11 @@ telekasten.setup({
     take_over_my_home=true, -- if true, telekasten will be enabled when opening a note within the configured home
     auto_set_filetype=true,
     -- dir names for special notes (absolute path or subdir name)
-    dailies=home .. '/' .. 'daily',
-    weeklies=home .. '/' .. 'weekly',
-    templates=home .. '/' .. 'templates',
+    dailies=home..'/'..'daily',
+    weeklies=home..'/'..'weekly',
+    templates=home..'/'..'templates',
     image_subdir="img",
-    extension   =".md",
+    extension=".md",
     new_note_filename="title",
     uuid_type="%Y%m%d%H%M",
     uuid_sep="-",
@@ -19,9 +19,9 @@ telekasten.setup({
     dailies_create_nonexisting=true,
     weeklies_create_nonexisting=true,
     journal_auto_open=false,
-    template_new_note=home .. '/' .. 'templates/new_note.md',
-    template_new_daily=home .. '/' .. 'templates/daily.md',
-    template_new_weekly= home .. '/' .. 'templates/weekly.md',
+    template_new_note=home..'/'..'templates/new_note.md',
+    template_new_daily=home..'/'..'templates/daily.md',
+    template_new_weekly=home..'/'..'templates/weekly.md',
     image_link_style="markdown",
     sort="filename",
     close_after_yanking=false,
@@ -42,23 +42,17 @@ vim.api.nvim_create_autocmd({"BufNewFile","BufRead"}, {
 	pattern="*.md",
 	group=vim.api.nvim_create_augroup('telekasten_only_keymap', {clear=true}),
 	callback=function()
-	    vim.keymap.set('n', '<leader>g', '<Cmd>lua require"telekasten".search_notes()<CR>', opts)
-        vim.keymap.set('n', '<leader>T', '<Cmd>lua require"telekasten".goto_today()<CR>', opts)
-        vim.keymap.set('n', '<leader>n', '<Cmd>lua require"telekasten".new_note()<CR>', opts)
-        vim.keymap.set('n', '<leader>N', '<Cmd>lua require"telekasten".new_templated_note()<CR>', opts)
-        vim.keymap.set('n', '<leader>y', '<Cmd>lua require"telekasten".yank_notelink()<CR>', opts)
-        vim.keymap.set('n', '<leader>i', '<Cmd>lua require"telekasten".paste_img_and_link()<CR>', opts)
-        vim.keymap.set('n', '<leader>t', '<Cmd>lua require"telekasten".toggle_todo()<CR>', opts)
-        vim.keymap.set('n', '<leader>b', '<Cmd>lua require"telekasten".show_backlinks()<CR>', opts)
+        vim.keymap.set('i', '<leader>[', '<Cmd>lua require"telekasten".insert_link({i=true})<CR>', opts)
+        vim.keymap.set('n', '<C-S-i>', '<Cmd>lua require"telekasten".insert_img_link({ i=true })<CR>', opts)
+        vim.keymap.set('n', '<C-a>', '<Cmd>lua require"telekasten".show_tags()<CR>', opts)
+        vim.keymap.set('n', '<C-i>', '<Cmd>lua require"telekasten".paste_img_and_link()<CR>', opts)
+        vim.keymap.set('n', '<C-m>', '<Cmd>lua require"telekasten".browse_media()<CR>', opts)
+        vim.keymap.set('n', '<C-n>', '<Cmd>lua require"telekasten".new_note()<CR>', opts)
+        vim.keymap.set('n', '<C-p>', '<Cmd>lua require"telekasten".preview_img()<CR>', opts)
+        vim.keymap.set('n', '<C-t>', '<Cmd>lua require"telekasten".toggle_todo()<CR>', opts)
+        vim.keymap.set('n', '<C-y>', '<Cmd>lua require"telekasten".yank_notelink()<CR>', opts)
         vim.keymap.set('n', '<leader>F', '<Cmd>lua require"telekasten".find_friends()<CR>', opts)
-        vim.keymap.set('n', '<leader>I', '<Cmd>lua require"telekasten".insert_img_link({ i=true })<CR>', opts)
-        vim.keymap.set('n', '<leader>p', '<Cmd>lua require"telekasten".preview_img()<CR>', opts)
-        vim.keymap.set('n', '<leader>m', '<Cmd>lua require"telekasten".browse_media()<CR>', opts)
-        vim.keymap.set('n', '<leader>a', '<Cmd>lua require"telekasten".show_tags()<CR>', opts)
-        vim.keymap.set('n', '<leader>#',  '<Cmd>lua require"telekasten".show_tags()<CR>', opts)
+        vim.keymap.set('n', '<leader>b', '<Cmd>lua require"telekasten".show_backlinks()<CR>', opts)
         vim.keymap.set('n', '<leader>r', '<Cmd>lua require"telekasten".rename_note()<CR>', opts)
-        vim.keymap.set('i', '<leader>[',  '<Cmd>lua require"telekasten".insert_link({i=true})<CR>', opts)
-        vim.keymap.set('i', '<leader>t', '<Cmd>lua require"telekasten".toggle_todo({i=true})<CR>', opts)
-        vim.keymap.set('i', '<leader>#',  '<Cmd>lua require"telekasten".show_tags({i=true})<CR>', opts)
 	end,
 })
