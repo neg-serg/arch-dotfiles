265019bbc51fd0414eceb2e4c8fca6b6e648cabe
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-03-25 13:24:15 +0300

    [nvim] cleanup telekasten

--- dev/.config/nvim/lua/cfg/telekasten.lua
+++ dev/.config/nvim/lua/cfg/telekasten.lua
@@ -1,4 +1,4 @@
-local home=vim.fn.expand("~/1st_level/")
+local home=vim.fn.expand("~/1st_level")
 local status, telekasten = pcall(require, 'telekasten')
 if (not status) then return end
 
@@ -7,9 +7,9 @@ telekasten.setup({
     take_over_my_home=true, -- if true, telekasten will be enabled when opening a note within the configured home
     auto_set_filetype=true,
     -- dir names for special notes (absolute path or subdir name)
-    dailies=home..'/'..'daily',
-    weeklies=home..'/'..'weekly',
-    templates=home..'/'..'templates',
+    dailies='daily',
+    weeklies='weekly',
+    templates='templates',
     image_subdir="img",
     extension=".md",
     new_note_filename="title",
@@ -19,21 +19,21 @@ telekasten.setup({
     dailies_create_nonexisting=true,
     weeklies_create_nonexisting=true,
     journal_auto_open=false,
-    template_new_note=home..'/'..'templates/new_note.md',
-    template_new_daily=home..'/'..'templates/daily.md',
-    template_new_weekly=home..'/'..'templates/weekly.md',
+    template_new_note='templates/new_note.md',
+    template_new_daily='templates/daily.md',
+    template_new_weekly='templates/weekly.md',
     image_link_style="markdown",
     sort="filename",
     close_after_yanking=false,
     insert_after_inserting=true,
-    tag_notation="#tag",
+    tag_notation='#tag',
     -- command palette theme: dropdown (window) or ivy (bottom panel)
     command_palette_theme="ivy",
     show_tags_theme="ivy",
     subdirs_in_links=true, -- when linking to a note in subdir/, create a [[subdir/title]] link instead of a [[title only]] link
-    template_handling="smart",
+    template_handling="always_ask",
     new_note_location="smart",
-    rename_update_links=true,
+    rename_update_links=true, -- should all links be updated when a file is renamed
     media_previewer="telescope-media-files",
 })
 
@@ -46,7 +46,8 @@ vim.api.nvim_create_autocmd({"BufNewFile","BufRead"}, {
         vim.keymap.set('n', '<C-S-i>', '<Cmd>lua require"telekasten".insert_img_link({ i=true })<CR>', opts)
         vim.keymap.set('n', '<C-a>', '<Cmd>lua require"telekasten".show_tags()<CR>', opts)
         vim.keymap.set('n', '<C-i>', '<Cmd>lua require"telekasten".paste_img_and_link()<CR>', opts)
-        vim.keymap.set('n', '<C-m>', '<Cmd>lua require"telekasten".browse_media()<CR>', opts)
+        vim.keymap.set('n', '<C-m>', '<Cmd>lua require"telekasten".follow_link()<CR>', opts)
+        vim.keymap.set('n', '<S-m>', '<Cmd>lua require"telekasten".browse_media()<CR>', opts)
         vim.keymap.set('n', '<C-n>', '<Cmd>lua require"telekasten".new_note()<CR>', opts)
         vim.keymap.set('n', '<C-p>', '<Cmd>lua require"telekasten".preview_img()<CR>', opts)
         vim.keymap.set('n', '<C-t>', '<Cmd>lua require"telekasten".toggle_todo()<CR>', opts)
