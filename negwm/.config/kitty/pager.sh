393cf2aca0aa183b8b5bda11ecda6d396bd94d89
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-06-26 17:49:21 +0300

    [kitty] cleanup settings

--- negwm/.config/kitty/pager.sh
+++ negwm/.config/kitty/pager.sh
@@ -16,7 +16,7 @@ exec nvim 63<&0 0</dev/null \
     -c "map <silent> <C-c> <Cmd>qa!<CR>" \
     -c "nnoremap <silent> <CR> \"+y<Cmd>qa!<CR>" \
     -c "vnoremap <silent> <CR> \"+y<Cmd>qa!<CR>" \
-    -c "set shell=bash scrollback=100000 termguicolors laststatus=0 clipboard+=unnamedplus" \
+    -c "set shell=bash termguicolors laststatus=0 clipboard+=unnamedplus" \
     -c "autocmd TermEnter * stopinsert" \
     -c "autocmd TermClose * ${AUTOCMD_TERMCLOSE_CMD}" \
     -c 'terminal sed </dev/fd/63 -e "s/'$'\x1b'']8;;file:[^\]*[\]//g" && sleep 0.01 && printf "'$'\x1b'']2;"'
