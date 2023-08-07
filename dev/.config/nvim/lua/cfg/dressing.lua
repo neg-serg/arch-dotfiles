a080b41bf2308d5513bcd76b807481b1f8bbe58c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-08-07 12:02:35 +0300

    [nvim] dressing make config more compact

--- dev/.config/nvim/lua/cfg/dressing.lua
+++ dev/.config/nvim/lua/cfg/dressing.lua
@@ -2,18 +2,12 @@ local status, dressing = pcall(require, 'dressing')
 if (not status) then return end
 dressing.setup({
     input = {
-        -- Set to false to disable the vim.ui.input implementation
-        enabled = true,
-        -- Default prompt string
-        default_prompt = 'Input:',
-        -- Can be 'left', 'right', or 'center'
-        prompt_align = 'center',
-        -- When true, <Esc> will close the modal
-        insert_only = true,
-        -- 'editor' and 'win' will default to being centered
-        relative = 'cursor',
-        -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-        prefer_width = 40,
+        enabled = true, -- Set to false to disable the vim.ui.input implementation
+        default_prompt = 'Input:', -- Default prompt string
+        prompt_align = 'center', -- Can be 'left', 'right', or 'center'
+        insert_only = true, -- When true, <Esc> will close the modal
+        relative = 'cursor', -- 'editor' and 'win' will default to being centered
+        prefer_width = 40, -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
         width = nil,
         -- min_width and max_width can be a list of mixed types.
         -- min_width = {20, 0.2} means 'the greater of 20 columns or 20% of total'
@@ -27,27 +21,20 @@ dressing.setup({
         -- see :help dressing_get_config
         get_config = nil,
         win_options = {
-            -- Window transparency (0-100)
-            winblend = 0,
-            -- Change default highlight groups (see :help winhl)
-            winhighlight = '',
+            winblend = 0, -- Window transparency (0-100)
+            winhighlight = '', -- Change default highlight groups (see :help winhl)
         },
     },
     select = {
-        -- Set to false to disable the vim.ui.select implementation
-        enabled = false,
-        -- Priority list of preferred vim.select implementations
-        backend = { 'telescope', 'builtin' },
-        -- Trim trailing `:` from prompt
-        trim_prompt = true,
+        enabled = false, -- Set to false to disable the vim.ui.select implementation
+        backend = { 'telescope', 'builtin' }, -- Priority list of preferred vim.select implementations
+        trim_prompt = true, -- Trim trailing `:` from prompt
         -- Options for telescope selector
         -- These are passed into the telescope picker directly. Can be used like:
         -- telescope = require('telescope.themes').get_ivy({...})
         telescope = nil,
-        -- Options for built-in selector
         builtin = {
-            -- 'editor' and 'win' will default to being centered
-            relative = 'editor',
+            relative = 'editor', -- 'editor' and 'win' will default to being centered
             -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
             -- the min_ and max_ options can be a list of mixed types.
             -- max_width = {140, 0.8} means 'the lesser of 140 columns or 80% of total'
@@ -58,10 +45,8 @@ dressing.setup({
             max_height = 0.9,
             min_height = { 10, 0.2 },
             win_options = {
-                -- Window transparency (0-100)
-                winblend = 10,
-                -- Change default highlight groups (see :help winhl)
-                winhighlight = '',
+                winblend = 10, -- Window transparency (0-100)
+                winhighlight = '', -- Change default highlight groups (see :help winhl)
             },
             override = function(conf)
                 -- This is the config that will be passed to nvim_open_win.
@@ -69,9 +54,7 @@ dressing.setup({
                 return conf
             end,
         },
-        -- Used to override format_item. See :help dressing-format
-        format_item_override = {},
-        -- see :help dressing_get_config
-        get_config = nil,
+        format_item_override = {}, -- Used to override format_item. See :help dressing-format
+        get_config = nil, -- see :help dressing_get_config
     },
 })
