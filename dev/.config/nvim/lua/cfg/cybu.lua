1271b242eb3c664218f073721381d317396d1003
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-08-09 02:44:46 +0300

    [nvim] cybu better settings

--- dev/.config/nvim/lua/cfg/cybu.lua
+++ dev/.config/nvim/lua/cfg/cybu.lua
@@ -3,12 +3,17 @@
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 local ok, cybu=pcall(require, 'cybu')
 if not ok then return end
-cybu.setup({style={
-    highlights={                          -- see highlights via :highlight
-        current_buffer='CursorLineNr',    -- for current / selected buffer
-        adjacent_buffers='CybuAdjacent',  -- for buffers not in focus
-        background='CybuBackground',      -- for the window background
+cybu.setup({
+    position={
+        anchor="bottomleft",      -- topleft, topcenter, topright,
+    },
+    style={
+        highlights={                          -- see highlights via :highlight
+            current_buffer='CursorLineNr',    -- for current / selected buffer
+            adjacent_buffers='CybuAdjacent',  -- for buffers not in focus
+            background='CybuBackground',      -- for the window background
+        }
     }
-}})
+})
 vim.keymap.set('n', '<Tab>', '<Plug>(CybuPrev)')
 vim.keymap.set('n', '<S-Tab>', '<Plug>(CybuNext)')
