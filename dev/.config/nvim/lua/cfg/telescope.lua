f1c25a4c4ef7e7548040b538a3c42ba67f688110
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-08-12 19:14:09 +0300

    [nvim] try more depth

--- dev/.config/nvim/lua/cfg/telescope.lua
+++ dev/.config/nvim/lua/cfg/telescope.lua
@@ -15,7 +15,7 @@ local undo=telescope.load_extension'undo'
 local zoxide=telescope.load_extension'zoxide'
 
 local long_find = {'rg','--files','--hidden','-g','!.git'}
-local short_find = {'fd','-H','--ignore-vcs','-d','2'}
+local short_find = {'fd','-H','--ignore-vcs','-d','3'}
 
 telescope.setup{
     defaults={
