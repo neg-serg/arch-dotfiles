064ae2438e46f09d202547f6d6fdc3f293ea45ab
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-08-18 00:37:01 +0300

    [nvim] modes.nvim: custom config

--- dev/.config/nvim/lua/cfg/modes.lua
+++ dev/.config/nvim/lua/cfg/modes.lua
@@ -1,1 +1,13 @@
-require('modes').setup()
+require'modes'.setup{
+    colors={
+        copy="#f5c359",
+        delete="#c75c6a",
+        insert="#00954a",
+        visual="#9745be",
+    },
+    line_opacity=0.15, -- Set opacity for cursorline and number background
+    set_cursor=true, -- Enable cursor highlights
+    set_cursorline=false, -- Enable cursorline initially, and disable cursorline for inactive windows or ignored filetypes
+    set_number=false, -- Enable line number highlights to match cursorline
+    ignore_filetypes={'NvimTree', 'TelescopePrompt'}
+}
