7be05cd5e58786dcf0b13d8bd4dd4dcb0cb7858d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-10-12 00:26:27 +0300

    [surfingkeys] better config

--- misc/.config/surfingkeys/surfingkeys.js
+++ misc/.config/surfingkeys/surfingkeys.js
@@ -1,5 +1,29 @@
+const actions = {};
+const util = {};
+const {
+    addSearchAlias,
+    removeSearchAlias,
+    tabOpenLink,
+    readText,
+    Clipboard,
+    Front,
+    Hints,
+    Visual,
+    Normal,
+} = api;
+
+function dbg(s) {
+    console.log("[megakeys]: " + s);
+}
+
 settings.hintAlign = "left";
+settings.defaultSearchEngine = "g"; // (b)rave, (n)eeva, etc
 settings.smoothScroll = false;
+settings.cursorAtEndOfInput = true; // Input box true
+settings.blocklistPattern = /mail.google.com/;
+settings.modeAfterYank = "Normal";
+settings.scrollStepSize = 100;
+settings.omnibarPosition = "bottom";
 //Hints.characters = 'yuiophjklnm'; // for right hand
 api.Hints.characters = 'fjdkrueisl'; // Home row without pinky
 
@@ -9,6 +33,37 @@ api.cmap('E', '<Shift-Tab>');
 settings.hintAlign = "left";
 //Hints.characters = 'yuiophjklnm'; // for right hand
 api.Hints.characters = "fjdkrueisl"; // Home row without pinky
+
+// -- [ HINTS ]
+Hints.characters = "qwertasdfgzxcvb";
+// Link Hints
+Hints.style(`
+    font-family: 'JetBrainsMono Nerd Font Mono', 'SF Pro', monospace;
+    font-size: 15px;
+    font-weight: bold;
+    text-transform: lowercase;
+    color: #E5E9F0 !important;
+    background: #3B4252 !important;
+    border: solid 1px #4C566A !important;
+    text-align: center;
+    padding: 5px;
+    line-height: 1;
+`);
+
+// Text Hints
+Hints.style(`
+    font-family: 'JetBrainsMono Nerd Font Mono', 'SF Pro', monospace;
+    font-size: 15px;
+    font-weight: bold;
+    text-transform: lowercase;
+    color: #E5E9F0 !important;
+    background: #6272a4 !important;
+    border: solid 2px #4C566A !important;
+    text-align: center;
+    padding: 5px;
+    line-height: 1;
+  `, "text"
+);
 
 let videoKeys = ["v", "s", "d", "h", "l", "r"]; //Video Speed Controller Keys
 let youtubeKeys = ["i", "f", "c", "0"];
@@ -22,9 +77,7 @@ let blockSites = [
 ];
 
 const leaderKey = ",";
-
 const alt_key = (key) => leaderKey + key;
-
 // `map` handles bad domain
 const key = (newKey, oldKey, domain) => {
   if (newKey) {
@@ -70,6 +123,15 @@ api.unmap("<Ctrl-i>")
 swapKeys(";u", ";U");
 swapKeys("P", "p");
 api.vunmap("p");
+// history Back/Forward
+api.map("H", "S");
+api.map("L", "D");
+api.map("F", "gf"); // open link in new tab
+// page up/down
+api.map("<Ctrl-f>", "d");
+api.map("<Ctrl-b>", "e");
+api.unmap("<Alt-p>"); // pin/unpin current tab
+api.unmap("<Alt-m>"); // mute/unmute current tab
 
 api.mapkey(';i', 'Copy src URL of an image', function() {
     Hints.create('img[src]',(element, event) =>  {
@@ -124,80 +186,295 @@ api.mapkey(
   { domain: /github.com/i }
 );
 
-const zenbonse = `
-.sk_theme {
-  font-family: SauceCodePro Nerd Font, Consolas, Menlo, monospace;
-  font-size: 10pt;
-  background: #f0edec;
-  color: #2c363c;
-}
-.sk_theme tbody {
-  color: #f0edec;
-}
-.sk_theme input {
-  color: #2c363c;
-}
-.sk_theme .url {
-  color: #1d5573;
-}
-.sk_theme .annotation {
-  color: #2c363c;
-}
-.sk_theme .omnibar_highlight {
-  color: #88507d;
-}
-.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
-  background: #f0edec;
-}
-.sk_theme #sk_omnibarSearchResult ul li.focused {
-  background: #cbd9e3;
-}
-#sk_status,
-#sk_find {
-  font-size: 10pt;
-}
-`;
+// -- EDITOR/ACE
+api.aceVimMap(",w", ":w", "normal");
+api.aceVimMap(",q", ":q", "normal");
+api.aceVimMap("kj", "<Esc>", "insert");
+api.aceVimMap("<C-c>", "<Esc>", "insert");
+api.vmapkey("<Ctrl-c>", "#9Exit visual mode", function () {
+  Visual.exit();
+});
+api.vmapkey("<Ctrl-[>", "#9Exit visual mode", function () {
+  if (Visual.state > 1) {
+    Visual.hideCursor();
+    Visual.selection.collapse(selection.anchorNode, selection.anchorOffset);
+    Visual.showCursor();
+  } else {
+    Visual.visualClear();
+    Visual.exit();
+  }
+  Visual.state--;
+  Visual._onStateChange();
+});
 
-const solarizedDark = `
-.sk_theme {
-	background: #100a14dd;
-	color: #4f97d7;
-}
-.sk_theme tbody {
-	color: #292d;
-}
-.sk_theme input {
-	color: #d9dce0;
-}
-.sk_theme .url {
-	color: #2d9574;
-}
-.sk_theme .annotation {
-	color: #a31db1;
-}
-.sk_theme .omnibar_highlight {
-	color: #333;
-	background: #ffff00aa;
-}
-.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
-	background: #5d4d7a55;
-}
-.sk_theme #sk_omnibarSearchResult ul li.focused {
-	background: #5d4d7aaa;
-}
-.sk_theme #sk_omnibarSearchResult .omnibar_folder {
-	color: #a31db1;
-}
-`;
+// set visual-mode style
+Visual.style(
+    "marks",
+    "background-color: #A3BE8C; border: 1px solid #3B4252 !important; text-decoration: underline;"
+);
+Visual.style(
+    "cursor",
+    "background-color: #E5E9F0 !important; border: 1px solid #6272a4 !important; border-bottom: 2px solid green !important; padding: 2px !important; outline: 1px solid rgba(255,255,255,.75) !important;"
+);
 
+// -- ESC hatch
+api.imap("<Ctrl-[>", "<Esc>");
+api.imap("<Ctrl-c>", "<Esc>");
+api.cmap("<Ctrl-[>", "<Esc>");
+api.cmap("<Ctrl-c>", "<Esc>");
+
+// set theme
 settings.theme = `
-  @media (prefers-color-scheme: dark) {
-  ${solarizedDark}
-}
-  @media (prefers-color-scheme: light) {
-  ${zenbonse}
-}
-}`;
+  :root {
+    --font: "Iosevka", Arial, sans-serif;
+    --font-size: 16px;
+    --font-weight: bold;
+    --fg: #E5E9F0;
+    --bg: #3B4252;
+    --bg-dark: #2E3440;
+    --border: #4C566A;
+    --main-fg: #88C0D0;
+    --accent-fg: #A3BE8C;
+    --info-fg: #5E81AC;
+    --select: #4C566A;
+    --orange: #D08770;
+    --red: #BF616A;
+    --yellow: #EBCB8B;
+  }
+  /* ---------- Generic ---------- */
+  .sk_theme {
+  background: var(--bg);
+  color: var(--fg);
+    background-color: var(--bg);
+    border-color: var(--border);
+    font-family: var(--font);
+    font-size: var(--font-size);
+    font-weight: var(--font-weight);
+  }
+  input {
+    font-family: var(--font);
+    font-weight: var(--font-weight);
+  }
+
+  div.surfingkeys_cursor {
+    background-color: #0642CE;
+    color: red;
+  }
+  .sk_theme tbody {
+    color: var(--fg);
+  }
+  .sk_theme input {
+    color: var(--fg);
+  }
+  /* Hints */
+  #sk_hints .begin {
+    color: var(--accent-fg) !important;
+  }
+  #sk_tabs .sk_tab {
+    background: var(--bg-dark);
+    border: 1px solid var(--border);
+  }
+  #sk_tabs .sk_tab_title {
+    color: var(--fg);
+  }
+  #sk_tabs .sk_tab_url {
+    color: var(--main-fg);
+  }
+  #sk_tabs .sk_tab_hint {
+    background: var(--bg);
+    border: 1px solid var(--border);
+    color: var(--accent-fg);
+  }
+  .sk_theme #sk_frame {
+    background: var(--bg);
+    opacity: 0.2;
+    color: var(--accent-fg);
+  }
+
+  /* ---------- Omnibar ---------- */
+  /* Uncomment this and use settings.omnibarPosition = 'bottom' for Pentadactyl/Tridactyl style bottom bar */
+  /* .sk_theme#sk_omnibar {
+    width: 100%;
+    left: 0;
+  } */
+  .sk_theme .title {
+    color: var(--accent-fg);
+  }
+  .sk_theme .url {
+    color: var(--main-fg);
+  }
+  .sk_theme .annotation {
+    color: var(--accent-fg);
+  }
+  .sk_theme .omnibar_highlight {
+    color: var(--accent-fg);
+  }
+  .sk_theme .omnibar_timestamp {
+    color: var(--info-fg);
+  }
+  .sk_theme .omnibar_visitcount {
+    color: var(--accent-fg);
+  }
+  .sk_theme #sk_omnibarSearchResult ul li .url {
+    font-size: calc(var(--font-size) - 2px);
+  }
+  .sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+    background: var(--border);
+    padding: 5px;
+  }
+  .sk_theme #sk_omnibarSearchResult ul li:nth-child(even) {
+    background: var(--border);
+    padding: 5px;
+  }
+  .sk_theme #sk_omnibarSearchResult ul li.focused {
+    background: var(--bg-dark);
+    border-left: 2px solid var(--orange);
+    padding: 5px;
+    padding-left: 15px;
+  }
+  .sk_theme #sk_omnibarSearchArea {
+    border-top-color: var(--border);
+    border-bottom-color: transparent;
+    margin: 0;
+    padding: 5px 10px;
+  }
+  .sk_theme #sk_omnibarSearchArea:before {
+    content: "󱋤";
+    display: inline-block;
+    margin-left: 5px;
+    font-size: 22px;
+  }
+  .sk_theme #sk_omnibarSearchArea input,
+  .sk_theme #sk_omnibarSearchArea span {
+    font-size: 20px;
+    padding:10px 0;
+  }
+  .sk_theme #sk_omnibarSearchArea .prompt {
+    text-transform: uppercase;
+    padding-left: 10px;
+  }
+  .sk_theme #sk_omnibarSearchArea .prompt:after {
+    content: "";
+    display: inline-block;
+    margin-right: 5px;
+    color: var(--accent-fg);
+  }
+  .sk_theme #sk_omnibarSearchArea .separator {
+    color: var(--bg);
+    display: none;
+  }
+  .sk_theme #sk_omnibarSearchArea .separator:after {
+    content: "";
+    display: inline-block;
+    margin-right: 5px;
+    color: var(--accent-fg);
+  }
+
+  /* ---------- Popup Notification Banner ---------- */
+  #sk_banner {
+    font-family: var(--font);
+    font-size: var(--font-size);
+    font-weight: var(--font-weight);
+    background: var(--bg);
+    border-color: var(--border);
+    color: var(--fg);
+    opacity: 0.9;
+  }
+
+  /* ---------- Popup Keys ---------- */
+  #sk_keystroke {
+    background-color: var(--bg);
+  }
+  .sk_theme kbd .candidates {
+    color: var(--info-fg);
+  }
+  .sk_theme span.annotation {
+    color: var(--accent-fg);
+  }
+
+  /* ---------- Popup Translation Bubble ---------- */
+  #sk_bubble {
+    background-color: var(--bg) !important;
+    color: var(--fg) !important;
+    border-color: var(--border) !important;
+  }
+  #sk_bubble * {
+    color: var(--fg) !important;
+  }
+  #sk_bubble div.sk_arrow div:nth-of-type(1) {
+    border-top-color: var(--border) !important;
+    border-bottom-color: var(--border) !important;
+  }
+  #sk_bubble div.sk_arrow div:nth-of-type(2) {
+    border-top-color: var(--bg) !important;
+    border-bottom-color: var(--bg) !important;
+  }
+
+  /* ---------- Search ---------- */
+  #sk_status,
+  #sk_find {
+    font-size: var(--font-size);
+    border-color: var(--border);
+  }
+  .sk_theme kbd {
+    background: var(--bg-dark);
+    border-color: var(--border);
+    box-shadow: none;
+    color: var(--fg);
+  }
+  .sk_theme .feature_name span {
+    color: var(--main-fg);
+  }
+
+  /* ---------- ACE Editor ---------- */
+  #sk_editor {
+    background: var(--bg-dark) !important;
+    height: 50% !important;
+    /* Remove this to restore the default editor size */
+  }
+  .ace_dialog-bottom {
+    border-top: 1px solid var(--bg) !important;
+  }
+  .ace-chrome .ace_print-margin,
+  .ace_gutter,
+  .ace_gutter-cell,
+  .ace_dialog {
+    background: var(--bg) !important;
+  }
+  .ace-chrome {
+    color: var(--fg) !important;
+  }
+  .ace_gutter,
+  .ace_dialog {
+    color: var(--fg) !important;
+  }
+  .ace_cursor {
+    color: var(--fg) !important;
+  }
+  .normal-mode .ace_cursor {
+    background-color: var(--fg) !important;
+    border: var(--fg) !important;
+    opacity: 0.7 !important;
+  }
+  .ace_marker-layer .ace_selection {
+    background: var(--select) !important;
+  }
+  .ace_editor,
+  .ace_dialog span,
+  .ace_dialog input {
+    font-family: var(--font);
+    font-size: var(--font-size);
+    font-weight: var(--font-weight);
+  }
+
+  /* Disable RichHints CSS animation */
+  .expandRichHints {
+      animation: 0s ease-in-out 1 forwards expandRichHints;
+  }
+  .collapseRichHints {
+      animation: 0s ease-in-out 1 forwards collapseRichHints;
+  }
+  `;
 
 api.mapkey("gH", "tmp", () => {
   location.href = `
@@ -218,3 +495,58 @@ api.Hints.style(
   "div{color:#efe1eb; background: none; background-color: #a73a1e;} div.begin{color:#ea6962; font-size: 0.9em;}",
   "text"
 );
+
+////////////////////////////////////////////////////////////////
+// github default shortcut lists                              //
+// https:help.github.com/articles/using-keyboard-shortcuts/   //
+////////////////////////////////////////////////////////////////
+const mapkeyGithub = (...args) => api.mapkey(...args, { domain: /github\.com/i });
+mapkeyGithub("yp", "Copy project path", () => {
+  const path = new URL(window.location.href).pathname.split("/");
+  Clipboard.write(`${path[1]}/${path[2]}`);
+});
+mapkeyGithub("ygh", "Copy project path", () => {
+  const path = new URL(window.location.href).pathname.split("/");
+  Clipboard.write(`${path[1]}/${path[2]}`);
+});
+mapkeyGithub("ygc", "git clone - git clone address", () =>
+  Clipboard.write("git clone " + window.location.href + ".git")
+);
+mapkeyGithub("yv", "Copy for vim", () => {
+  const path = new URL(window.location.href).pathname.split("/");
+  Clipboard.write(`use({"${path[1]}/${path[2]}"})`);
+});
+mapkeyGithub(";gC", "Go to the code tab", () => {
+  document
+    .querySelectorAll(".js-selected-navigation-item.reponav-item")[0]
+    .click();
+});
+mapkeyGithub(";gI", "Go to the Issues tab", () => {
+  document
+    .querySelectorAll(".js-selected-navigation-item.reponav-item")[1]
+    .click();
+});
+mapkeyGithub(";gP", "Go to the Pull requests tab", () => {
+  document
+    .querySelectorAll(".js-selected-navigation-item.reponav-item")[2]
+    .click();
+});
+mapkeyGithub(";gB", "Go to the Projects tab", () => {
+  document
+    .querySelectorAll(".js-selected-navigation-item.reponav-item")[3]
+    .click();
+});
+mapkeyGithub(";gW", "Go to the Wiki tab", () => {
+  document
+    .querySelectorAll(".js-selected-navigation-item.reponav-item")[4]
+    .click();
+});
+mapkeyGithub(";gO", "Go to the Overview tab", () => {
+  document.querySelectorAll(".UnderlineNav-item")[0].click();
+});
+mapkeyGithub(";gR", "Go to the Repository tab", () => {
+  document.querySelectorAll(".UnderlineNav-item")[1].click();
+});
+mapkeyGithub(";gS", "Go to the Stars tab", () => {
+  document.querySelectorAll(".UnderlineNav-item")[2].click();
+});
