417a6e529deb737bc4aff795913ef7b0cd33cdb7
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-11-10 15:56:29 +0300

    [nvim] navigator: cleanup

--- dev/.config/nvim/lua/cfg/navigator.lua
+++ dev/.config/nvim/lua/cfg/navigator.lua
@@ -14,8 +14,7 @@ require'navigator'.setup({
     treesitter_analysis_max_num=100, -- how many items to run treesitter analysis
     treesitter_analysis_condense=true, -- condense form for treesitter analysis this value prevent slow in large projects, e.g. found 100000 reference in a project
     transparency=50, -- 0 ~ 100 blur the main window, 100: fully transparent, 0: opaque,  set to nil or 100 to disable it
-    lsp_signature_help=true, -- ray-x/lsp_signature plugin in navigator setup here. if it is nil, navigator will not init signature help
-    signature_help_cfg=nil, -- ray-x/lsp_signature plugin in navigator, and pass in your own config to signature help
+    lsp_signature_help=false, -- ray-x/lsp_signature plugin in navigator setup here. if it is nil, navigator will not init signature help
     icons={ -- refer to lua/navigator.lua for more icons config
         icons=true, -- requires nerd fonts or nvim-web-devicons
         code_action_icon='🏏', -- note: need terminal support, for those not support unicode, might crash
