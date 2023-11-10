1ba646a8a537f89ef4538d014719e7786ac8c87b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-11-10 17:32:49 +0300

    [nvim] trim: CS

--- dev/.config/nvim/lua/cfg/trim.lua
+++ dev/.config/nvim/lua/cfg/trim.lua
@@ -2,8 +2,8 @@
 -- │ █▓▒░ cappyzawa/trim.nvim                                                          │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 require'trim'.setup{
-    ft_blocklist = {"markdown"},
-    trim_on_write = false,
-    trim_last_line = false,
-    trim_first_line = false,
+    ft_blocklist={'markdown'},
+    trim_on_write=false,
+    trim_last_line=false,
+    trim_first_line=false,
 }
