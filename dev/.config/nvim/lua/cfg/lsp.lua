9df2bbcafef97ac51e4a691785cc873c30282811
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-11-10 16:00:53 +0300

    [nvim] lsp: cleanup

--- dev/.config/nvim/lua/cfg/lsp.lua
+++ dev/.config/nvim/lua/cfg/lsp.lua
@@ -1,21 +1,6 @@
 -- ┌───────────────────────────────────────────────────────────────────────────────────┐
 -- │ █▓▒░ neovim/nvim-lspconfig                                                        │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
-local nvim_lsp=require'lspconfig'
-local util=require'lspconfig/util'
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach=function(_, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-    local opts={noremap=true, silent=true}
-    buf_set_keymap('n', ']d', '<Cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '[d', '<Cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', '<leader>A', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', '<leader>e', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-end
-
 vim.diagnostic.config({
     virtual_text=true,
     signs=true,
@@ -52,33 +37,3 @@ mason_null_ls.setup({
     automatic_installation = true,
     automatic_setup = true
 })
-
-nvim_lsp.pyright.setup {
-    on_attach=on_attach,
-    root_dir=function(fname)
-        return util.root_pattern('.git','setup.py','setup.cfg','pyproject.toml','requirements.txt','Pipfile','pyrightconfig.json')(fname) or
-            util.path.dirname(fname)
-    end,
-    cmd={'pyright-langserver','--stdio'},
-    filetypes={'python'},
-    flags={debounce_text_changes=150},
-    settings={
-        python={
-            analysis={
-                autoImportCompletions=true,
-                autoSearchPaths=true,
-                diagnosticMode="openFilesOnly", -- or "workspace"
-                stubPath="typings", --or ""
-                typeshedPaths={},
-                useLibraryCodeForTypes=true,
-            },
-            linting={enabled=false},
-        }
-    },
-    single_file_support=true
-}
-nvim_lsp.bashls.setup{on_attach=on_attach}
-nvim_lsp.dockerls.setup{on_attach=on_attach}
-nvim_lsp.tsserver.setup{on_attach=on_attach}
-nvim_lsp.vimls.setup{on_attach=on_attach}
-nvim_lsp.yamlls.setup{on_attach=on_attach}
