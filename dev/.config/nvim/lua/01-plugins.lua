5a02249af299b40aed9500c86862489e298bc1bb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-11-29 01:32:58 +0300

    [nvim] kitty-scrollback fix settings

--- dev/.config/nvim/lua/01-plugins.lua
+++ dev/.config/nvim/lua/01-plugins.lua
@@ -24,7 +24,7 @@ require'lazy'.setup({
         enabled=true, lazy=true,
         cmd={'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth'},
         event={'User KittyScrollbackLaunch'},
-        opts={kitty_get_text={extent='last_cmd_output', ansi=false}},
+        opts={kitty_get_text={extent='all', ansi=false}},
         version='*', -- latest stable version, may have breaking changes if major version changed
         config=function() require('kitty-scrollback').setup() end},
     {'folke/persistence.nvim',
