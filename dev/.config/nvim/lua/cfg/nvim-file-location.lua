35974ac7ff556b5333ddd5ee1a7472ec4623abfb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-12-02 23:36:22 +0300

    [nvim] cleanup

--- dev/.config/nvim/lua/cfg/nvim-file-location.lua
+++ dev/.config/nvim/lua/cfg/nvim-file-location.lua
@@ -2,11 +2,10 @@ local status, nvim_file_location = pcall(require, "nvim-file-location")
 if not status then
     return
 end
--- custom config
 nvim_file_location.setup({
-    keymap = "<leader>c",
-    mode = "workdir", -- options: workdir | absolute
-    add_line = true,
-    add_column = false,
-    default_register = "*",
+    keymap="<leader>c",
+    mode="workdir", -- options: workdir | absolute
+    add_line=true,
+    add_column=false,
+    default_register="*",
 })
